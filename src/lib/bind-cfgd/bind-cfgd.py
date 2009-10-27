f44c0e006020211db404b03614600885bf2bd635
Jelte Jansen <jelte@isc.org>
2009-10-27 23:59:15 +0000

    oops, added bind-cfgd instead of bind-cfgd.in
    git-svn-id: svn://bind10.isc.org/svn/bind10/branches/f2f200910@133 e5f2f494-b856-4b98-b285-d166d9295462

--- src/lib/bind-cfgd/bind-cfgd.py
+++ src/lib/bind-cfgd/bind-cfgd.py
@@ -1,2 +1,24 @@
+import ISC
 
-print "Hello, BIND10 world!"
+class ConfigManager:
+    def __init__(self):
+        self.cc = ISC.CC.Session()
+
+    def read_config(self, filename):
+        pass
+
+    def write_config(self, filename):
+        pass
+
+    def run(self):
+        while (True):
+            pass
+
+if __name__ == "__main__":
+    print "Hello, BIND10 world!"
+    try:
+        cm = ConfigManager()
+        cm.run()
+    except socket.error:
+        print "Error creating config manager, "\
+              "is the command channel daemon running?"
