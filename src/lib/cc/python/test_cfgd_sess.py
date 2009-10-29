982dcbed4990e8afd57f87112824b089e27f72e7
Jelte Jansen <jelte@isc.org>
2009-10-29 00:30:23 +0000

    fixup config manager functionality now working: add zone, remove zone, list zones
    git-svn-id: svn://bind10.isc.org/svn/bind10/branches/f2f200910@157 e5f2f494-b856-4b98-b285-d166d9295462

---
+++ src/lib/cc/python/test_cfgd_sess.py
@@ -0,0 +1,70 @@
+import ISC
+
+cc = ISC.CC.Session()
+
+cc.group_subscribe("ConfigManager")
+
+cmd = { "command": [ "zone", "add", "vix.com" ] }
+print "Sending:"
+print cmd
+cc.group_sendmsg(cmd, "ConfigManager")
+data, routing = cc.group_recvmsg(False)
+print "Answer:"
+print data
+
+cmd = { "command": [ "zone", "add", "tjeb.nl" ] }
+print "Sending:"
+print cmd
+cc.group_sendmsg(cmd, "ConfigManager")
+data, routing = cc.group_recvmsg(False)
+print "Answer:"
+print data
+
+cmd = { "command": [ "zone", "add", "flame.org" ] }
+print "Sending:"
+print cmd
+cc.group_sendmsg(cmd, "ConfigManager")
+data, routing = cc.group_recvmsg(False)
+print "Answer:"
+print data
+
+cmd = { "command": [ "zone", "list" ] }
+print "Sending:"
+print cmd
+cc.group_sendmsg(cmd, "ConfigManager")
+data, routing = cc.group_recvmsg(False)
+print "Answer:"
+print data
+
+cmd = { "command": [ "zone", "remove", "flame.org" ] }
+print "Sending:"
+print cmd
+cc.group_sendmsg(cmd, "ConfigManager")
+data, routing = cc.group_recvmsg(False)
+print "Answer:"
+print data
+
+cmd = { "command": [ "zone", "list" ] }
+print "Sending:"
+print cmd
+cc.group_sendmsg(cmd, "ConfigManager")
+data, routing = cc.group_recvmsg(False)
+print "Answer:"
+print data
+
+cmd = { "command": [ "zone", "bad_command", "foo" ] }
+print "Sending:"
+print cmd
+cc.group_sendmsg(cmd, "ConfigManager")
+data, routing = cc.group_recvmsg(False)
+print "Answer:"
+print data
+
+cmd = { "command": [ "bad_module", "bad_command", "foo" ] }
+print "Sending:"
+print cmd
+cc.group_sendmsg(cmd, "ConfigManager")
+data, routing = cc.group_recvmsg(False)
+print "Answer:"
+print data
+
