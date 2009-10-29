5a55b7f27d9363e567fc2c25d0fd1f0bce7f3434
Kazunori Fujiwara <fujiwara@jprs.co.jp>
2009-10-29 21:51:12 +0000

    An example statistics respondent accepts two command:    "statistics" group "getstat"    "Boss"       group "shutdown" An example statistics collector send "getstat" command to "statistics" group. It also accepts "Boss" group "shutdown" command.
    git-svn-id: svn://bind10.isc.org/svn/bind10/branches/f2f200910@179 e5f2f494-b856-4b98-b285-d166d9295462

--- src/bin/statistics-test/respondent.py
+++ src/bin/statistics-test/respondent.py
@@ -3,34 +3,40 @@
 # This program acts statistics respondent.
 # It has pseudo "counter" which is incremented each 0.3 second and C-channel query.
 # Two command is available
-#   "whoareyou"
-#   "getstat"
+#   "statistics" group: "getstat"
+#   "Boss"       group: "shutdown"
 
 import ISC
-import socket
 import select
-import time
+import random
 
 statgroup = "statistics"
 
 cc = ISC.CC.Session()
-print cc.lname
+print (cc.lname)
 cc.group_subscribe(statgroup)
+cc.group_subscribe("Boss")
 
 counter = 0
 
 while 1:
-    r,w,e = select.select([cc._socket],[],[],0.3)
-    counter += 1
+    r,w,e = select.select([cc._socket],[],[])
     for sock in r:
         if sock == cc._socket:
             data,envelope = cc.group_recvmsg(False);
             if (envelope["group"] == statgroup):
-                print data["command"]
-                if (data["command"] == "whoareyou"):
-                    cc.group_reply(envelope, {"name": cc.lname })
-                elif (data["command"] == "getstat"):
-                    cc.group_reply(envelope, {"timestamp": time.time(), "counter":counter})
+                if (data["command"] == "getstat"):
+                    cc.group_reply(envelope,
+                                   {
+                                    "response":data["command"],
+                                    "sent": data["sent"],
+                                    "counter":counter
+                                   })
+                # Do another statistics command
+            elif (envelope["group"] == "Boss"):
+                if (data["command"] == "shutdown"):
+                    exit()
             # Do another group
          # Do another socket
     # Do main work
+    counter += random.randrange(1,100)
