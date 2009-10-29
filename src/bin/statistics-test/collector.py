b48bc6513578fdb8c012b3deca174a0fba68cc09
Kazunori Fujiwara <fujiwara@jprs.co.jp>
2009-10-29 21:51:12 +0000

    An example statistics respondent accepts two command:    "statistics" group "getstat"    "Boss"       group "shutdown" An example statistics collector send "getstat" command to "statistics" group. It also accepts "Boss" group "shutdown" command.
    git-svn-id: svn://bind10.isc.org/svn/bind10/branches/f2f200910@179 e5f2f494-b856-4b98-b285-d166d9295462

--- src/bin/statistics-test/collector.py
+++ src/bin/statistics-test/collector.py
@@ -1,39 +1,59 @@
+#!/usr/bin/python
+#
+# This program collects "counter" from "statistics" channel.
+# It accepts one command: "Boss" group "shutdown"
+
 import ISC
 import time
 import select
 
-timeout = 1
+step_time = 1
+statgroup = "statistics"
 
-tcp = ISC.CC.Session()
-print tcp.lname
+cc = ISC.CC.Session()
+print cc.lname
+cc.group_subscribe(statgroup)
+cc.group_subscribe("Boss")
 
-tcp.group_subscribe("statistics")
+server_by_name = {}
+server_by_id = []
+counter = []
+timestamp = []
+servers = 0
 
-sent = time.time()
-tcp.group_sendmsg({ "command": "whoareyou"}, "statistics")
-
-print "SEND: whoareyou"
-while (sent + timeout - time.time()) > 0:
-    wait = sent + timeout - time.time()
-    print "wait=",wait
-    r,w,e = select.select([tcp._socket],[],[], wait)
-    for sock in r:
-        if sock == tcp._socket:
-            data,envelope = tcp.group_recvmsg(False); 
-            print "data:", data
-
-print ""
-
+sent = -1
+last_sent = -1
 loop = 0
-while loop < 10000:
-    print "loop=", loop, "    SEND: getstat"
-    sent = time.time();
-    tcp.group_sendmsg({ "command": "getstat"}, "statistics")
-    while (sent + timeout - time.time()) > 0:
-        wait = sent + timeout - time.time()
-        r,w,e = select.select([tcp._socket],[],[], wait)
-        for sock in r:
-            if sock == tcp._socket:
-                data,envelope = tcp.group_recvmsg(False); 
-                print envelope["from"], " : ", data
-    loop += 1
+while 1:
+    wait = sent + step_time - time.time()
+    if wait <= 0:
+        sent = time.time();
+        command = { "command": "getstat", "sent":time.time() }
+        print "loop=", loop, "    SEND: ", command
+        cc.group_sendmsg(command, "statistics")
+        wait = last_sent + step_time - time.time()
+        if wait < 0:
+            wait = step_time
+        loop += 1
+    r,w,e = select.select([cc._socket],[],[], wait)
+    for sock in r:
+        if sock == cc._socket:
+            data,envelope = cc.group_recvmsg(False)
+            if (envelope["group"] == "Boss"):
+                if (data["command"] == "shutdown"):
+                    exit()
+            if (envelope["group"] == statgroup):
+                if (envelope["from"] in server_by_name):
+                    id = server_by_name[envelope["from"]]
+                    delta_t = float(data["sent"]) - float(timestamp[id])
+                    delta_c = float(data["counter"]) - float(counter[id])
+                    print "server",id,"  time=",data["sent"], " counter=", data["counter"], "  dT=", delta_t, "  dC=", delta_c
+                    timestamp[id] = data["sent"]
+                    counter[id] = data["counter"]
+                else:
+                    print "server ", servers, " name ", envelope["from"]
+                    server_by_id.append(envelope["from"])
+                    server_by_name[envelope["from"]] = len(server_by_id) - 1
+                    counter.append(data["counter"])
+                    timestamp.append(data["sent"])
+                    servers += 1
