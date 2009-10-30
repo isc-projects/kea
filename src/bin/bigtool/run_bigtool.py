7529cf9ce54e076384745f88fc602362b6f0fa16
Likun Zhang <zlkzhy@gmail.com>
2009-10-30 00:45:09 +0000

    1. Remove some unused path for BigTool. 2. Fix bug: can't parse the commands which don't have paramters correctly.
    git-svn-id: svn://bind10.isc.org/svn/bind10/branches/f2f200910@213 e5f2f494-b856-4b98-b285-d166d9295462

--- src/bin/bigtool/run_bigtool.py
+++ src/bin/bigtool/run_bigtool.py
@@ -1,26 +1,43 @@
 from moduleinfo  import *
 from bigtool import *
+import ISC
 
 
 def _prepare_fake_data(bigtool):
     add_cmd = CommandInfo(name = "add", desc = "add one zone")
     remove_cmd = CommandInfo(name = 'remove', desc = 'remove one zone')
-    list_cmd = CommandInfo(name = 'list', desc = 'list all zones', need_inst_param = False)
+    list_cmd = CommandInfo(name = 'list', desc = 'list all zones', 
+                           need_inst_param = False)                                                                                           
 
     zone_module = ModuleInfo(name = "zone", 
                              inst_name = "zone_name", 
                              inst_type = STRING_TYPE, 
                              inst_desc = "the name of one zone",
                              desc = "manage all the zones")
-
     zone_module.add_command(add_cmd)
     zone_module.add_command(remove_cmd)
     zone_module.add_command(list_cmd)
+
+    shutdown_cmd = CommandInfo(name = 'shutdown', desc = "stop bind10",
+                               need_inst_param = False)
+    boss_module = ModuleInfo(name = "boss", desc = "boss of bind10")
+    boss_module.add_command(shutdown_cmd)               
+
     bigtool.add_module_info(zone_module)
+    bigtool.add_module_info(boss_module)
     
 
 if __name__ == '__main__':
-    tool = BigTool()
-    _prepare_fake_data(tool)   
-    tool.cmdloop()
+    try:
+        cc = ISC.CC.Session()
+        cc.group_subscribe("BigTool")
+        cc.group_subscribe("ConfigManager")
+
+        tool = BigTool(cc)
+        _prepare_fake_data(tool)   
+        tool.cmdloop()
+    except ISC.CC.SessionError:
+        print("Failed to create cchannel session, "
+              "is the command channel daemon running?")
+
 
