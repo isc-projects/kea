a6823843adaffcf06b231854e97b0bd62dd2dbf2
Michael Graff <mgraff@isc.org>
2009-12-03 06:42:40 +0000

    checkpoint work; Python-based msgq mostly works.  Bad input will crash it, which should be fixed, probably by wrapping the entire message processing in a try loop.  Gross, but...
    git-svn-id: svn://bind10.isc.org/svn/bind10/branches/parkinglot@333 e5f2f494-b856-4b98-b285-d166d9295462

---
+++ src/lib/cc/python/test_session.py
@@ -0,0 +1,44 @@
+import ISC
+
+import time
+import pprint
+import unittest
+
+#
+# This test requires the MsgQ daemon to be running.  We are doing nasty
+# tricks here, and so insert sleeps to give things time to migrate from
+# this process, to the MsgQ, and back to this process.
+#
+
+class TestCCWireEncoding(unittest.TestCase):
+    def setUp(self):
+        self.s1 = ISC.CC.Session()
+        self.s2 = ISC.CC.Session()
+
+    def test_lname(self):
+        self.assertTrue(self.s1.lname)
+        self.assertTrue(self.s2.lname)
+
+    def test_subscribe(self):
+        self.s1.group_subscribe("g1", "i1")
+        self.s2.group_subscribe("g1", "i1")
+        time.sleep(0.5)
+        outmsg = { "data" : "foo" }
+        self.s1.group_sendmsg(outmsg, "g1", "i1")
+        time.sleep(0.5)
+        msg, env = self.s2.group_recvmsg()
+        self.assertEqual(env["from"], self.s1.lname)
+
+    def test_unsubscribe(self):
+        self.s1.group_subscribe("g1", "i1")
+        self.s2.group_subscribe("g1", "i1")
+        time.sleep(0.5)
+        self.s2.group_unsubscribe("g1", "i1")
+        outmsg = { "data" : "foo" }
+        self.s1.group_sendmsg(outmsg, "g1", "i1")
+        time.sleep(0.5)
+        msg, env = self.s2.group_recvmsg()
+        self.assertFalse(env)
+
+if __name__ == '__main__':
+    unittest.main()
