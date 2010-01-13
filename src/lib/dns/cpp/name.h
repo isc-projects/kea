b8b1724c28a7dc3aac9285218b68fa94b8f0e233
JINMEI Tatuya <jinmei@isc.org>
2010-01-13 09:09:50 +0000

    cleanup: removed redundant 'this'
    git-svn-id: svn://bind10.isc.org/svn/bind10/branches/jinmei-dnsrrparams@461 e5f2f494-b856-4b98-b285-d166d9295462

--- src/lib/dns/cpp/name.h
+++ src/lib/dns/cpp/name.h
@@ -369,17 +369,17 @@ public:
     bool equals(const Name& other) const;
 
     /// Same as equals()
-    bool operator==(const Name& other) const { return (this->equals(other)); }
+    bool operator==(const Name& other) const { return (equals(other)); }
 
     /// \brief Return true iff two names are not equal.
     ///
     /// This method simply negates the result of \c equal() method, and in that
     /// sense it's redundant.  The separate method is provided just for
     /// convenience.
-    bool nequals(const Name& other) const { return !(this->equals(other)); }
+    bool nequals(const Name& other) const { return !(equals(other)); }
 
     /// Same as nequals()
-    bool operator!=(const Name& other) const { return (this->nequals(other)); }
+    bool operator!=(const Name& other) const { return (nequals(other)); }
 
     /// \brief Less-than or equal comparison for Name against <code>other</code>
     ///
