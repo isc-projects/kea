a08e17837fb5c3e1fb2c4571380fb4f497e411e0
JINMEI Tatuya <jinmei@isc.org>
2010-02-24 02:38:40 +0000

    completed Question class with doc and more test cases
    git-svn-id: svn://bind10.isc.org/svn/bind10/trunk@943 e5f2f494-b856-4b98-b285-d166d9295462

--- src/lib/dns/cpp/question_unittest.cc
+++ src/lib/dns/cpp/question_unittest.cc
@@ -14,6 +14,11 @@
 
 // $Id$
 
+#include <vector>
+#include <sstream>
+
+#include <exceptions/exceptions.h>
+
 #include "buffer.h"
 #include "messagerenderer.h"
 #include "name.h"
@@ -33,36 +38,86 @@ namespace {
 class QuestionTest : public ::testing::Test {
 protected:
     QuestionTest() : obuffer(0), renderer(obuffer),
-                     test_question(Name("example.com"), RRClass::IN(),
-                                   RRType::NS()) 
+                     example_name1(Name("foo.example.com")),
+                     example_name2(Name("bar.example.com")),
+                     test_question1(example_name1, RRClass::IN(),
+                                    RRType::NS()),
+                     test_question2(example_name2, RRClass::CH(),
+                                    RRType::A())
     {}
     OutputBuffer obuffer;
     MessageRenderer renderer;
-    Question test_question;
-    static const uint8_t wiredata[];
+    Name example_name1;
+    Name example_name2;
+    Question test_question1;
+    Question test_question2;
+    vector<unsigned char> wiredata;
 };
 
-// wire-format representation of "example.com. NS IN"
-const uint8_t QuestionTest::wiredata[] = { 0x07, 0x65, 0x78, 0x61, 0x6d, 0x70,
-                                           0x6c, 0x65, 0x03, 0x63, 0x6f, 0x6d,
-                                           0x00, 0x00, 0x02, 0x00, 0x01 };
+Question
+questionFromWire(const char* datafile, size_t position = 0)
+{
+    vector<unsigned char> data;
+    UnitTestUtil::readWireData(datafile, data);
+
+    InputBuffer buffer(&data[0], data.size());
+    buffer.setPosition(position);
+
+    return (Question(buffer));
+}
 
 TEST_F(QuestionTest, fromWire)
 {
-    InputBuffer ibuffer(wiredata, sizeof(wiredata));
-    Question q(ibuffer);
-    EXPECT_EQ(test_question, q);
+    Question q = questionFromWire("testdata/question_fromWire");
+
+    EXPECT_EQ(example_name1, q.getName());
+    EXPECT_EQ(RRClass::IN(), q.getClass());
+    EXPECT_EQ(RRType::NS(), q.getType());
+
+    // owner name of the second Question is compressed.  It's uncommon
+    // (to have multiple questions), but isn't prohibited by the protocol.
+    q = questionFromWire("testdata/question_fromWire", 21);
+    EXPECT_EQ(example_name2, q.getName());
+    EXPECT_EQ(RRClass::CH(), q.getClass());
+    EXPECT_EQ(RRType::A(), q.getType());
+
+    // Pathological cases: Corresponding exceptions will be thrown from
+    // the underlying parser.
+    EXPECT_THROW(questionFromWire("testdata/question_fromWire", 31),
+                 BadPointer);
+    EXPECT_THROW(questionFromWire("testdata/question_fromWire", 36),
+                 IncompleteRRClass);
 }
 
 TEST_F(QuestionTest, toText)
 {
-    EXPECT_EQ("example.com. IN NS\n", test_question.toText());
+    EXPECT_EQ("foo.example.com. IN NS\n", test_question1.toText());
+    EXPECT_EQ("bar.example.com. CH A\n", test_question2.toText());
 }
 
-TEST_F(QuestionTest, toWire)
+TEST_F(QuestionTest, toWireBuffer)
 {
-    test_question.toWire(obuffer);
+    test_question1.toWire(obuffer);
+    test_question2.toWire(obuffer);
+    UnitTestUtil::readWireData("testdata/question_toWire1", wiredata);
     EXPECT_PRED_FORMAT4(UnitTestUtil::matchWireData, obuffer.getData(),
-                        obuffer.getLength(), wiredata, sizeof(wiredata));
+                        obuffer.getLength(), &wiredata[0], wiredata.size());
+}
+
+TEST_F(QuestionTest, toWireRenderer)
+{
+    test_question1.toWire(renderer);
+    test_question2.toWire(renderer);
+    UnitTestUtil::readWireData("testdata/question_toWire2", wiredata);
+    EXPECT_PRED_FORMAT4(UnitTestUtil::matchWireData, obuffer.getData(),
+                        obuffer.getLength(), &wiredata[0], wiredata.size());
+}
+
+// test operator<<.  We simply confirm it appends the result of toText().
+TEST_F(QuestionTest, LeftShiftOperator)
+{
+    ostringstream oss;
+    oss << test_question1;
+    EXPECT_EQ(test_question1.toText(), oss.str());
 }
 }
