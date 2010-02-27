a418240378dde3e1d65187659d930ee0b744356a
JINMEI Tatuya <jinmei@isc.org>
2010-02-27 00:04:06 +0000

    copyright and propset
    git-svn-id: svn://bind10.isc.org/svn/bind10/trunk@1018 e5f2f494-b856-4b98-b285-d166d9295462

--- src/lib/auth/cpp/unittest_util.cc
+++ src/lib/auth/cpp/unittest_util.cc
@@ -12,7 +12,7 @@
 // OR OTHER TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR
 // PERFORMANCE OF THIS SOFTWARE.
 
-// $Id: unittest_util.cc 754 2010-02-08 22:28:56Z each $
+// $Id$
 
 #include <iostream>
 #include <fstream>
