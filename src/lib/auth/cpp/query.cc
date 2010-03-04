140be7df88b2185863a2960861f14ee031a83933
JINMEI Tatuya <jinmei@isc.org>
2010-03-04 00:39:03 +0000

    cleanup: - minimize including header files by forward declaring pinter/reference   members - move definitions to .cc
    git-svn-id: svn://bind10.isc.org/svn/bind10/trunk@1104 e5f2f494-b856-4b98-b285-d166d9295462

--- src/lib/auth/cpp/query.cc
+++ src/lib/auth/cpp/query.cc
@@ -23,8 +23,75 @@
 
 #include "query.h"
 
+using namespace isc::dns;
+
 namespace isc {
 namespace auth {
+
+// Destructors defined here to avoid confusing the linker
+QueryTask::~QueryTask() {}
+Query::~Query() {}
+
+QueryTask::QueryTask(const isc::dns::Name& n, const isc::dns::RRClass& c,
+                     const isc::dns::RRType& t, const isc::dns::Section& sect) :
+    qname(n), qclass(c), qtype(t), zone(NULL),
+        section(sect), op(AUTH_QUERY), state(GETANSWER), flags(0)
+{}
+
+QueryTask::QueryTask(const isc::dns::Name& n, const isc::dns::RRClass& c,
+                     const isc::dns::RRType& t, const isc::dns::Section& sect,
+                     const Op o) :
+    qname(n), qclass(c), qtype(t), zone(NULL),
+    section(sect), op(o), state(GETANSWER), flags(0)
+{}
+
+QueryTask::QueryTask(const isc::dns::Name& n, const isc::dns::RRClass& c,
+                     const isc::dns::RRType& t, const isc::dns::Section& sect,
+                     const State st) :
+    qname(n), qclass(c), qtype(t), zone(NULL),
+    section(sect), op(AUTH_QUERY), state(st), flags(0)
+{}
+
+QueryTask::QueryTask(const isc::dns::Name& n, const isc::dns::RRClass& c,
+                     const isc::dns::RRType& t, const isc::dns::Section& sect,
+                     const Op o, const State st) :
+    qname(n), qclass(c), qtype(t), zone(NULL),
+    section(sect), op(o), state(st), flags(0) 
+{}
+
+QueryTask::QueryTask(const isc::dns::Name& n, const isc::dns::RRClass& c,
+                     const isc::dns::RRType& t, const Op o) :
+    qname(n), qclass(c), qtype(t), zone(NULL),
+    section(Section::ANSWER()),
+    op(o), state(GETANSWER), flags(0)
+{
+    if (op != SIMPLE_QUERY) {
+        throw "invalid constructor for this task operation";
+    }
+}
+
+// A referral query doesn't need to specify section, state, or type.
+QueryTask::QueryTask(const isc::dns::Name& n, const isc::dns::RRClass& c,
+                     const Op o) :
+    qname(n), qclass(c), qtype(RRType::ANY()), zone(NULL),
+    section(Section::ANSWER()), op(o), state(GETANSWER),
+    flags(0)
+{
+    if (op != REF_QUERY) {
+        throw "invalid constructor for this task operation";
+    }
+}
+
+QueryTask::QueryTask(const isc::dns::Name& n, const isc::dns::RRClass& c,
+                     const isc::dns::Section& sect, const Op o,
+                     const State st) :
+        qname(n), qclass(c), qtype(RRType::ANY()), zone(NULL),
+        section(sect), op(o), state(st), flags(0)
+{
+    if (op != GLUE_QUERY && op != NOGLUE_QUERY) {
+        throw "invalid constructor for this task operation";
+    }
+}
 
 Query::Query(Message& m, bool dnssec) :
     status_(PENDING), qname_(NULL), qclass_(NULL), qtype_(NULL),
@@ -44,10 +111,6 @@ Query::Query(Message& m, bool dnssec) :
     querytasks_.push(QueryTaskPtr(new QueryTask(*qname_, *qclass_, *qtype_,
                                                 Section::ANSWER())));
 }
-
-// Destructors defined here to avoid confusing the linker
-QueryTask::~QueryTask() {}
-Query::~Query() {}
 
 }
 }
