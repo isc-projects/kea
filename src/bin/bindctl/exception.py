1bed75ccf083262665e798962f0bceeff4b01bd5
JINMEI Tatuya <jinmei@isc.org>
2010-06-01 22:22:17 +0000

    removed redundant blank lines. trivial, no review.
    git-svn-id: svn://bind10.isc.org/svn/bind10/trunk@2024 e5f2f494-b856-4b98-b285-d166d9295462

--- src/bin/bindctl/exception.py
+++ src/bin/bindctl/exception.py
@@ -115,5 +115,3 @@ class CmdMissParamSyntaxError(CmdSyntaxError):
     def __str__(self):
         return str("Parameter '%s' is missed for command '%s' of module '%s'" % 
                    (self.param, self.command, self.module))
-                   
-   
