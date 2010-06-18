dd3186156c43a6b2d2d6fd245c13f33c5df12609
JINMEI Tatuya <jinmei@isc.org>
2010-06-18 03:20:31 +0000

    minor style fix: fold a long line. trivial, skipping review.
    git-svn-id: svn://bind10.isc.org/svn/bind10/trunk@2163 e5f2f494-b856-4b98-b285-d166d9295462

--- src/bin/auth/main.cc
+++ src/bin/auth/main.cc
@@ -148,7 +148,8 @@ main(int argc, char* argv[]) {
         io_service = new asio_link::IOService(auth_server, port, use_ipv4,
                                               use_ipv6);
 
-        ModuleCCSession cs(specfile, io_service->get_io_service(), my_config_handler, my_command_handler);
+        ModuleCCSession cs(specfile, io_service->get_io_service(),
+                           my_config_handler, my_command_handler);
 
         auth_server->setConfigSession(&cs);
         auth_server->updateConfig(ElementPtr());
