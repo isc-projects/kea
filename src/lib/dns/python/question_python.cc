024bea20e2b4bc861d5608c6e72854ed4879d53e
Jelte Jansen <jelte@isc.org>
2010-06-23 09:33:10 +0000

    other simple review comment fixed; alignment of inline comments, and c++ style comments instead of c style
    git-svn-id: svn://bind10.isc.org/svn/bind10/experiments/python-binding@2237 e5f2f494-b856-4b98-b285-d166d9295462

--- src/lib/dns/python/question_python.cc
+++ src/lib/dns/python/question_python.cc
@@ -77,54 +77,54 @@ static PyMethodDef Question_methods[] = {
 static PyTypeObject question_type = {
     PyVarObject_HEAD_INIT(NULL, 0)
     "libdns_python.Question",
-    sizeof(s_Question),                  /* tp_basicsize */
-    0,                                  /* tp_itemsize */
-    (destructor)Question_destroy,        /* tp_dealloc */
-    NULL,                               /* tp_print */
-    NULL,                               /* tp_getattr */
-    NULL,                               /* tp_setattr */
-    NULL,                               /* tp_reserved */
-    NULL,                               /* tp_repr */
-    NULL,                               /* tp_as_number */
-    NULL,                               /* tp_as_sequence */
-    NULL,                               /* tp_as_mapping */
-    NULL,                               /* tp_hash  */
-    NULL,                               /* tp_call */
-    Question_str,                               /* tp_str */
-    NULL,                               /* tp_getattro */
-    NULL,                               /* tp_setattro */
-    NULL,                               /* tp_as_buffer */
-    Py_TPFLAGS_DEFAULT,                 /* tp_flags */
+    sizeof(s_Question),                 // tp_basicsize
+    0,                                  // tp_itemsize
+    (destructor)Question_destroy,       // tp_dealloc
+    NULL,                               // tp_print
+    NULL,                               // tp_getattr
+    NULL,                               // tp_setattr
+    NULL,                               // tp_reserved
+    NULL,                               // tp_repr
+    NULL,                               // tp_as_number
+    NULL,                               // tp_as_sequence
+    NULL,                               // tp_as_mapping
+    NULL,                               // tp_hash 
+    NULL,                               // tp_call
+    Question_str,                       // tp_str
+    NULL,                               // tp_getattro
+    NULL,                               // tp_setattro
+    NULL,                               // tp_as_buffer
+    Py_TPFLAGS_DEFAULT,                 // tp_flags
     "The Question class encapsulates the common search key of DNS"
     "lookup, consisting of owner name, RR type and RR class.",
-    NULL,                               /* tp_traverse */
-    NULL,                               /* tp_clear */
-    NULL,                               /* tp_richcompare */
-    0,                                  /* tp_weaklistoffset */
-    NULL,                               /* tp_iter */
-    NULL,                               /* tp_iternext */
-    Question_methods,                    /* tp_methods */
-    NULL,                               /* tp_members */
-    NULL,                               /* tp_getset */
-    NULL,                               /* tp_base */
-    NULL,                               /* tp_dict */
-    NULL,                               /* tp_descr_get */
-    NULL,                               /* tp_descr_set */
-    0,                                  /* tp_dictoffset */
-    (initproc)Question_init,                /* tp_init */
-    NULL,                               /* tp_alloc */
-    PyType_GenericNew,                  /* tp_new */
-    NULL,                               /* tp_free */
-    NULL,                               /* tp_is_gc */
-    NULL,                               /* tp_bases */
-    NULL,                               /* tp_mro */
-    NULL,                               /* tp_cache */
-    NULL,                               /* tp_subclasses */
-    NULL,                               /* tp_weaklist */
+    NULL,                               // tp_traverse
+    NULL,                               // tp_clear
+    NULL,                               // tp_richcompare
+    0,                                  // tp_weaklistoffset
+    NULL,                               // tp_iter
+    NULL,                               // tp_iternext
+    Question_methods,                   // tp_methods
+    NULL,                               // tp_members
+    NULL,                               // tp_getset
+    NULL,                               // tp_base
+    NULL,                               // tp_dict
+    NULL,                               // tp_descr_get
+    NULL,                               // tp_descr_set
+    0,                                  // tp_dictoffset
+    (initproc)Question_init,            // tp_init
+    NULL,                               // tp_alloc
+    PyType_GenericNew,                  // tp_new
+    NULL,                               // tp_free
+    NULL,                               // tp_is_gc
+    NULL,                               // tp_bases
+    NULL,                               // tp_mro
+    NULL,                               // tp_cache
+    NULL,                               // tp_subclasses
+    NULL,                               // tp_weaklist
     // Note: not sure if the following are correct.  Added them just to
     // make the compiler happy.
-    NULL,                               /* tp_del */
-    0                                   /* tp_version_tag */
+    NULL,                               // tp_del
+    0                                   // tp_version_tag
 };
 
 static int
