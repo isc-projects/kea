ce1f12ede620e7892b805d17b70708605c658068
tingting shen <shentingting@cnnic.cn>
2010-06-30 07:27:30 +0000

    Added verbose options to exactly what is happening with loadzone. Added loadzone test suite of different file formats to load.
    git-svn-id: svn://bind10.isc.org/svn/bind10/trunk@2340 e5f2f494-b856-4b98-b285-d166d9295462

---
+++ src/bin/loadzone/tests/correct/get_zonedatas.py
@@ -0,0 +1,8 @@
+from isc.datasrc import sqlite3_ds
+import sys
+ZONE_FILE = "zone.sqlite3"
+zonename_set = ["include.", "ttl1.", "ttl2.", "mix1.", "mix2.", "ttlext.", "example.com."]
+for zone_name in zonename_set:
+    for rr_data in sqlite3_ds.get_zone_datas(zone_name, ZONE_FILE):
+        data_len = len(rr_data[2])
+        sys.stdout.write(rr_data[2] + '\t\t' + str(rr_data[4]) + '\tIN\t' + rr_data[5] + '\t' + rr_data[7] + '\n')
