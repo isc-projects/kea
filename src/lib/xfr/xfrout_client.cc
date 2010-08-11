fc4a4b29d232d395b7a07e0aa04a3e86da642905
Jeremy C. Reed <jreed@isc.org>
2010-08-11 16:10:43 +0000

    Minor grammar fix in error output (closer matches logging output).
    git-svn-id: svn://bind10.isc.org/svn/bind10/trunk@2704 e5f2f494-b856-4b98-b285-d166d9295462

--- src/lib/xfr/xfrout_client.cc
+++ src/lib/xfr/xfrout_client.cc
@@ -77,7 +77,7 @@ XfroutClient::sendXfroutRequestInfo(const int tcp_sock,
 {
     if (-1 == send_fd(impl_->socket_.native(), tcp_sock)) {
         isc_throw(XfroutError,
-                  "Fail to send the socket file descriptor to xfrout module");
+                  "Failed to send the socket file descriptor to xfrout module");
     }
 
     // XXX: this shouldn't be blocking send, even though it's unlikely to
