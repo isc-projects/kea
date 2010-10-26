f4214c33fe3c48f72b005e4ef08aa791dcf02580
Michal Vaner <michal.vaner@nic.cz>
2010-10-26 08:18:29 +0000

    Update --help of b10-recurse.
    git-svn-id: svn://bind10.isc.org/svn/bind10/branches/vorner-recursor-config@3353 e5f2f494-b856-4b98-b285-d166d9295462

--- src/bin/recurse/main.cc
+++ src/bin/recurse/main.cc
@@ -61,7 +61,6 @@ static bool verbose_mode = false;
 
 // Default port current 5300 for testing purposes
 static const string PROGRAM = "Recurse";
-static const char* DNSPORT = "5300";
 
 static IOService* io_service;
 static Recursor *recursor;
@@ -82,21 +81,13 @@ my_command_handler(const string& command, ConstElementPtr args) {
     } else if (command == "shutdown") {
         io_service->stop();
     }
-    
+
     return (answer);
 }
 
 void
 usage() {
-    cerr << "Usage:  b10-recurse -f nameserver [-a address] [-p port] [-u user]"
-                     "[-4|-6] [-v]" << endl;
-    cerr << "\t-f: specify the nameserver to which queries should be forwarded"
-         << endl;
-    cerr << "\t-a: specify the address to listen on (default: all)" << endl;
-    cerr << "\t-p: specify the port to listen on (default: " << DNSPORT << ")"
-         << endl;
-    cerr << "\t-4: listen on all IPv4 addresses (incompatible with -a)" << endl;
-    cerr << "\t-6: listen on all IPv6 addresses (incompatible with -a)" << endl;
+    cerr << "Usage:  b10-recurse [-u user] [-v]" << endl;
     cerr << "\t-u: change process UID to the specified user" << endl;
     cerr << "\t-v: verbose output" << endl;
     exit(1);
