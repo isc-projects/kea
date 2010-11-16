a3dd5fcdffecc0dd2ba3c92ed8a98fdc95632071
Jeremy C. Reed <jreed@isc.org>
2010-11-16 15:43:13 +0000

    Fix some doxygen commands.
    Fix misspelling of a parameter for IOAddress. (Misspelled word not
    used anywhere else.)
    
    
    git-svn-id: svn://bind10.isc.org/svn/bind10/trunk@3541 e5f2f494-b856-4b98-b285-d166d9295462

--- src/bin/auth/asio_link.h
+++ src/bin/auth/asio_link.h
@@ -132,7 +132,7 @@ public:
     /// This constructor never throws an exception.
     ///
     /// \param asio_address The ASIO \c ip::address to be converted.
-    IOAddress(const asio::ip::address& asio_adress);
+    IOAddress(const asio::ip::address& asio_address);
     //@}
 
     /// \brief Convert the address to a string.
