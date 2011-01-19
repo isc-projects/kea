03fddc0d2107ec9a573a66e75204fd1ac076f667
Jelte Jansen <jelte@isc.org>
2011-01-19 21:38:08 +0100

    propagate timeout/retries values
    apparently these weren't passed on to the recursivequery class
    (anymore?), resulting in the use of hardcoded defaults -1 and 0. This
    one-line change fixes that, acked by Michal on jabber.

--- src/bin/resolver/resolver.cc
+++ src/bin/resolver/resolver.cc
@@ -76,7 +76,7 @@ public:
     void querySetup(DNSService& dnss) {
         assert(!rec_query_); // queryShutdown must be called first
         dlog("Query setup");
-        rec_query_ = new RecursiveQuery(dnss, upstream_);
+        rec_query_ = new RecursiveQuery(dnss, upstream_, timeout_, retries_);
     }
 
     void queryShutdown() {
