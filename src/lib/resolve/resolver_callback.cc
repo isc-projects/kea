f3f9891cf8a62fbd4211df55aba1fb04e3e5c99b
Jelte Jansen <jelte@isc.org>
2011-01-28 13:51:21 +0100

    [trac494] moved extra callback layer to its own file

--- src/lib/resolve/resolver_callback.cc
+++ src/lib/resolve/resolver_callback.cc
@@ -12,7 +12,7 @@
 // OR OTHER TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR
 // PERFORMANCE OF THIS SOFTWARE.
 
-#include <resolve/resolve.h>
+#include <resolve/resolver_callback.h>
 
 namespace isc {
 namespace resolve {
