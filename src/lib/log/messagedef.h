b4dae70d94c492d94cba2f671c4f2375172d5613
Stephen Morris <stephen@isc.org>
2011-02-09 11:35:35 +0000

    [trac558] Make message IDs external symbols
    The .h file now contains external references to message IDs and
    the .cc file contains both the definition and the initialization
    of the global dictionary with the associated text.

--- src/lib/log/messagedef.h
+++ src/lib/log/messagedef.h
@@ -1,49 +1,30 @@
-// File created from messagedef.mes on Tue Feb  8 18:01:54 2011
+// File created from messagedef.mes on Wed Feb  9 10:11:36 2011
 
 #ifndef __MESSAGEDEF_H
 #define __MESSAGEDEF_H
 
 #include <log/message_types.h>
-#include <log/message_initializer.h>
 
 namespace isc {
 namespace log {
 
-static const isc::log::MessageID MSG_DUPLNS = "DUPLNS";
-static const isc::log::MessageID MSG_DUPLPRFX = "DUPLPRFX";
-static const isc::log::MessageID MSG_IDNOTFND = "IDNOTFND";
-static const isc::log::MessageID MSG_NSEXTRARG = "NSEXTRARG";
-static const isc::log::MessageID MSG_NSINVARG = "NSINVARG";
-static const isc::log::MessageID MSG_NSNOARG = "NSNOARG";
-static const isc::log::MessageID MSG_ONETOKEN = "ONETOKEN";
-static const isc::log::MessageID MSG_OPENIN = "OPENIN";
-static const isc::log::MessageID MSG_OPENOUT = "OPENOUT";
-static const isc::log::MessageID MSG_PRFEXTRARG = "PRFEXTRARG";
-static const isc::log::MessageID MSG_PRFINVARG = "PRFINVARG";
-static const isc::log::MessageID MSG_PRFNOARG = "PRFNOARG";
-static const isc::log::MessageID MSG_READERR = "READERR";
-static const isc::log::MessageID MSG_UNRECDIR = "UNRECDIR";
-static const isc::log::MessageID MSG_WRITERR = "WRITERR";
+extern const isc::log::MessageID MSG_DUPLNS;
+extern const isc::log::MessageID MSG_DUPLPRFX;
+extern const isc::log::MessageID MSG_IDNOTFND;
+extern const isc::log::MessageID MSG_NSEXTRARG;
+extern const isc::log::MessageID MSG_NSINVARG;
+extern const isc::log::MessageID MSG_NSNOARG;
+extern const isc::log::MessageID MSG_ONETOKEN;
+extern const isc::log::MessageID MSG_OPENIN;
+extern const isc::log::MessageID MSG_OPENOUT;
+extern const isc::log::MessageID MSG_PRFEXTRARG;
+extern const isc::log::MessageID MSG_PRFINVARG;
+extern const isc::log::MessageID MSG_PRFNOARG;
+extern const isc::log::MessageID MSG_READERR;
+extern const isc::log::MessageID MSG_UNRECDIR;
+extern const isc::log::MessageID MSG_WRITERR;
 
 } // namespace log
 } // namespace isc
 
-namespace isc {
-namespace log {
-
-// The next two objects are needed to bring the default message
-// definitions into the program.  They make sure that the file
-// containing the message text is included in the link process.
-//
-// The objects are uniquely named (with file name and date and
-// time of compilation) to avoid clashes with other objects of
-// the same type, either by another #include or as a global
-// symbol in another module.
-
-extern MessageInitializer messagedef_cc_Tue_Feb__8_18_01_54_2011;
-static MessageInstantiator instantiate_messagedef_cc_Tue_Feb__8_18_01_54_2011(
-   &messagedef_cc_Tue_Feb__8_18_01_54_2011);
-
-} // namespace log
-} // namespace isc
 #endif // __MESSAGEDEF_H
