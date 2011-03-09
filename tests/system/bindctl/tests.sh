1ac4ea1a6996dd39d15d4f6cbdf0a766e8d8d569
Michal 'vorner' Vaner <michal.vaner@nic.cz>
2011-03-09 11:01:08 +0100

    [trac610] Newlines fix
    The '\n' is not expanded here, which leads to sending it verbatim to
    bindctl. Replaced with real newlines.

--- tests/system/bindctl/tests.sh
+++ tests/system/bindctl/tests.sh
@@ -35,7 +35,8 @@ echo "I:Checking BIND 10 statistics after a pose ($n)"
 # wait for 2sec to make sure b10-stats gets the latest statistics.
 # note that we set statistics-interval to 1.
 sleep 2
-echo 'Stats show\n' | $RUN_BINDCTL \
+echo 'Stats show
+' | $RUN_BINDCTL \
 	--csv-file-dir=$BINDCTL_CSV_DIR > bindctl.out.$n || status=1
 # the server should have received 1 UDP and 1 TCP queries (TCP query was
 # sent from the server startup script)
@@ -45,7 +46,10 @@ if [ $status != 0 ]; then echo "I:failed"; fi
 n=`expr $n + 1`
 
 echo "I:Stopping b10-auth and checking that ($n)"
-echo 'config set Boss/start_auth false\nconfig commit\nquit\n' | $RUN_BINDCTL \
+echo 'config set Boss/start_auth false
+config commit
+quit
+' | $RUN_BINDCTL \
 	--csv-file-dir=$BINDCTL_CSV_DIR 2>&1 > /dev/null || status=1
 # dig should exit with a failure code.
 $DIG +tcp +norec @10.53.0.1 -p 53210 ns.example.com. A && status=1
@@ -53,7 +57,10 @@ if [ $status != 0 ]; then echo "I:failed"; fi
 n=`expr $n + 1`
 
 echo "I:Restarting b10-auth and checking that ($n)"
-echo 'config set Boss/start_auth true\nconfig commit\nquit\n' | $RUN_BINDCTL \
+echo 'config set Boss/start_auth true
+config commit
+quit
+' | $RUN_BINDCTL \
 	--csv-file-dir=$BINDCTL_CSV_DIR 2>&1 > /dev/null || status=1
 $DIG +norec @10.53.0.1 -p 53210 ns.example.com. A >dig.out.$n || status=1
 grep 192.0.2.1 dig.out.$n > /dev/null || status=1
@@ -62,7 +69,8 @@ n=`expr $n + 1`
 
 echo "I:Rechecking BIND 10 statistics after a pose ($n)"
 sleep 2
-echo 'Stats show\n' | $RUN_BINDCTL \
+echo 'Stats show
+' | $RUN_BINDCTL \
 	--csv-file-dir=$BINDCTL_CSV_DIR > bindctl.out.$n || status=1
 # The statistics counters should have been reset while stop/start.
 grep "\"auth.queries.tcp\": 0," bindctl.out.$n > /dev/null || status=1
@@ -73,7 +81,10 @@ n=`expr $n + 1`
 echo "I:Changing the data source from sqlite3 to in-memory ($n)"
 DATASRC_SPEC='[{"type": "memory", "zones": [{"origin": "com","file":'
 DATASRC_SPEC="${DATASRC_SPEC} \"${TEST_TOP}/bindctl/nsx1/example-normalized.db\"}]}]"
-echo "config set Auth/datasources ${DATASRC_SPEC}\nconfig commit\nquit\n" | $RUN_BINDCTL \
+echo "config set Auth/datasources ${DATASRC_SPEC}
+config commit
+quit
+" | $RUN_BINDCTL \
 	--csv-file-dir=$BINDCTL_CSV_DIR > bindctl.out.$n || status=1
 $DIG +norec @10.53.0.1 -p 53210 ns.example.com. A >dig.out.$n || status=1
 grep 192.0.2.2 dig.out.$n > /dev/null || status=1
@@ -82,7 +93,8 @@ n=`expr $n + 1`
 
 echo "I:Rechecking BIND 10 statistics after changing the datasource ($n)"
 sleep 2
-echo 'Stats show\n' | $RUN_BINDCTL \
+echo 'Stats show
+' | $RUN_BINDCTL \
 	--csv-file-dir=$BINDCTL_CSV_DIR > bindctl.out.$n || status=1
 # The statistics counters shouldn't be reset due to hot-swapping datasource.
 grep "\"auth.queries.tcp\": 0," bindctl.out.$n > /dev/null || status=1
