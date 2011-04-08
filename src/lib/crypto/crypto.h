3fe40ac4216e0c0f9296b824631207eed8ee3645
Jelte Jansen <jelte@isc.org>
2011-04-08 15:49:22 +0200

    [trac781] intermediate checkin

--- src/lib/crypto/crypto.h
+++ src/lib/crypto/crypto.h
@@ -12,13 +12,64 @@
 // OR OTHER TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR
 // PERFORMANCE OF THIS SOFTWARE.
 
+// This is an abstraction layer from an actual crypto implementation
+// The abstraction works as follows; we provide abstract base classes,
+// one general one for the entire library, one for a per-operation
+// context, and one for 'keys', in all their forms as we need them
+// for BIND10.
+// Any implementation (we currently use Botan as a backend), should
+// subclass all of these, and provide the necessary translations
+// between our calls and the underlying library. This also includes
+// translating between 'real' algorithm identifiers and ours.
+//
+// For future expansion, we may need to introduce a fourth type; sessions
+//
+
+#include <string>
+#include <dns/buffer.h>
+
 #ifndef _ISC_CRYPTO_H
 #define _ISC_CRYPTO_H
+
+void doHMAC(const isc::dns::OutputBuffer& data, char* key, isc::dns::OutputBuffer& result);
+bool verifyHMAC(const isc::dns::OutputBuffer& data, char* key, const isc::dns::OutputBuffer& mac);
 
 namespace isc {
 namespace crypto {
 
-void init();
+class Crypto {
+    static Crypto& getInstance();
+    virtual void init() = 0;
+    virtual void cleanup() = 0;
+};
+
+class TSIGKeyImpl;
+
+class TSIGKey {
+public:
+    enum algorithms {
+        TSIG_HMAC_MD5 = 1,
+        TSIG_HMAC_SHA256 = 2
+    };
+
+    TSIGKey(const std::string& str);
+    ~TSIGKey();
+    algorithms getAlgorithm();
+    const char* getSecret();
+    size_t getSecretLength();
+    
+private:
+    TSIGKeyImpl* impl_;
+};
+
+class Context {
+    virtual ~Context();
+    
+    virtual void update(void *data, size_t length) = 0;
+    virtual void signTSIG(void *result, TSIGKey* key) = 0;
+    virtual void verifyTSIG(void *signature, TSIGKey* key) = 0;
+    virtual void digest(void *result) = 0;
+};
 
 } // namespace crypto
 } // namespace isc
