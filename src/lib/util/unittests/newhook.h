78bd2b77782510d02748254a8c79249e493b6024
JINMEI Tatuya <jinmei@isc.org>
2011-04-27 16:44:48 -0700

    [trac812] added some more notes about throw_size_on_new (also made a minor editorial cleanup: removed a redundant space in .cc)

--- src/lib/util/unittests/newhook.h
+++ src/lib/util/unittests/newhook.h
@@ -61,6 +61,12 @@ extern bool force_throw_on_new;
 
 /// The allocation size that triggers an exception in the special operator new
 ///
+/// This is the exact size that causes an exception to be thrown;
+/// for example, if it is set to 100, an attempt of allocating 100 bytes
+/// will result in an exception, but allocation attempt for 101 bytes won't
+/// (unless, of course, memory is really exhausted and allocation really
+/// fails).
+///
 /// The default value is 0.  The value of this variable has no meaning
 /// unless the use of the special operator is enabled at build time and
 /// via \c force_throw_on_new.
