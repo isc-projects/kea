3bb68553bb39502b749265542c5b6d36ad80e32d
JINMEI Tatuya <jinmei@isc.org>
2011-05-12 02:30:56 -0700

    [trac915] added new support for the rest of TSIG related implementations and detailed tests.

--- src/lib/dns/python/tsig_rdata_python.h
+++ src/lib/dns/python/tsig_rdata_python.h
@@ -12,40 +12,45 @@
 // OR OTHER TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR
 // PERFORMANCE OF THIS SOFTWARE.
 
-#ifndef __PYTHON_TSIGERROR_H
-#define __PYTHON_TSIGERROR_H 1
+#ifndef __PYTHON_TSIG_H
+#define __PYTHON_TSIG_H 1
 
 #include <Python.h>
 
 namespace isc {
 namespace dns {
-class TSIGError;
+namespace rdata {
+namespace any {
+class TSIG;
+}
+}
 
 namespace python {
 
 // The s_* Class simply covers one instantiation of the object
-class s_TSIGError : public PyObject {
+class s_TSIG : public PyObject {
 public:
-    s_TSIGError();
-    const TSIGError* cppobj;
+    s_TSIG();
+    const rdata::any::TSIG* cppobj;
 };
 
-extern PyTypeObject tsigerror_type;
+extern PyTypeObject tsig_type;
 
-bool initModulePart_TSIGError(PyObject* mod);
+bool initModulePart_TSIG(PyObject* mod);
 
-/// This is A simple shortcut to create a python TSIGError object (in the
+/// This is A simple shortcut to create a python TSIG object (in the
 /// form of a pointer to PyObject) with minimal exception safety.
 /// On success, it returns a valid pointer to PyObject with a reference
 /// counter of 1; if something goes wrong it throws an exception (it never
 /// returns a NULL pointer).
 /// This function is expected to be called with in a try block
 /// followed by necessary setup for python exception.
-PyObject* createTSIGErrorObject(const TSIGError& source);
+PyObject* createTSIGObject(const rdata::any::TSIG& source);
+
 } // namespace python
 } // namespace dns
 } // namespace isc
-#endif // __PYTHON_TSIGERROR_H
+#endif // __PYTHON_TSIG_H
 
 // Local Variables:
 // mode: c++
