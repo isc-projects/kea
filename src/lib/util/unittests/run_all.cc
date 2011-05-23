56d35fbfa09fef9ae6849b1267e9460ad95bb7a0
JINMEI Tatuya <jinmei@isc.org>
2011-05-23 11:44:20 -0700

    [trac542] typo in a comment

--- src/lib/util/unittests/run_all.cc
+++ src/lib/util/unittests/run_all.cc
@@ -30,7 +30,7 @@ run_all() {
     int ret = 0;
 
     // The catching of exceptions generated in tests is controlled by the
-    // B10TEST_CATCH_EXCEPTIONS environment variable.  Setting this to
+    // B10TEST_CATCH_EXCEPTION environment variable.  Setting this to
     // 1 enables the cacthing of exceptions; setting it to 0 disables it.
     // Anything else causes a message to be printed to stderr and the default
     // taken.  (The default is to catch exceptions if compiling with clang
