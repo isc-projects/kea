127c782fc3c663639a67fb391d38f01de0d630f4
Michal 'vorner' Vaner <michal.vaner@nic.cz>
2011-06-20 16:24:47 +0200

    [trac979] Implement the creator and its tests
    It takes some test utilities from inside the loader tests and places
    them to a header, to be shared by this test as well.
    
    This includes small fix of loader interface, some of its methods can
    (and should) be const.

--- src/lib/acl/logic_check.h
+++ src/lib/acl/logic_check.h
@@ -58,16 +58,8 @@ public:
  * reasons.
  */
 template<typename Mode, typename Context>
-class LogicOperator : public CompoundCheck<Context>, boost::noncopyable {
+class LogicOperator : public CompoundCheck<Context> {
 public:
-    /// Destructor
-    ~ LogicOperator() {
-        for (typename CompoundCheck<Context>::Checks::iterator
-                 i(checks_.begin());
-             i != checks_.end(); ++i) {
-            delete *i;
-        }
-    }
     /**
      * \brief Add another subexpression.
      *
@@ -79,19 +71,21 @@ public:
      * or to remove it. It might turn out it would be needed in future to
      * optimise or it might even turn out we need shared pointers for it.
      *
-     * \param expr The new expression to put inside. This takes ownership
-     *     of the object pointed to by expr (eg. if this function does not
-     *     throw - and it throws only std::badalloc, you don't delete it,
-     *     it will be deleted when the expression is deleted.
+     * \param expr The new expression to put inside.
      */
-    void addSubexpression(const Check<Context>* expr) {
+    void addSubexpression(const boost::shared_ptr<Check<Context> >& expr) {
         checks_.push_back(expr);
     }
     /**
      * \brief The current list of subexpressions.
      */
     virtual typename CompoundCheck<Context>::Checks getSubexpressions() const {
-        return (checks_);
+        typename CompoundCheck<Context>::Checks result;
+        for (typename Checks::const_iterator i(checks_.begin());
+             i != checks_.end(); ++i) {
+            result.push_back(&**i);
+        }
+        return (result);
     }
     /**
      * \brief The match of the check.
@@ -112,8 +106,7 @@ public:
          * return that one right away. If it meets no such expression, we
          * get to the end and return the default.
          */
-        for (typename CompoundCheck<Context>::Checks::const_iterator
-                 i(checks_.begin());
+        for (typename Checks::const_iterator i(checks_.begin());
              i != checks_.end(); ++i) {
             if (Mode::terminate((*i)->matches(context))) {
                 return (!Mode::start());
@@ -123,20 +116,88 @@ public:
     }
 private:
     /// \brief List of subexpressions
-    typename CompoundCheck<Context>::Checks checks_;
+    typedef typename std::vector<boost::shared_ptr<Check<Context> > > Checks;
+    Checks checks_;
 };
 
+/**
+ * \brief Creator for the LogicOperator compound check.
+ *
+ * This class can load the ANY and ALL operators from JSON. They expect
+ * a list of subexpressions as a parameter, eg. like this:
+ *
+ * \verbatim
+ * {"ANY": [
+ *    {"ip": "1.2.3.4"},
+ *    {"ip": "5.6.7.8"}
+ * ]}
+ * \endverbatim
+ *
+ * It uses the loader to load the subexpressions, therefore whatever is
+ * supported there is supported here as well.
+ *
+ * The Mode template parameter has the same meaning as with LogicOperator,
+ * it is used to know which operators to create.
+ */
 template<typename Mode, typename Context, typename Action = BasicAction>
 class LogicCreator : public Loader<Context, Action>::CheckCreator {
 public:
-    LogicCreator(const std::string& name);
-    virtual std::vector<std::string> names() const;
+    /**
+     * \brief Constructor.
+     *
+     * \param name The name for which the loader will work. In practice,
+     *     it will usually be ANY or ALL (depending on the mode), but
+     *     anything else can be used as well.
+     */
+    LogicCreator(const std::string& name) :
+        name_(name)
+    {}
+    /// \brief Returns vector containing the name.
+    virtual std::vector<std::string> names() const {
+        std::vector<std::string> result;
+        result.push_back(name_);
+        return (result);
+    }
+    /**
+     * \brief Converts a JSON description into the logic operator.
+     *
+     * This is the place where the actual loading happens. It creates
+     * the logic operator and calls the loader on each of the list
+     * elements, placing the result into the logic operator.
+     *
+     * The first parameter is ignored and is there only to match interface.
+     *
+     * \param definition The JSON definition of the subexpressions. This must
+     *     be a list (if it isn't, the LoaderError is thrown) and the elements
+     *     must be loadable by the loader (the exceptions from it are not
+     *     caught).
+     * \param loader The loader to use for loading of subexpressions.
+     */
     virtual boost::shared_ptr<Check<Context> > create(const std::string&,
                                                       data::ConstElementPtr
                                                       definition,
                                                       const Loader<Context,
-                                                      Action>& loader);
+                                                      Action>& loader)
+    {
+        std::vector<data::ConstElementPtr> subexprs;
+        try {
+            subexprs = definition->listValue();
+        }
+        catch (const data::TypeError&) {
+            isc_throw_1(LoaderError, "Logic operator takes list", definition);
+        }
+        boost::shared_ptr<LogicOperator<Mode, Context> >
+            result(new LogicOperator<Mode, Context>);
+        for (std::vector<data::ConstElementPtr>::const_iterator
+                 i(subexprs.begin());
+             i != subexprs.end(); ++i) {
+            result->addSubexpression(loader.loadCheck(*i));
+        }
+        return (result);
+    }
     virtual bool allowListAbbreviation() const { return (false); }
+private:
+    const std::string name_;
 };
 
 }
