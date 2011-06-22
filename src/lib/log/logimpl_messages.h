46de537fd3efb4e4b43c9d420c17e00a017abba9
Stephen Morris <stephen@isc.org>
2011-06-22 10:57:42 +0100

    [trac1040] Expand logger implementation message code names
    Altered the message code names in the logger implementation messages
    to be in the form of words separated by underscores.  Also updated
    some of the descriptions at the same time.

--- src/lib/log/logimpl_messages.h
+++ src/lib/log/logimpl_messages.h
@@ -1,4 +1,4 @@
-// File created from logimpl_messages.mes on Wed Jun 22 10:38:55 2011
+// File created from logimpl_messages.mes on Wed Jun 22 10:57:02 2011
 
 #ifndef __LOGIMPL_MESSAGES_H
 #define __LOGIMPL_MESSAGES_H
@@ -8,9 +8,9 @@
 namespace isc {
 namespace log {
 
-extern const isc::log::MessageID LOGIMPL_ABOVEDBGMAX;
-extern const isc::log::MessageID LOGIMPL_BADDEBUG;
-extern const isc::log::MessageID LOGIMPL_BELOWDBGMIN;
+extern const isc::log::MessageID LOGIMPL_ABOVE_MAX_DEBUG;
+extern const isc::log::MessageID LOGIMPL_BAD_DEBUG_STRING;
+extern const isc::log::MessageID LOGIMPL_BELOW_MIN_DEBUG;
 
 } // namespace log
 } // namespace isc
