585d1c63d6d0126607f424571e38a4a60683cf4b
Stephen Morris <stephen@isc.org>
2011-06-22 17:32:06 +0100

    [trac998] Move splitIPAddress into .cc file

--- src/lib/acl/ip_check.h
+++ src/lib/acl/ip_check.h
@@ -116,53 +116,7 @@ T createMask(size_t prefixlen) {
 /// \exception InvalidParameter Address prefix not of the expected syntax
 
 std::pair<std::string, int>
-splitIPAddress(const std::string& prefix) {
-
-    // Set the default value for the prefix length.  As the type of the address
-    // is not known at the point this function is called, the maximum
-    // allowable value is also not known.  And the value of 0 is reserved for
-    // a "match any address" match.
-    int prefix_size = -1;
-
-    // Split string into its components - an address and a prefix length.
-    // We initialize by assuming that there is no slash in the string given.
-    std::string address = prefix;
-    std::string prefixlen = "";
-
-    // Parse the string given after stripping leading and trailing spaces.
-    std::string mod_prefix = isc::util::str::trim(prefix);
-    size_t slashpos = mod_prefix.find('/');
-    if ((mod_prefix.size() == 0) || (slashpos == 0) ||
-        (slashpos == (mod_prefix.size() - 1))) {
-        // Nothing in prefix, or it starts with or ends with a slash.
-        isc_throw(isc::InvalidParameter, "address prefix of " << prefix <<
-                                         " is not valid");
-
-    } else if (slashpos != std::string::npos) {
-        // There is a slash somewhere in the string, split the string on it.
-        // Don't worry about multiple slashes - if there are some, they will
-        // appear in the prefixlen segment and will be detected when an attempt
-        // is made to convert it to a number.
-        address = mod_prefix.substr(0, slashpos);
-        prefixlen = mod_prefix.substr(slashpos + 1);
-    }
-
-    // If there is a prefixlength, attempt to convert it.
-    if (!prefixlen.empty()) {
-        try {
-            prefix_size = boost::lexical_cast<int>(prefixlen);
-            if (prefix_size < 0) {
-                isc_throw(isc::InvalidParameter, "address prefix of " <<
-                          prefix << " is not valid");
-            }
-        } catch (boost::bad_lexical_cast&) {
-            isc_throw(isc::InvalidParameter, "prefix length of " << prefixlen <<
-                                             " is not valid");
-        }
-    }
-
-    return (std::make_pair(address, prefix_size));
-}
+splitIPAddress(const std::string& prefix);
 
 } // namespace internal
 
