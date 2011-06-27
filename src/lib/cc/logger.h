5f009544d1ba9f185dce2b83efff3d5fe49ddd8c
Michal 'vorner' Vaner <michal.vaner@nic.cz>
2011-06-27 17:15:43 +0200

    [trac742] Documentation comment

--- src/lib/cc/logger.h
+++ src/lib/cc/logger.h
@@ -18,14 +18,27 @@
 #include <cc/cc_messages.h>
 #include <log/macros.h>
 
+/// \file logger.h
+/// \brief Command Channel library global logger
+///
+/// This holds the logger for the CC library. It is a private header
+/// and should not be included in any publicly used header, only in local
+/// cc files.
+
 namespace isc {
 namespace cc {
 
 enum {
+    /// \brief Trace basic operation
     DBG_TRACE_BASIC = 10,
+    /// \brief Trace even details
+    ///
+    /// This includes messages being sent and received, waiting for messages
+    /// and alike.
     DBG_TRACE_DETAILED = 80
 };
 
+/// \brief Logger for this library
 extern isc::log::Logger logger;
 
 }
