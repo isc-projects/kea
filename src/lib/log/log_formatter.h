86bda90c530335386d59c72fe4d81f4d2617b663
Michal 'vorner' Vaner <michal.vaner@nic.cz>
2011-07-04 12:33:57 +0200

    [trac747] Add doxygen params

--- src/lib/log/log_formatter.h
+++ src/lib/log/log_formatter.h
@@ -163,6 +163,8 @@ public:
     }
 
     /// \brief String version of arg.
+    ///
+    /// \param arg The text to place into the placeholder.
     Formatter& arg(const std::string& arg) {
         if (logger_) {
             // Note that this method does a replacement and returns the
@@ -182,7 +184,8 @@ public:
 
     /// \brief Exception version of arg.
     ///
-    /// Simply for convenience, so the .what() doesn't have to be typed.
+    /// \param e An exception which the "what()" is extracted an put into the
+    ///     message.
     Formatter& arg(const std::exception& e) {
         if (logger_) {
             return (arg(e.what()));
