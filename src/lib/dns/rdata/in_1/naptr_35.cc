39a57104c17327895608d6cf5b026bc01720a8dc
Ocean Wang <wanghaidong@cnnic.cn>
2011-08-02 19:07:02 +0800

    [trac1130] Add basic <character-string> extraction function

--- src/lib/dns/rdata/in_1/naptr_35.cc
+++ src/lib/dns/rdata/in_1/naptr_35.cc
@@ -33,13 +33,34 @@ using namespace isc::util;
 // BEGIN_ISC_NAMESPACE
 // BEGIN_RDATA_NAMESPACE
 
-NAPTR::NAPTR(InputBuffer& buffer, size_t len) {
+NAPTR::NAPTR(InputBuffer& buffer, size_t len):
+    replacement_(".")
+{
 }
 
-NAPTR::NAPTR(const std::string& naptr_str) {
+NAPTR::NAPTR(const std::string& naptr_str):
+    replacement_(".")
+{
+    istringstream iss(naptr_str);
+    uint16_t order;
+    uint16_t preference;
+
+    iss >> order >> preference;
+
+    if (iss.bad() || iss.fail()) {
+        isc_throw(InvalidRdataText, "Invalid NAPTR text format");
+    }
+
+    order_ = order;
+    preference_ = preference;
+
+    string::const_iterator input_iterator = naptr_str.begin() + iss.tellg();
+    flags_ = getNextCharacterString(naptr_str, input_iterator);
 }
 
-NAPTR::NAPTR(const NAPTR& naptr) {
+NAPTR::NAPTR(const NAPTR& naptr):
+    replacement_(".")
+{
 }
 
 void
@@ -52,11 +73,84 @@ NAPTR::toWire(AbstractMessageRenderer& renderer) const {
 
 string
 NAPTR::toText() const {
+    return "";
 }
 
 int
 NAPTR::compare(const Rdata& other) const {
     return 0;
+}
+
+uint16_t
+NAPTR::getOrder() const {
+    return order_;
+}
+
+uint16_t
+NAPTR::getPreference() const {
+    return preference_;
+}
+
+const std::string&
+NAPTR::getFlags() const {
+    return flags_;
+}
+
+const std::string&
+NAPTR::getServices() const {
+    return services_;
+}
+
+const std::string&
+NAPTR::getRegexp() const {
+    return regexp_;
+}
+
+const Name&
+NAPTR::getReplacement() const {
+    return replacement_;
+}
+
+std::string
+NAPTR::getNextCharacterString(const std::string& input_str, std::string::const_iterator& input_iterator){
+    string result;
+
+    // Skip white spaces
+    while (input_iterator < input_str.end() && isspace(*input_iterator)) {
+        ++input_iterator;
+    }
+
+    // If the input string only contains white-spaces, it is an invalid <character-string>
+    if (input_iterator >= input_str.end()) {
+        isc_throw(InvalidRdataText, "Invalid NAPTR text format");
+    }
+
+    // Whether the <character-string> is seperated with doulble quotes symbol (")
+    bool quotes_seperated = (*input_iterator == '"');
+
+    if (quotes_seperated) {
+        ++input_iterator;
+    }
+
+    while(input_iterator < input_str.end()){
+        if (quotes_seperated) {
+            // If the <character-string> is seperated with quotes symbol and another
+            // quotes symbol is encountered, it is the end of the <character-string>
+            if (*input_iterator == '"') {
+                break;
+            }
+        } else if (*input_iterator == ' ') {
+            // If the <character-string> is not seperated with quotes symbol, it is 
+            // seperated with <space> char
+            break;
+        }
+
+        result.push_back(*input_iterator);
+
+        ++input_iterator;
+    }
+
+    return result;
 }
 
 // END_RDATA_NAMESPACE
