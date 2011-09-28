7592596f7a9f8dce2e5e8d9311cc40c5199c66e3
Jeremy C. Reed <jreed@ISC.org>
2011-09-28 10:13:59 -0500

    [master] spelling typo in comment

--- src/lib/python/isc/__init__.py
+++ src/lib/python/isc/__init__.py
@@ -1,7 +1,7 @@
 # On some systems, it appears the dynamic linker gets
 # confused if the order is not right here
 # There is probably a solution for this, but for now:
-# order is imporant here!
+# order is important here!
 import isc.cc
 import isc.config
 import isc.datasrc
