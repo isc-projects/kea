a4f7df1f7c17d097d994cdb8fb7a8121d0f71835
Stephen Morris <stephen@isc.org>
2011-10-06 19:20:50 +0100

    [1213] Miscellaneous corrections to names and content

--- tests/system/ixfr/in-2/tests.sh
+++ tests/system/ixfr/in-2/tests.sh
@@ -17,18 +17,24 @@
 
 # \file
 # This script performs the first IXFR-IN test.  A BIND 9 nameserver (the
-# "server") contains a version of the zone (version N) and two previous
-# versions, N-2 and N-4.  A BIND 10 nameserver (the "client") is loaded with
-# version N-2 of the zone.  A NOTIFY is sent to it, and it is expected that
+# "server") contains a version of the zone (version N) and three previous
+# versions, N-2, N-4 and N-6.  A BIND 10 nameserver (the "client") is loaded
+# with version N-6 of the zone.  A NOTIFY is sent to it, and it is expected that
 # it will send an IXFR to the server and update itself with the latest version
-# of the zone.  (The changes are such that the update should be in the form of
-# a single UDP packet.)
-#
+# of the zone.  (The changes are such that the update will have to take place
+# over TCP.)
+
+# On entry, the IXFR server is at version N-6.  The common tests assume that
+# it is an N-4, so update it.
+echo "I:updating IXFR-server $SERVER_NAME to suitable start version"
+update_server_zone $SERVER_NAME $SERVER_IP $IXFR_TOP/largezone_n-4.db
+status=$?
+
 # The pre-requisites for this test are the same as for the common tests, so
 # we can execute that directly.
 
 . ../common_tests.sh
-status=$?
+status=`expr $status + $?`
 
 # TODO: Check the BIND 10 log, looking for the IXFR messages that indicate that
 # it has initiated an IXFR and that it received the update via TCP.
