1ac3c4daa87f60cecfd1e21b822b72c4b817a68a
Jelte Jansen <jelte@isc.org>
2011-10-19 14:14:39 +0200

    [1290] allow examination of past b10 output

--- tests/lettuce/features/steps.py
+++ tests/lettuce/features/steps.py
@@ -7,14 +7,17 @@ def initialize(feature):
     # just make sure our cleanup won't fail if we never did
     # run the bind10 instance
     world.bind10 = None
+    world.bind10_output = []
 
 @after.each_scenario
 def cleanup(feature):
     world.shutdown_server()
+    world.bind10_output = []
 
 @step(u'Given I have no database')
 def given_i_have_no_database(step):
-    assert not os.path.exists("test.db")
+    if os.path.exists("test.db"):
+        os.remove("test.db")
 
 @step(u'I should see a database file')
 def i_should_see_a_database_file(step):
