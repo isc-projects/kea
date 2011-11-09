99575a3316c772a1f34f6c3ef884cce7bf7806e9
John DuBois <johnd@isc.org>
2011-11-08 23:25:51 -0800

    [1324] Added braces around some single-line if blocks missed in the previous coding-guidelines changes Further work on BIND9 style conformance, where it doesn't contradict BIND10 style.  Not yet complete. Added comments in headers and source files Added BIND10 headers to remaining files Removed debugging code left in Added more gtests, for option combinations Removed files that are not part of currently tested code base Addressed other requirements from code review (not yet complete)

--- tests/tools/perfdhcp/cloptions.h
+++ tests/tools/perfdhcp/cloptions.h
@@ -1,5 +1,42 @@
+/*
+ * Copyright (C) 2011  Internet Systems Consortium, Inc. ("ISC")
+ *
+ * Permission to use, copy, modify, and/or distribute this software for any
+ * purpose with or without fee is hereby granted, provided that the above
+ * copyright notice and this permission notice appear in all copies.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS" AND ISC DISCLAIMS ALL WARRANTIES WITH
+ * REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF MERCHANTABILITY
+ * AND FITNESS.  IN NO EVENT SHALL ISC BE LIABLE FOR ANY SPECIAL, DIRECT,
+ * INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES WHATSOEVER RESULTING FROM
+ * LOSS OF USE, DATA OR PROFITS, WHETHER IN AN ACTION OF CONTRACT, NEGLIGENCE
+ * OR OTHER TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR
+ * PERFORMANCE OF THIS SOFTWARE.
+ */
+
+#ifndef CLOPTIONS_H
+#define CLOPTIONS_H
+
 #include <stdint.h>
 #include "procconf.h"
 
+/*
+ * Process the options/arguments passed to the program.
+ *
+ * Input varibles:
+ * argc, argv: Command line data.
+ *
+ * Output variables:
+ * confdata; Processed command line data.
+ * server: The server to contact.  This will always be set, either to the given
+ *     value or to "all" (see usage).
+ *
+ * Return value:
+ * 0 if the command has been satisfied and the program should exit 0.
+ * 2 for usage error, in which case an error message will have been printed.
+ * 1 if argument processing was successful and the program should continue.
+ */
 int procArgs(int argc, const char* argv[], confdata_t* confdata,
              const char** server);
+
+#endif
--- tests/tools/perfdhcp/tests/cloptions_unittest.cc
+++ tests/tools/perfdhcp/tests/cloptions_unittest.cc
@@ -15,44 +15,53 @@
 #include <stdint.h>
 #include <gtest/gtest.h>
 #include <exceptions/exceptions.h>
+#include <stdarg.h>
 
 #include "../cloptions.h"
 
-int checkOption(const char* optflag, const char* optval,
-                confdata_t* confdata, int retExpected) {
-    const char* argv[] = { "perfdhcp", NULL, NULL, NULL };
+// Up to 4 arguments may be specified
+// The final argument must be NULL.  This is not counted in the 4 arguments.
+int checkOption(confdata_t* confdata, int retExpected, ...) {
+    const char* argv[6] = { "perfdhcp" };
     int argc = 1;
     const char* server;
     int ret;
+    const char* p;
+    va_list ap;
 
-    argv[argc++] = optflag;
-    if (optval != NULL) {
-        argv[argc++] = optval;
+    va_start(ap, retExpected);
+    p = va_arg(ap, const char *);
+    while (p != NULL && argc < 5) {
+        argv[argc++] = p;
+        p = va_arg(ap, const char *);
     }
+    va_end(ap);
     argv[argc++] = "foo";   /* server */
     ret = procArgs(argc, argv, confdata, &server);
     EXPECT_EQ(ret, retExpected);
-    return ret == retExpected;
+    return(ret == retExpected);
 }
 
 void checkOptionInvalid(const char* optflag, const char* optval) {
     confdata_t confdata;
 
-    checkOption(optflag, optval, &confdata, 2);
+    checkOption(&confdata, 2, optflag, optval, NULL);
 }
 
 int checkOptionValid(const char* optflag, const char* optval,
                      confdata_t* confdata) {
 
-    return checkOption(optflag, optval, confdata, 1);
+    return(checkOption(confdata, 1, optflag, optval, NULL));
 }
 
 void checkPosFloatOpt(const char* optflag) {
     confdata_t confdata;
 
-    if (checkOptionValid(optflag, "100.0", &confdata))
+    if (checkOptionValid(optflag, "100.0", &confdata)) {
         EXPECT_EQ(100.0,
-                  confdata.map[(unsigned)optflag[1]]->values[0]->value.floatval);
+                  confdata.map[(unsigned)optflag[1]]->values[0]->
+                  value.floatval);
+    }
     checkOptionInvalid(optflag, "0");
     checkOptionInvalid(optflag, "-1");
     checkOptionInvalid(optflag, "0.0");
@@ -63,9 +72,10 @@ void checkPosFloatOpt(const char* optflag) {
 void checkPosIntOpt(const char* optflag) {
     confdata_t confdata;
 
-    if (checkOptionValid(optflag, "100", &confdata))
+    if (checkOptionValid(optflag, "100", &confdata)) {
         EXPECT_EQ(100,
                   confdata.map[(unsigned)optflag[1]]->values[0]->value.nnint);
+    }
     checkOptionInvalid(optflag, "0");
     checkOptionInvalid(optflag, "-1");
     checkOptionInvalid(optflag, "1.0");
@@ -75,32 +85,36 @@ void checkPosIntOpt(const char* optflag) {
 void checkFlag(const char* optflag) {
     confdata_t confdata;
 
-    if (checkOptionValid(optflag, NULL, &confdata))
+    if (checkOptionValid(optflag, NULL, &confdata)) {
         EXPECT_EQ(1,
-                  confdata.map[(unsigned)optflag[1]]->values[0]->value.switchval);
+                  confdata.map[(unsigned)optflag[1]]->values[0]->
+                  value.switchval);
+    }
 }
 
 void checkStrOpt(const char* optflag) {
     confdata_t confdata;
 
-    if (checkOptionValid(optflag, "bar", &confdata))
+    if (checkOptionValid(optflag, "bar", &confdata)) {
         EXPECT_EQ("bar",
                   confdata.map[(unsigned)optflag[1]]->values[0]->value.string);
+    }
 }
 
 void checkNEStrOpt(const char* optflag) {
     confdata_t confdata;
 
-    if (checkOptionValid(optflag, "bar", &confdata))
+    if (checkOptionValid(optflag, "bar", &confdata)) {
         EXPECT_EQ("bar",
                   confdata.map[(unsigned)optflag[1]]->values[0]->value.string);
+    }
     checkOptionInvalid(optflag, "");
 }
 
 void checkFlagHandled(const char* optflag) {
     confdata_t confdata;
 
-    checkOption(optflag, NULL, &confdata, 0);
+    checkOption(&confdata, 0, optflag, NULL);
 }
 
 TEST(CommandOptionsTest, numreq) {
@@ -159,4 +173,40 @@ TEST(CommandOptionsTest, maxdrop) {
  */
 TEST(CommandOptionsTest, nosuchopt) {
     checkOptionInvalid("-W", "");
+}
+
+TEST(CommandOptionsTest, v4v6) {
+    checkOptionInvalid("-4", "-6");
+}
+
+TEST(CommandOptionsTest, ilr6) {
+    confdata_t confdata;
+
+    if (checkOption(&confdata, 1, "-6i", "-leth0", "-r", "50", NULL)) {
+        EXPECT_EQ("eth0",
+                  confdata.map[(unsigned)'l']->values[0]->value.string);
+        EXPECT_EQ(50, confdata.map[(unsigned)'r']->values[0]->value.nnint);
+        EXPECT_EQ(1, confdata.map[(unsigned)'6']->values[0]-> value.switchval);
+        EXPECT_EQ(1, confdata.map[(unsigned)'i']->values[0]-> value.switchval);
+    }
+}
+
+TEST(CommandOptionsTest, ind) {
+    confdata_t confdata;
+
+    if (checkOption(&confdata, 1, "-i", "-n10", "-d", "5", NULL)) {
+        EXPECT_EQ(1, confdata.map[(unsigned)'i']->values[0]-> value.switchval);
+        EXPECT_EQ(10, confdata.map[(unsigned)'n']->values[0]->value.nnint);
+        EXPECT_EQ(5, confdata.map[(unsigned)'d']->values[0]-> value.floatval);
+    }
+}
+
+TEST(CommandOptionsTest, px) {
+    confdata_t confdata;
+
+    if (checkOption(&confdata, 1, "-p", "5.5", "-x", "fo", NULL)) {
+        EXPECT_EQ("fo",
+                  confdata.map[(unsigned)'x']->values[0]->value.string);
+        EXPECT_EQ(5.5, confdata.map[(unsigned)'p']->values[0]->value.floatval);
+    }
 }
