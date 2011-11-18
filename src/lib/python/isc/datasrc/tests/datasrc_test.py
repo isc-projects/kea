c9d3a387b49a4d9a44b8ef6cf37bf81344ef9dbd
JINMEI Tatuya <jinmei@isc.org>
2011-11-18 09:11:54 -0800

    [1333r] added comment about why we need to clear some JournalRead attributes in tearDown.

--- src/lib/python/isc/datasrc/tests/datasrc_test.py
+++ src/lib/python/isc/datasrc/tests/datasrc_test.py
@@ -694,6 +694,11 @@ class JournalRead(unittest.TestCase):
         self.reader = None
 
     def tearDown(self):
+        # Some test leaves the reader in the middle of sequence, holding
+        # the lock.  Since the unittest framework keeps each test object
+        # until the end of the entire tests, we need to make sure the reader
+        # is released at the end of each test.  The client shouldn't do harm
+        # but we clean it up, too, just in case.
         self.dsc = None
         self.reader = None
 
