354bb735d31ec83aba2eb0afe3a0e05aa8f254d5
JINMEI Tatuya <jinmei@isc.org>
2012-01-17 09:47:05 -0800

    [1546] swapped comments about deferring asynchronous listening to cc messages. the detailed version of comments were intended to be for auth, but placed in the wrong place (resolver/main).

--- src/bin/auth/main.cc
+++ src/bin/auth/main.cc
@@ -163,7 +163,12 @@ main(int argc, char* argv[]) {
         isc::server_common::initSocketRequestor(*cc_session);
 
         // We delay starting listening to new commands/config just before we
-        // go into the main loop.   See auth/main.cc for the rationale.
+        // go into the main loop to avoid confusion due to mixture of
+        // synchronous and asynchronous operations (this would happen in
+        // initial communication with the boss that takes place in
+        // updateConfig() for listen_on and in initializing TSIG keys below).
+        // Until then all operations on the CC session will take place
+        // synchronously.
         config_session = new ModuleCCSession(specfile, *cc_session,
                                              my_config_handler,
                                              my_command_handler, false);
