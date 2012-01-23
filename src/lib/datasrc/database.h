1262df829dea239e99206d71aa0281928c5ff426
Michal 'vorner' Vaner <michal.vaner@nic.cz>
2012-01-23 15:58:52 +0100

    [1431] Indentation

--- src/lib/datasrc/database.h
+++ src/lib/datasrc/database.h
@@ -754,10 +754,10 @@ public:
         virtual isc::dns::Name findPreviousName(const isc::dns::Name& query)
             const;
 
-	/// Look for NSEC3 for proving (non)existence of given name.
-	///
-	/// See documentation in \c Zone.
-	virtual FindNSEC3Result
+        /// Look for NSEC3 for proving (non)existence of given name.
+        ///
+        /// See documentation in \c Zone.
+        virtual FindNSEC3Result
         findNSEC3(const isc::dns::Name& name, bool recursive);
 
         /// \brief The zone ID
