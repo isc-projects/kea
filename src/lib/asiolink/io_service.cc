556a50993bd1091ed6a8485c367bae1be6df4e4e
Jeremy C. Reed <jreed@ISC.org>
2012-02-01 21:06:10 -0600

    [trac1644] add system headers or reorder existing headers
    needed for some systems that have system headers that don't include headers that
    define what they need.

--- src/lib/asiolink/io_service.cc
+++ src/lib/asiolink/io_service.cc
@@ -14,9 +14,9 @@
 
 #include <config.h>
 
+#include <unistd.h>             // for some IPC/network system calls
 #include <netinet/in.h>
 #include <sys/socket.h>
-#include <unistd.h>             // for some IPC/network system calls
 
 #include <asio.hpp>
 #include <asiolink/io_service.h>
