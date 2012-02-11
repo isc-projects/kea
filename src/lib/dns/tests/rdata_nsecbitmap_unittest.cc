c35950bad4e73ce685071f4293a7ff9cc36d2c63
JINMEI Tatuya <jinmei@isc.org>
2012-02-10 19:09:18 -0800

    [1641] consolidated rrtype bitmap parser for NSEC and NSEC3.

--- src/lib/dns/tests/rdata_nsecbitmap_unittest.cc
+++ src/lib/dns/tests/rdata_nsecbitmap_unittest.cc
@@ -168,6 +168,13 @@ TYPED_TEST(NSECLikeBitmapTest, createFromWire) {
                  DNSMessageFORMERR);
 }
 
+TYPED_TEST(NSECLikeBitmapTest, badText) {
+    // redundant space after the sequence
+    EXPECT_THROW(this->fromText(this->getCommonText() + "A "),
+                 InvalidRdataText);
+}
+
+
 // This tests the result of toText() with various kinds of NSEC/NSEC3 bitmaps.
 // It also tests the "from text" constructor as a result.
 TYPED_TEST(NSECLikeBitmapTest, toText) {
