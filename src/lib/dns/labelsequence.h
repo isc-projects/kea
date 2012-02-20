0255bfb9925710dc04d1bf88ad138c33ed7e7530
Jelte Jansen <jelte@isc.org>
2012-02-20 23:29:31 +0100

    [1602] initial implementation

---
+++ src/lib/dns/labelsequence.h
@@ -0,0 +1,50 @@
+// Copyright (C) 2009  Internet Systems Consortium, Inc. ("ISC")
+//
+// Permission to use, copy, modify, and/or distribute this software for any
+// purpose with or without fee is hereby granted, provided that the above
+// copyright notice and this permission notice appear in all copies.
+//
+// THE SOFTWARE IS PROVIDED "AS IS" AND ISC DISCLAIMS ALL WARRANTIES WITH
+// REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF MERCHANTABILITY
+// AND FITNESS.  IN NO EVENT SHALL ISC BE LIABLE FOR ANY SPECIAL, DIRECT,
+// INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES WHATSOEVER RESULTING FROM
+// LOSS OF USE, DATA OR PROFITS, WHETHER IN AN ACTION OF CONTRACT, NEGLIGENCE
+// OR OTHER TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR
+// PERFORMANCE OF THIS SOFTWARE.
+
+#ifndef __LABELSEQUENCE_H
+#define __LABELSEQUENCE_H 1
+
+#include <dns/name.h>
+#include <util/buffer.h>
+
+namespace isc {
+namespace dns {
+
+class LabelSequence {
+public:
+    LabelSequence(const Name& name);
+    ~LabelSequence();
+
+    const char* getData() const;
+    const char* getData(size_t* len) const;
+
+    bool equals(const LabelSequence& other, bool case_sensitive = false) const;
+
+    void split(int i);
+
+    size_t getLabelCount() { return last_label_ - first_label_ + 1; }
+
+private:
+    const Name& name_;
+    size_t first_label_;
+    size_t last_label_;
+    char* data_;
+    size_t* offsets_;
+};
+
+
+} // end namespace dns
+} // end namespace isc
+
+#endif
