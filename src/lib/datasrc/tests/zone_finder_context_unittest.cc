514260275e586d0c42c7cad91a5a0fb3b2075d34
JINMEI Tatuya <jinmei@isc.org>
2012-03-09 23:26:01 -0800

    [1608] added a new test: wildcard expansion. not work for in-memory for now.

--- src/lib/datasrc/tests/zone_finder_context_unittest.cc
+++ src/lib/datasrc/tests/zone_finder_context_unittest.cc
@@ -261,6 +261,21 @@ TEST_P(ZoneFinderContextTest, getAdditionalDelegationWithEmptyName) {
                 result_sets_.begin(), result_sets_.end());
 }
 
+TEST_P(ZoneFinderContextTest, getAdditionalDelegationWithWild) {
+    // The NS name needs to be expanded by a wildcard.  Currently it doesn't
+    // work for the optimized in-memory version.
+    if (GetParam() == createInMemoryClient) {
+        return;
+    }
+
+    ZoneFinderContextPtr ctx = finder_->find(Name("www.e.example.org"),
+                                             RRType::AAAA());
+    EXPECT_EQ(ZoneFinder::DELEGATION, ctx->code);
+    ctx->getAdditional(REQUESTED_BOTH, result_sets_);
+    rrsetsCheck("ns.wild.example.org. 3600 IN A 192.0.2.15\n",
+                result_sets_.begin(), result_sets_.end());
+}
+
 TEST_P(ZoneFinderContextTest, getAdditionalMX) {
     // Similar to the previous cases, but for MX addresses.  The test zone
     // contains MX name under a zone cut.  Its address shouldn't be returned.
