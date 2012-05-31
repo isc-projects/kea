d70a49cb524bd7179d9cc1948ca458951a4245c5
Marcin Siodelski <marcin@isc.org>
2012-05-31 18:39:11 +0200

    [1956] PerfPkt6 class code improvements.
    Improved error checks, comments, additional unit tests added.

--- tests/tools/perfdhcp/perf_pkt6.cc
+++ tests/tools/perfdhcp/perf_pkt6.cc
@@ -21,31 +21,62 @@
 
 using namespace std;
 using namespace isc;
+using namespace dhcp;
 
 namespace isc {
 namespace perfdhcp {
 
-PerfPkt6::PerfPkt6(const uint8_t* buf, uint32_t len, size_t transid_offset) :
-    Pkt6(buf, len, dhcp::Pkt6::UDP),
-    transid_offset_(transid_offset) {
+PerfPkt6::PerfPkt6(const uint8_t* buf, uint32_t len,
+                   uint32_t transid, const Offset& transid_offset) :
+    Pkt6(buf, len, Pkt6::UDP),
+    transid_offset_(transid_offset.get()) {
+    transid_ = transid;
+}
 
-    // Let's reset timestamp of the packet
-    memset(static_cast<void*>(&time_stamp_), 0, sizeof(time_stamp_));
+PerfPkt6::PerfPkt6(const uint8_t* buf,
+                   uint32_t len,
+                   const Offset&
+                   transid_offset) :
+    Pkt6(buf, len, Pkt6::UDP),
+    transid_offset_(transid_offset.get()) {
 }
 
 bool
 PerfPkt6::rawPack() {
+    // Always override the packet if function is called.
+    bufferOut_.clear();
+    // Write whole buffer to output buffer.
+    bufferOut_.writeData(&data_[0], data_.size());
+
+    if ((transid_offset_ + 4 > data_.size()) ||
+        (transid_offset_ == 0)) {
+        cout << "Failed to build packet: provided transaction id offset: "
+             << transid_offset_ <<  " is out of bounds (expected 1.."
+             << data_.size()-1 << ")." << endl;
+        return (false);
+    }
+
+    // Seek to the transaction id position in output buffer.
+    bufferOut_.clear();
+    bufferOut_.skip(transid_offset_);
+
     try {
-        // Always override the packet if function is called.
-        bufferOut_.clear();
-        // Write whole buffer to output buffer.
-        bufferOut_.writeData(&data_[0], data_.size());
-        // We already have packet template stored in out buffer
+        // Write 3 octets of transaction id
+        bufferOut_.writeUint8(transid_ >> 16 & 0xFF);
+        bufferOut_.writeUint8(transid_ >> 8 & 0xFF);
+        bufferOut_.writeUint8(transid_ & 0xFF);
+
+        // Buffer pointer is at the end of transaction id.
+        // We have to seek to the end of buffer so as data don't
+        // get truncated.
+        bufferOut_.skip(data_.size() - bufferOut_.getLength());
+
+        // We already have packet template stored in output buffer
         // but still some options have to be updated if client
         // specified them along with their offsets in the buffer.
         rawPackOptions();
-    } catch (isc::Unexpected& e) {
-        cout << "Failed to build packet: " << e.what() << endl;
+    } catch (isc::BadValue& e) {
+        cout << "Building packet failed: " << e.what() << endl;
         return (false);
     }
     return (true);
@@ -53,6 +84,15 @@ PerfPkt6::rawPack() {
 
 bool
 PerfPkt6::rawUnpack() {
+    // Validate transaction id offset.
+    if ((transid_offset_ + 4 > data_.size()) ||
+        (transid_offset_ == 0)) {
+        cout << "Failed to parse packet: provided transaction id offset: "
+             << transid_offset_ <<  " is out of bounds (expected 1.."
+             << data_.size()-1 << ")." << endl;
+        return (false);
+    }
+
     // Read transaction id from the packet at the given offset.
     transid_ = ( (data_[transid_offset_]) << 16 ) +
         ((data_[transid_offset_ + 1]) << 8) + (data_[transid_offset_ + 2]);
@@ -62,7 +102,7 @@ PerfPkt6::rawUnpack() {
     msg_type_ = data_[0];
     try {
         rawUnpackOptions();
-    } catch (isc::Unexpected& e) {
+    } catch (isc::BadValue& e) {
         cout << "Packet parsing failed: " << e.what() << endl;
         return (false);
     }
@@ -73,66 +113,77 @@ PerfPkt6::rawUnpack() {
 void
 PerfPkt6::rawPackOptions() {
     try {
-        // If there are any options on the list we will use provided options offsets
-        // to override them in the output buffer with new contents.
-        for (dhcp::Option::OptionCollection::const_iterator it = options_.begin();
+        // If there are any options on the list, we will use provided
+        // options offsets to override them in the output buffer
+        // with new contents.
+        for (Option::OptionCollection::const_iterator it = options_.begin();
              it != options_.end(); ++it) {
             // Get options with their position (offset).
-            boost::shared_ptr<LocalizedOption> option = boost::dynamic_pointer_cast<LocalizedOption>(it->second);
-            uint32_t position = option->getOffset();
-            if (position > 0) {
-                bufferOut_.clear();
-                bufferOut_.skip(position);
+            boost::shared_ptr<LocalizedOption> option =
+                boost::dynamic_pointer_cast<LocalizedOption>(it->second);
+            uint32_t offset = option->getOffset();
+            if ((offset == 0) ||
+                (offset + option->len() > data_.size())) {
+                isc_throw(isc::BadValue,
+                          "option offset for option: " << option->getType()
+                          << " is out of bounds (expected 1.."
+                          << data_.size() - option->len() << ")");
             }
+            bufferOut_.clear();
+            bufferOut_.skip(offset);
+
             // Replace existing option with new value.
             option->pack(bufferOut_);
         }
-        // Seek to the end of the buffer to make sure size is correct.
+        // Seek to the end of the buffer to make sure its size is correct.
         bufferOut_.skip(data_.size() - bufferOut_.getLength());
     }
     catch (const Exception&) {
-        isc_throw(isc::Unexpected, "Failed to build packet (Option build failed");
+        isc_throw(isc::BadValue, "failed to pack options into buffer.");
     }
 }
 
 void
 PerfPkt6::rawUnpackOptions() {
-    for (dhcp::Option::OptionCollection::const_iterator it = options_.begin();
+    for (Option::OptionCollection::const_iterator it = options_.begin();
          it != options_.end(); ++it) {
 
-        LocalizedOptionPtr option = boost::dynamic_pointer_cast<LocalizedOption>(it->second);
+        LocalizedOptionPtr option =
+            boost::dynamic_pointer_cast<LocalizedOption>(it->second);
         size_t opt_pos = option->getOffset();
-
         if (opt_pos == 0) {
-            isc_throw(isc::BadValue, "Failed to unpack packet from raw buffer "
+            isc_throw(isc::BadValue, "failed to unpack packet from raw buffer "
                       "(Option position not specified)");
         } else if (opt_pos + 4 > data_.size()) {
-            isc_throw(isc::BadValue, "Failed to unpack options from from raw buffer "
+            isc_throw(isc::BadValue,
+                      "failed to unpack options from from raw buffer "
                       "(Option position out of bounds)");
         }
 
         size_t offset = opt_pos;
+
+        // Get option type from first two octets.
         uint16_t opt_type = data_[offset] * 256 + data_[offset + 1];
         if (opt_type != option->getType()) {
             isc_throw(isc::BadValue,
-                      "Failed to unpack option from raw buffer (Option type mismatch)");
+                      "failed to unpack option from raw buffer "
+                      "(option type mismatch)");
         }
+
+        // Get length from next two octets.
         offset += 2;
         uint16_t opt_len = data_[offset] * 256 + data_[offset + 1];
-        offset += 2;
 
         if (offset + opt_len > data_.size()) {
             isc_throw(isc::BadValue,
-                      "Failed to unpack option from raw buffer (Option truncated)");
+                      "failed to unpack option from raw buffer "
+                      "(option truncated)");
         }
-        option->setData(data_.begin() + offset, data_.begin() + offset + opt_len);
-    }
-}
 
-void
-PerfPkt6::updateTimestamp() {
-    if (clock_gettime(CLOCK_REALTIME, &time_stamp_) < 0) {
-        isc_throw(isc::Unexpected, "Failed to get timestamp for packet");
+        // Seek to actual option data and replace it.
+        offset += 2;
+        option->setData(data_.begin() + offset,
+                        data_.begin() + offset + opt_len);
     }
 }
 
