ac60866c8f452624c1291877cdd613ea35d68e14
Mukund Sivaraman <muks@isc.org>
2012-07-06 11:35:38 +0530

    [2088] Indent test code

--- src/lib/util/tests/memory_segment_local_unittest.cc
+++ src/lib/util/tests/memory_segment_local_unittest.cc
@@ -22,34 +22,34 @@ namespace isc {
 namespace util {
 
 TEST(MemorySegmentLocal, TestLocal) {
-  auto_ptr<MemorySegment> segment(new MemorySegmentLocal());
+    auto_ptr<MemorySegment> segment(new MemorySegmentLocal());
 
-  // By default, nothing is allocated.
-  EXPECT_TRUE(segment->allMemoryDeallocated());
+    // By default, nothing is allocated.
+    EXPECT_TRUE(segment->allMemoryDeallocated());
 
-  void *ptr = segment->allocate(1024);
+    void *ptr = segment->allocate(1024);
 
-  // Now, we have an allocation:
-  EXPECT_FALSE(segment->allMemoryDeallocated());
+    // Now, we have an allocation:
+    EXPECT_FALSE(segment->allMemoryDeallocated());
 
-  void *ptr2 = segment->allocate(42);
+    void *ptr2 = segment->allocate(42);
 
-  // Still:
-  EXPECT_FALSE(segment->allMemoryDeallocated());
+    // Still:
+    EXPECT_FALSE(segment->allMemoryDeallocated());
 
-  // These should not fail, because the buffers have been allocated.
-  EXPECT_NO_FATAL_FAILURE(memset(ptr, 0, 1024));
-  EXPECT_NO_FATAL_FAILURE(memset(ptr, 0, 42));
+    // These should not fail, because the buffers have been allocated.
+    EXPECT_NO_FATAL_FAILURE(memset(ptr, 0, 1024));
+    EXPECT_NO_FATAL_FAILURE(memset(ptr, 0, 42));
 
-  segment->deallocate(ptr, 1024);
+    segment->deallocate(ptr, 1024);
 
-  // Still:
-  EXPECT_FALSE(segment->allMemoryDeallocated());
+    // Still:
+    EXPECT_FALSE(segment->allMemoryDeallocated());
 
-  segment->deallocate(ptr2, 42);
+    segment->deallocate(ptr2, 42);
 
-  // Now, we have an deallocated everything:
-  EXPECT_TRUE(segment->allMemoryDeallocated());
+    // Now, we have an deallocated everything:
+    EXPECT_TRUE(segment->allMemoryDeallocated());
 }
 
 } // namespace util
