c89c4b36594acb3f811acb793b8c4bb06e51900f
Mukund Sivaraman <muks@isc.org>
2012-07-06 07:41:09 +0530

    [2088] Add initial MemorySegment and MemorySegmentLocal classes
    These could very well not be the implementations we are looking
    for. From the bug report, it's not clear how these methods will be used:
    
    * Is deallocate() meant to free smaller blocks than what allocate()
      returns?
    
    * Because these methods will be virtual, a MemorySegment object is
      necessary. Will this be a singleton?
    
    * Does allMemoryDeallocated() need anything fancy like a set of all
      allocated buffers kept, or is just a counter of total size of memory
      allocated sufficient? (The counter is implemented.)

---
+++ src/lib/util/memory_segment_local.cc
@@ -0,0 +1,43 @@
+// Copyright (C) 2012  Internet Systems Consortium, Inc. ("ISC")
+//
+// Permission to use, copy, modify, and/or distribute this software for any
+// purpose with or without fee is hereby granted, provided that the above
+// copyright notice and this permission notice appear in all copies.
+//
+// THE SOFTWARE IS PROVIDED "AS IS" AND ISC DISCLAIMS ALL WARRANTIES WITH
+// REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF MERCHANTABILITY
+// AND FITNESS.  IN NO EVENT SHALL ISC BE LIABLE FOR ANY SPECIAL, DIRECT,
+// INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES WHATSOEVER RESULTING FROM
+// LOSS OF USE, DATA OR PROFITS, WHETHER IN AN ACTION OF CONTRACT, NEGLIGENCE
+// OR OTHER TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR
+// PERFORMANCE OF THIS SOFTWARE.
+
+#include "memory_segment_local.h"
+
+namespace isc {
+namespace util {
+
+void*
+MemorySegmentLocal::allocate(size_t size) {
+    void *ptr = malloc(size);
+
+    if (ptr != NULL) {
+        allocated_size_ += size;
+    }
+
+    return (ptr);
+}
+
+void
+MemorySegmentLocal::deallocate(void* ptr, size_t size) {
+    allocated_size_ -= size;
+    free(ptr);
+}
+
+bool
+MemorySegmentLocal::allMemoryDeallocated() const {
+    return (allocated_size_ == 0);
+}
+
+} // namespace util
+} // namespace isc
---
+++ src/lib/util/tests/memory_segment_local_unittest.cc
@@ -0,0 +1,56 @@
+// Copyright (C) 2012  Internet Systems Consortium, Inc. ("ISC")
+//
+// Permission to use, copy, modify, and/or distribute this software for any
+// purpose with or without fee is hereby granted, provided that the above
+// copyright notice and this permission notice appear in all copies.
+//
+// THE SOFTWARE IS PROVIDED "AS IS" AND ISC DISCLAIMS ALL WARRANTIES WITH
+// REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF MERCHANTABILITY
+// AND FITNESS.  IN NO EVENT SHALL ISC BE LIABLE FOR ANY SPECIAL, DIRECT,
+// INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES WHATSOEVER RESULTING FROM
+// LOSS OF USE, DATA OR PROFITS, WHETHER IN AN ACTION OF CONTRACT, NEGLIGENCE
+// OR OTHER TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR
+// PERFORMANCE OF THIS SOFTWARE.
+
+#include "util/memory_segment_local.h"
+#include <gtest/gtest.h>
+#include <memory>
+
+using namespace std;
+
+namespace isc {
+namespace util {
+
+TEST(MemorySegmentLocal, testDefault) {
+  auto_ptr<MemorySegment> segment(new MemorySegmentLocal());
+
+  // By default, nothing is allocated.
+  EXPECT_TRUE(segment->allMemoryDeallocated());
+
+  void *ptr = segment->allocate(1024);
+
+  // Now, we have an allocation:
+  EXPECT_FALSE(segment->allMemoryDeallocated());
+
+  void *ptr2 = segment->allocate(42);
+
+  // Still:
+  EXPECT_FALSE(segment->allMemoryDeallocated());
+
+  // These should not fail, because the buffers have been allocated.
+  EXPECT_NO_FATAL_FAILURE(memset(ptr, 0, 1024));
+  EXPECT_NO_FATAL_FAILURE(memset(ptr, 0, 42));
+
+  segment->deallocate(ptr, 1024);
+
+  // Still:
+  EXPECT_FALSE(segment->allMemoryDeallocated());
+
+  segment->deallocate(ptr2, 42);
+
+  // Now, we have an deallocated everything:
+  EXPECT_TRUE(segment->allMemoryDeallocated());
+}
+
+} // namespace util
+} // namespace isc
