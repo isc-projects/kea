56c424889a7203c96896039309531ca15c42e80e
JINMEI Tatuya <jinmei@isc.org>
2012-07-18 16:23:22 -0700

    [2094] complete test cases.  added a couple of RR types.

--- src/lib/datasrc/memory/rdata_encoder.h
+++ src/lib/datasrc/memory/rdata_encoder.h
@@ -15,6 +15,15 @@
 #ifndef DATASRC_MEMORY_RDATA_ENCODER_H
 #define DATASRC_MEMORY_RDATA_ENCODER_H 1
 
+#include <dns/labelsequence.h>
+#include <dns/rdata.h>
+#include <dns/rrclass.h>
+#include <dns/rrtype.h>
+
+#include <boost/function.hpp>
+
+#include <vector>
+
 namespace isc {
 namespace datasrc {
 namespace memory {
@@ -23,6 +32,19 @@ enum RdataNameAttributes {
     NAMEATTR_COMPRESSIBLE = 1,
     NAMEATTR_ADDITIONAL = (NAMEATTR_COMPRESSIBLE << 1)
 };
+
+void encodeRdata(const dns::rdata::Rdata& rdata, dns::RRClass rrclass,
+                 dns::RRType rrtype, std::vector<uint8_t>& data_result,
+                 std::vector<uint16_t>& len_result);
+
+typedef boost::function<void(const dns::LabelSequence&,
+                             RdataNameAttributes)> NameCallback;
+typedef boost::function<void(const uint8_t*, size_t)> DataCallback;
+
+void foreachRdataField(dns::RRClass rrclass, dns::RRType rrtype,
+                       const std::vector<uint8_t>& encoded_data,
+                       const std::vector<uint16_t>& varlen_list,
+                       NameCallback name_callback, DataCallback data_callback);
 
 } // namespace memory
 } // namespace datasrc
--- src/lib/datasrc/memory/tests/rdata_encoder_unittest.cc
+++ src/lib/datasrc/memory/tests/rdata_encoder_unittest.cc
@@ -12,20 +12,102 @@
 // OR OTHER TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR
 // PERFORMANCE OF THIS SOFTWARE.
 
+#include <dns/name.h>
+#include <dns/labelsequence.h>
+#include <dns/messagerenderer.h>
+#include <dns/rdata.h>
+#include <dns/rrclass.h>
+#include <dns/rrtype.h>
+
 #include <datasrc/memory/rdata_encoder.h>
+
+#include <util/unittests/wiredata.h>
 
 #include <gtest/gtest.h>
 
+#include <boost/bind.hpp>
+
+#include <set>
+#include <string>
+#include <vector>
+
+using namespace isc::dns;
+using namespace isc::dns::rdata;
 using namespace isc::datasrc::memory;
 
+using isc::util::unittests::matchWireData;
+using std::string;
+using std::vector;
+
 namespace {
-class RdataEncoderTest : public ::testing::Test {
-protected:
-    RdataEncoderTest() {}
+struct TestRdata {
+    const char* const rrclass;
+    const char* const rrtype;
+    const char* const rdata;
+};
+const TestRdata test_rdata_list[] = {
+    {"IN", "A", "192.0.2.1"},
+    {"IN", "NS", "ns.example.com"},
+    {NULL, NULL, NULL}
 };
 
-TEST_F(RdataEncoderTest, test1) {
-    EXPECT_NE(NAMEATTR_COMPRESSIBLE, NAMEATTR_ADDITIONAL);
+void
+renderNameField(MessageRenderer* renderer, bool additional_required,
+                const LabelSequence& labels, RdataNameAttributes attributes)
+{
+    EXPECT_EQ(additional_required,
+              (attributes & NAMEATTR_ADDITIONAL) != 0);
+    renderer->writeName(labels, (attributes & NAMEATTR_COMPRESSIBLE) != 0);
+}
+
+void
+renderDataField(MessageRenderer* renderer, const uint8_t* data,
+                size_t data_len)
+{
+    renderer->writeData(data, data_len);
+}
+
+TEST(RdataFieldSpec, checkData) {
+    const Name dummy_name("example.com");
+    MessageRenderer expected_renderer, actual_renderer;
+    vector<uint8_t> encoded_data;
+    vector<uint16_t> varlen_list;
+
+    std::set<RRType> need_additionals;
+    need_additionals.insert(RRType::NS());
+    need_additionals.insert(RRType::MX());
+    need_additionals.insert(RRType::SRV());
+
+    for (size_t i = 1; test_rdata_list[i].rrclass != NULL; ++i) {
+        SCOPED_TRACE(string(test_rdata_list->rrclass) + "/" +
+                     test_rdata_list->rrtype);
+
+        expected_renderer.clear();
+        actual_renderer.clear();
+        expected_renderer.writeName(dummy_name);
+        actual_renderer.writeName(dummy_name);
+
+        const RRClass rrclass(test_rdata_list[i].rrclass);
+        const RRType rrtype(test_rdata_list[i].rrtype);
+        const ConstRdataPtr rdata = createRdata(rrtype, rrclass,
+                                                test_rdata_list[i].rdata);
+        rdata->toWire(expected_renderer);
+
+        const bool additional_required =
+            (need_additionals.find(rrtype) != need_additionals.end());
+
+        encodeRdata(*rdata, rrclass, rrtype, encoded_data, varlen_list);
+        foreachRdataField(rrclass, rrtype, encoded_data, varlen_list,
+                          boost::bind(renderNameField, &actual_renderer,
+                                      additional_required, _1, _2),
+                          boost::bind(renderDataField, &actual_renderer,
+                                      _1, _2));
+
+        matchWireData(expected_renderer.getData(),
+                      expected_renderer.getLength(),
+                      actual_renderer.getData(),
+                      actual_renderer.getLength());
+    }
 }
 
 }
