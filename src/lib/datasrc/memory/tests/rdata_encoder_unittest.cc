2cdab2ad864e0e2843500904b3e6458354625d65
JINMEI Tatuya <jinmei@isc.org>
2012-07-18 23:17:07 -0700

    [2094] complete the spec data and test cases

--- src/lib/datasrc/memory/tests/rdata_encoder_unittest.cc
+++ src/lib/datasrc/memory/tests/rdata_encoder_unittest.cc
@@ -44,11 +44,45 @@ struct TestRdata {
     const char* const rrclass;
     const char* const rrtype;
     const char* const rdata;
+    const size_t n_varlen_fields; // expected # of variable-len fields
 };
 const TestRdata test_rdata_list[] = {
-    {"IN", "A", "192.0.2.1"},
-    {"IN", "NS", "ns.example.com"},
-    {NULL, NULL, NULL}
+    {"IN", "A", "192.0.2.1", 0},
+    {"IN", "NS", "ns.example.com", 0},
+    {"IN", "CNAME", "cname.example.com", 0},
+    {"IN", "SOA", "ns.example.com root.example.com 0 0 0 0 0", 0},
+    {"IN", "PTR", "reverse.example.com", 0},
+    {"IN", "HINFO", "\"cpu-info\" \"OS-info\"", 1},
+    {"IN", "MINFO", "root.example.com mbox.example.com", 0},
+    {"IN", "MX", "10 mx.example.com", 0},
+    {"IN", "TXT", "\"test1\" \"test 2\"", 1},
+    {"IN", "RP", "root.example.com. rp-text.example.com", 0},
+    {"IN", "AFSDB", "1 afsdb.example.com", 0},
+    {"IN", "AAAA", "2001:db8::1", 0},
+    {"IN", "SRV", "1 0 10 target.example.com", 0},
+    {"IN", "NAPTR", "100 50 \"s\" \"http\" \"\" _http._tcp.example.com", 1},
+    {"IN", "DNAME", "dname.example.com", 0},
+    {"IN", "DS", "12892 5 2 5F0EB5C777586DE18DA6B5", 1},
+    {"IN", "SSHFP", "1 1 dd465c09cfa51fb45020cc83316fff", 1},
+    // Note: in our implementation RRSIG is treated as opaque data (including
+    // the signer name).  We use "com" for signer so it won't be a compress
+    // target in the test.
+    {"IN", "RRSIG", "SOA 5 2 3600 20120814220826 20120715220826 12345 "
+     "com. FAKEFAKEFAKE", 1},
+    {"IN", "NSEC", "next.example.com. A AAAA NSEC RRSIG", 1},
+    {"IN", "DNSKEY", "256 3 5 FAKEFAKE", 1},
+    {"IN", "DHCID", "FAKEFAKE", 1},
+    {"IN", "NSEC3", "1 1 12 AABBCCDD FAKEFAKE A RRSIG", 1},
+    {"IN", "NSEC3PARAM", "1 0 12 AABBCCDD", 1},
+    {"IN", "SPF", "v=spf1 +mx a:colo.example.com/28 -all", 1},
+    {"IN", "DLV", "12892 5 2 5F0EB5C777586DE18DA6B5", 1},
+    {"IN", "TYPE65000", "\\# 3 010203", 1}, // some "custom" type
+    {"IN", "TYPE65535", "\\# 0", 1},        // max RR type, 0-length RDATA
+    {"CH", "A", "\\# 2 0102", 1}, // A RR for non-IN class; varlen data
+    {"CH", "NS", "ns.example.com", 0}, // class CH, generic data
+    {"CH", "TXT", "BIND10", 1},        // ditto
+    {"HS", "A", "\\# 5 0102030405", 1}, // A RR for non-IN class; varlen data
+    {NULL, NULL, NULL, 0}
 };
 
 void
@@ -68,7 +102,8 @@ renderDataField(MessageRenderer* renderer, const uint8_t* data,
 }
 
 TEST(RdataFieldSpec, checkData) {
-    const Name dummy_name("example.com");
+    const Name dummy_name("com");
+    const Name dummy_name2("example.com");
     MessageRenderer expected_renderer, actual_renderer;
     vector<uint8_t> encoded_data;
     vector<uint16_t> varlen_list;
@@ -79,30 +114,35 @@ TEST(RdataFieldSpec, checkData) {
     need_additionals.insert(RRType::SRV());
 
     for (size_t i = 1; test_rdata_list[i].rrclass != NULL; ++i) {
-        SCOPED_TRACE(string(test_rdata_list->rrclass) + "/" +
-                     test_rdata_list->rrtype);
+        SCOPED_TRACE(string(test_rdata_list[i].rrclass) + "/" +
+                     test_rdata_list[i].rrtype);
 
         expected_renderer.clear();
         actual_renderer.clear();
         expected_renderer.writeName(dummy_name);
         actual_renderer.writeName(dummy_name);
+        encoded_data.clear();
+        varlen_list.clear();
 
         const RRClass rrclass(test_rdata_list[i].rrclass);
         const RRType rrtype(test_rdata_list[i].rrtype);
         const ConstRdataPtr rdata = createRdata(rrtype, rrclass,
                                                 test_rdata_list[i].rdata);
         rdata->toWire(expected_renderer);
+        expected_renderer.writeName(dummy_name2);
 
         const bool additional_required =
             (need_additionals.find(rrtype) != need_additionals.end());
 
         encodeRdata(*rdata, rrclass, rrtype, encoded_data, varlen_list);
+        EXPECT_EQ(varlen_list.size(), test_rdata_list[i].n_varlen_fields);
         foreachRdataField(rrclass, rrtype, encoded_data, varlen_list,
                           boost::bind(renderNameField, &actual_renderer,
                                       additional_required, _1, _2),
                           boost::bind(renderDataField, &actual_renderer,
                                       _1, _2));
 
+        actual_renderer.writeName(dummy_name2);
         matchWireData(expected_renderer.getData(),
                       expected_renderer.getLength(),
                       actual_renderer.getData(),
