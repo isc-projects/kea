a74df58679549545f04310506d819d4a92da6500
Tomek Mrugalski <tomasz@isc.org>
2012-08-20 15:40:37 +0200

    [2040] All benchmark classes are now documented.

--- tests/tools/dhcp-ubench/sqlite_ubench.h
+++ tests/tools/dhcp-ubench/sqlite_ubench.h
@@ -15,22 +15,60 @@
 #include <string>
 #include "benchmark.h"
 
+/// @brief SQLite benchmark
+///
+/// That is a specific backend implementation. See \ref uBenchmark class for
+/// detailed explanation of its operations. This class uses SQLite as DB backend.
 class SQLite_uBenchmark: public uBenchmark {
 public:
+
+    /// @brief The sole SQL benchmark constructor
+    ///
+    /// DB file must be present and appropriate database schema must
+    /// be used. See sqlite.schema script and isc-dhcp-perf-guide.html
+    /// for details.
+    ///
+    /// sync flag affects "PRAGMA synchronous" to be ON or OFF.
+    ///
+    /// @param filename name of the SQLite DB file. Must be present.
+    /// @param num_iterations number of iterations of basic lease operations
+    /// @param sync should the operations be synchronous or not?
+    /// @param verbose would you like extra details be logged?
     SQLite_uBenchmark(const std::string& filename,
                       uint32_t num_iterations,
                       bool sync, bool verbose);
 
+    /// @brief Prints SQLite version info.
     virtual void printInfo();
+
+    /// @brief Opens connection to the SQLite database.
     virtual void connect();
+
+    /// @brief Closes connection to the SQLite database.
     virtual void disconnect();
+
+    /// @brief Creates new leases.
+    ///
+    /// See uBenchmark::createLease4Test() for detailed explanation.
     virtual void createLease4Test();
+
+    /// @brief Searches for existing leases.
+    ///
+    /// See uBenchmark::searchLease4Test() for detailed explanation.
     virtual void searchLease4Test();
+
+    /// @brief Updates existing leases.
+    ///
+    /// See uBenchmark::updateLease4Test() for detailed explanation.
     virtual void updateLease4Test();
+
+    /// @brief Deletes existing leases.
+    ///
+    /// See uBenchmark::deleteLease4Test() for detailed explanation.
     virtual void deleteLease4Test();
 
 protected:
-    void failure(const char* operation);
 
+    /// Handle to SQLite database connection.
     sqlite3 *DB_;
 };
