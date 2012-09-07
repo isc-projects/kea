1bcc747a1b8114e4f94ab171647ff76c0eccd6f2
Jelte Jansen <jelte@isc.org>
2012-09-07 17:20:28 +0200

    [2151] add well-known static wildcard labelsequence

--- src/lib/dns/tests/labelsequence_unittest.cc
+++ src/lib/dns/tests/labelsequence_unittest.cc
@@ -1174,4 +1174,10 @@ TEST_F(ExtendableLabelSequenceTest, extendBadData) {
     check_equal(full_ls2, els);
 }
 
+// Check the static fixed 'wildcard' LabelSequence
+TEST(WildCardLabelSequence, wildcard) {
+    ASSERT_FALSE(LabelSequence::WILDCARD_LABEL().isAbsolute());
+    ASSERT_EQ("*", LabelSequence::WILDCARD_LABEL().toText());
+}
+
 }
