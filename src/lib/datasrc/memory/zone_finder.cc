7f32771af9a9b350a9247700bcc4e5b2915f01d5
JINMEI Tatuya <jinmei@isc.org>
2012-09-20 10:44:24 -0700

    [master] cleanup: removed dead code.
    I suspect it was a leftover of an intermediate attempt that ended up with
        found = RdataSet::find(node->getData(), type);
    at commi 32f6a873.
    
    Whatever the intent was, it's clear the code does nothing, so cleaning it
    up shouldn't harm anyway.
    
    okayed on jabber.

--- src/lib/datasrc/memory/zone_finder.cc
+++ src/lib/datasrc/memory/zone_finder.cc
@@ -561,10 +561,6 @@ InMemoryZoneFinder::find_internal(const isc::dns::Name& name,
                                  wild, &name));
     }
 
-    const RdataSet* currds = node->getData();
-    while (currds != NULL) {
-        currds = currds->getNext();
-    }
     found = RdataSet::find(node->getData(), type);
     if (found != NULL) {
         // Good, it is here
