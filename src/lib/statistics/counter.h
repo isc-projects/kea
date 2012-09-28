c76e3fc072e8b1b771b55b6547889863ba263f31
Yoshitaka Aharen <aharen@jprs.co.jp>
2012-09-28 19:03:52 +0900

    [2155] inlining isc::statistics::Counter
    make isc::statistics::Counter and isc::statistics::CounterDict
    inline for performance

--- src/lib/statistics/counter.h
+++ src/lib/statistics/counter.h
@@ -15,24 +15,29 @@
 #ifndef __COUNTER_H
 #define __COUNTER_H 1
 
+#include <exceptions/exceptions.h>
+
 #include <boost/noncopyable.hpp>
 #include <boost/scoped_ptr.hpp>
 
-#include <exceptions/exceptions.h>
+#include <vector>
+
+namespace {
+const unsigned int InitialValue = 0;
+} // anonymous namespace
 
 namespace isc {
 namespace statistics {
 
-// forward declaration for pImpl idiom
-class CounterImpl;
-
 class Counter : boost::noncopyable {
-private:
-    boost::scoped_ptr<CounterImpl> impl_;
 public:
     typedef unsigned int Type;
     typedef unsigned int Value;
 
+private:
+    std::vector<Counter::Value> counters_;
+
+public:
     /// The constructor.
     ///
     /// This constructor is mostly exception free. But it may still throw
@@ -41,29 +46,56 @@ public:
     /// \param items A number of counter items to hold (greater than 0)
     ///
     /// \throw isc::InvalidParameter \a items is 0
-    Counter(const size_t items);
+    explicit inline Counter(const size_t items);
 
     /// The destructor.
     ///
     /// This method never throws an exception.
-    ~Counter();
+    inline ~Counter();
 
     /// \brief Increment a counter item specified with \a type.
     ///
     /// \param type %Counter item to increment
     ///
     /// \throw isc::OutOfRange \a type is invalid
-    void inc(const Type& type);
+    inline void inc(const Counter::Type);
 
     /// \brief Get the value of a counter item specified with \a type.
     ///
     /// \param type %Counter item to get the value of
     ///
     /// \throw isc::OutOfRange \a type is invalid
-    const Value& get(const Type& type) const;
+    inline const Counter::Value& get(const Counter::Type) const;
 };
+
+inline Counter::Counter(const size_t items) :
+    counters_(items, InitialValue)
+{
+    if (items == 0) {
+        isc_throw(isc::InvalidParameter, "Items must not be 0");
+    }
+}
+
+inline Counter::~Counter() {}
+
+inline void
+Counter::inc(const Counter::Type type) {
+    if(type >= counters_.size()) {
+        isc_throw(isc::OutOfRange, "Counter type is out of range");
+    }
+    ++counters_.at(type);
+    return;
+}
+
+inline const Counter::Value&
+Counter::get(const Counter::Type type) const {
+    if(type >= counters_.size()) {
+        isc_throw(isc::OutOfRange, "Counter type is out of range");
+    }
+    return (counters_.at(type));
+}
 
 }   // namespace statistics
 }   // namespace isc
 
-#endif
+#endif // __COUNTER_H
