8e9048d42d17662247bf8a6a6ba9912535125f74
Tomek Mrugalski <tomasz@isc.org>
2012-10-01 18:05:06 +0200

    [master] Added missing header (Solaris compilation fix)

--- src/lib/dhcp/addr_utilities.cc
+++ src/lib/dhcp/addr_utilities.cc
@@ -12,6 +12,7 @@
 // OR OTHER TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR
 // PERFORMANCE OF THIS SOFTWARE.
 
+#include <string.h>
 #include <dhcp/addr_utilities.h>
 
 using namespace isc::asiolink;
