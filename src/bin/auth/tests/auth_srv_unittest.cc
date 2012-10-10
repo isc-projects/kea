9b02fc549c12601513ee0bae1a481953435475e7
Yoshitaka Aharen <aharen@jprs.co.jp>
2012-10-10 19:34:26 +0900

    [2155] fixed wrong comment

--- src/bin/auth/tests/auth_srv_unittest.cc
+++ src/bin/auth/tests/auth_srv_unittest.cc
@@ -1089,7 +1089,7 @@ TEST_F(AuthSrvTest, queryCounterUDPNormal) {
     server.processMessage(*io_message, *parse_message, *response_obuffer,
                           &dnsserv);
     // After processing the UDP query, these counters should be incremented:
-    //   request.tcp, opcode.query, qtype.ns, rcode.refused, response
+    //   request.udp, opcode.query, qtype.ns, rcode.refused, response
     // and these counters should not be incremented:
     //   request.tcp
     ConstElementPtr stats_after = server.getStatistics();
