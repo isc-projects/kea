45b74ff3e85ad3b48afe3027565d21321976e748
Yoshitaka Aharen <aharen@jprs.co.jp>
2012-10-10 14:14:46 +0900

    [2157] fixed systest

--- tests/system/bindctl/tests.sh
+++ tests/system/bindctl/tests.sh
@@ -25,9 +25,9 @@ status=0
 n=0
 
 # TODO: consider consistency with statistics definition in auth.spec
-cnt_name1="\<request\.tcp\>"
-cnt_name2="\<request\.udp\>"
-cnt_name3="\<opcode\.query\>"
+cnt_name1="\<tcp\>"
+cnt_name2="\<udp\>"
+cnt_name3="\<query\>"
 cnt_value1=0
 cnt_value2=0
 cnt_value3=0
