ee91de7e042ad182c76badec497c6b0c7902e654
JINMEI Tatuya <jinmei@isc.org>
2012-10-10 20:09:58 -0700

    [2332] supported wait() and signal()

--- src/lib/util/threads/tests/condvar_unittest.cc
+++ src/lib/util/threads/tests/condvar_unittest.cc
@@ -13,9 +13,16 @@
 // PERFORMANCE OF THIS SOFTWARE.
 
 #include <util/threads/lock.h>
-#include <util/threads/condvar.h>
+#include <util/threads/thread.h>
 
 #include <gtest/gtest.h>
+
+#include <boost/bind.hpp>
+
+#include <cstring>
+
+#include <unistd.h>
+#include <signal.h>
 
 using namespace isc::util::thread;
 
@@ -23,6 +30,46 @@ namespace {
 TEST(CondVarTest, create) {
     // Just construct and destruct it.  Nothing unusual should happen.
     EXPECT_NO_THROW(CondVar condvar);
+}
+
+// Running on a separate thread, just updating the argument and waking up
+// the other thread via the condition variable passed.
+void
+ringSignal(CondVar* condvar, Mutex* mutex, bool* arg) {
+    assert(*arg == false);
+    Mutex::Locker locker(*mutex);
+    *arg = true;
+    condvar->signal();
+}
+
+void
+no_handler(int) {}
+
+// A simple wait-signal operation on a condition variable.
+TEST(CondVarTest, waitAndSignal) {
+    CondVar condvar;
+    Mutex mutex;
+
+    struct sigaction ignored, original;
+    std::memset(&ignored, 0, sizeof(ignored));
+    ignored.sa_handler = no_handler;
+    if (sigaction(SIGALRM, &ignored, &original) != 0) {
+        FAIL() << "Couldn't set alarm";
+    }
+    alarm(10);                  // 10sec duration: arbitrary choice
+
+    Mutex::Locker locker(mutex);
+    bool must_become_true = false; // let the other thread reset it to true
+    Thread t(boost::bind(&ringSignal, &condvar, &mutex, &must_become_true));
+    condvar.wait(mutex);
+    t.wait();
+    EXPECT_TRUE(must_become_true);
+
+    // Cancel the alarm and return the original handler
+    alarm(0);
+    if (sigaction(SIGALRM, &original, NULL)) {
+        FAIL() << "Couldn't restore alarm";
+    }
 }
 
 }
