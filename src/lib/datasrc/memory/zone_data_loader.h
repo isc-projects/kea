7e2a223f1ebb11581fad278dc1356aa8dd7dc695
Mukund Sivaraman <muks@isc.org>
2012-10-12 09:54:54 +0530

    [2268] Move ZoneDataLoader class out of its header

--- src/lib/datasrc/memory/zone_data_loader.h
+++ src/lib/datasrc/memory/zone_data_loader.h
@@ -15,70 +15,20 @@
 #ifndef DATASRC_ZONE_DATA_LOADER_H
 #define DATASRC_ZONE_DATA_LOADER_H 1
 
-#include <datasrc/memory/zone_data_updater.h>
-
 #include <datasrc/memory/zone_data.h>
 #include <datasrc/iterator.h>
 #include <dns/name.h>
 #include <dns/rrclass.h>
-#include <dns/rrset.h>
 #include <util/memory_segment.h>
-
-#include <boost/noncopyable.hpp>
-#include <map>
 
 namespace isc {
 namespace datasrc {
 namespace memory {
 
-// A helper internal class for load().  make it non-copyable to avoid
-// accidental copy.
-//
-// The current internal implementation expects that both a normal
-// (non RRSIG) RRset and (when signed) its RRSIG are added at once.
-// Also in the current implementation, the input sequence of RRsets
-// are grouped with their owner name (so once a new owner name is encountered,
-// no subsequent RRset has the previous owner name), but the ordering
-// in the same group is not fixed.  So we hold all RRsets of the same
-// owner name in node_rrsets_ and node_rrsigsets_, and add the matching
-// pairs of RRsets to the zone when we see a new owner name.
-//
-// The caller is responsible for adding the RRsets of the last group
-// in the input sequence by explicitly calling flushNodeRRsets() at the
-// end.  It's cleaner and more robust if we let the destructor of this class
-// do it, but since we cannot guarantee the adding operation is exception free,
-// we don't choose that option to maintain the common expectation for
-// destructors.
-class ZoneDataLoader : boost::noncopyable {
-public:
-    ZoneDataLoader(util::MemorySegment& mem_sgmt,
-                   const isc::dns::RRClass rrclass,
-                   const isc::dns::Name& zone_name, ZoneData& zone_data) :
-        updater_(mem_sgmt, rrclass, zone_name, zone_data)
-    {}
-
-    void addFromLoad(const isc::dns::ConstRRsetPtr& rrset);
-    void flushNodeRRsets();
-
-private:
-    typedef std::map<isc::dns::RRType, isc::dns::ConstRRsetPtr> NodeRRsets;
-    typedef NodeRRsets::value_type NodeRRsetsVal;
-
-    // A helper to identify the covered type of an RRSIG.
-    static isc::dns::RRType getCoveredType
-        (const isc::dns::ConstRRsetPtr& sig_rrset);
-    const isc::dns::Name& getCurrentName() const;
-
-private:
-    NodeRRsets node_rrsets_;
-    NodeRRsets node_rrsigsets_;
-    ZoneDataUpdater updater_;
-};
-
 /// \brief Zone is empty exception.
 ///
-/// This is thrown if we have an empty zone created as a result of
-/// load().
+/// This is thrown if we have an empty zone created during
+/// \c loadZoneData().
 struct EmptyZone : public InvalidParameter {
     EmptyZone(const char* file, size_t line, const char* what) :
         InvalidParameter(file, line, what)
