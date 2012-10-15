155f1f8c6d79064e86f64a8a0002155786762b36
Michal 'vorner' Vaner <michal.vaner@nic.cz>
2012-10-15 12:14:04 +0200

    [2207] Implementation of the getZoneReloader
    The concrete version for local table segment and reloader.

--- src/lib/datasrc/memory/zone_table_segment_local.cc
+++ src/lib/datasrc/memory/zone_table_segment_local.cc
@@ -13,6 +13,7 @@
 // PERFORMANCE OF THIS SOFTWARE.
 
 #include <datasrc/memory/zone_table_segment_local.h>
+#include "zone_reloader.h"
 
 using namespace isc::util;
 
@@ -36,6 +37,14 @@ ZoneTableSegmentLocal::getHeader() const {
 MemorySegment&
 ZoneTableSegmentLocal::getMemorySegment() {
      return (mem_sgmt_);
+}
+
+ZoneReloader*
+ZoneTableSegmentLocal::getZoneReloader(const LoadAction& load_action,
+                                       const dns::Name& name,
+                                       const dns::RRClass& rrclass)
+{
+    return (new ZoneReloaderLocal(this, load_action, name, rrclass));
 }
 
 } // namespace memory
