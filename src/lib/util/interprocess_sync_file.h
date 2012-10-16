eaf183dce742edd479fda9f77a9333b468c26104
Mukund Sivaraman <muks@isc.org>
2012-10-17 00:17:08 +0530

    [master] Avoid static destruction fiasco with InterprocessSyncFile and logger

--- src/lib/util/interprocess_sync_file.h
+++ src/lib/util/interprocess_sync_file.h
@@ -21,6 +21,9 @@
 
 #include <boost/shared_ptr.hpp>
 
+#include <map>
+#include <string>
+
 namespace isc {
 namespace util {
 
@@ -81,12 +84,16 @@ protected:
     bool unlock();
 
 private:
+    typedef boost::shared_ptr<isc::util::thread::Mutex> MutexPtr;
+    typedef boost::shared_ptr<isc::util::thread::Mutex::Locker> LockerPtr;
+    typedef std::map<std::string, boost::weak_ptr<isc::util::thread::Mutex> >
+        SyncMap;
+
+    SyncMap& getSyncMap();
+    isc::util::thread::Mutex& getSyncMapMutex();
     bool do_lock(int cmd, short l_type);
 
     int fd_; ///< The descriptor for the open file
-
-    typedef boost::shared_ptr<isc::util::thread::Mutex> MutexPtr;
-    typedef boost::shared_ptr<isc::util::thread::Mutex::Locker> LockerPtr;
     MutexPtr mutex_;   ///< A mutex for mutual exclusion among threads
     LockerPtr locker_; ///< A locker on mutex_
 };
