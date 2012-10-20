93ffcbb3ecdb4f882bb833addb68908446f7ae77
Stephen Morris <stephen@isc.org>
2012-10-20 22:52:25 +0100

    [2342] Now have addLease and getLease6 methods apparently working

--- src/lib/dhcp/tests/mysql_lease_mgr_unittest.cc
+++ src/lib/dhcp/tests/mysql_lease_mgr_unittest.cc
@@ -16,6 +16,7 @@
 #include <iostream>
 #include <sstream>
 #include <utility>
+#include <string>
 #include <gtest/gtest.h>
 
 #include <asiolink/io_address.h>
@@ -28,12 +29,6 @@ using namespace isc::dhcp;
 using namespace std;
 
 namespace {
-// empty class for now, but may be extended later.
-class MySqlLeaseMgrTest : public ::testing::Test {
-public:
-    MySqlLeaseMgrTest() {
-    }
-};
 
 // Connection strings
 const char* VALID_TYPE = "type=mysql";
@@ -61,13 +56,42 @@ string validConnectionString() {
                              VALID_USER, VALID_PASSWORD));
 }
 
+/// @brief Test Fixture Class
+///
+/// Opens the database prior to each test and closes it afterwards.
+/// All pending transactions are deleted prior to closure.
 
-// This test checks if the MySqlLeaseMgr can be instantiated.  This happens
-// only if:
-// a) The database can be opened
-// b) It finds the schema_version table
-// c) The schema version matches that in the Lease Manager code
-TEST_F(MySqlLeaseMgrTest, OpenDatabase) {
+class MySqlLeaseMgrTest : public ::testing::Test {
+public:
+    /// @brief Constructor
+    ///
+    /// Open the database.
+
+    MySqlLeaseMgrTest() {
+        lmptr_ = LeaseMgrFactory::create(validConnectionString());
+    }
+
+    /// @brief Destructor
+    ///
+    /// Rolls back all pending transactions.  The deletion of the
+    /// lmptr_ member variable will close the database.
+
+    virtual ~MySqlLeaseMgrTest() {
+        //lmptr_->rollback();
+    }
+
+    LeaseMgrPtr lmptr_;     // Pointer to the lease manager
+};
+
+
+/// @brief Check that Database Can Be Opened
+///
+/// This test checks if the MySqlLeaseMgr can be instantiated.  This happens
+/// only if the database can be opened.  Note that this is not part of the
+/// MySqlLeaseMgr test fixure set.  This test checks that the database can be
+/// opened: the fixtures assume that and check basic operations.
+
+TEST(MySqlOpenTest, OpenDatabase) {
     LeaseMgrPtr lmptr;
 
     // Check that wrong specification of backend throws an exception.
@@ -92,22 +116,156 @@ TEST_F(MySqlLeaseMgrTest, OpenDatabase) {
         DbOpenError);
 
     // Check that database opens correctly and that version is as expected
-    ASSERT_NO_THROW(lmptr = LeaseMgrFactory::create(connectionString(
-        VALID_TYPE, VALID_NAME, VALID_HOST, VALID_USER, VALID_PASSWORD)));
+    ASSERT_NO_THROW(lmptr = LeaseMgrFactory::create(validConnectionString()));
     ASSERT_TRUE(lmptr);
 }
 
-TEST_F(MySqlLeaseMgrTest, CheckVersion) {
-    // Open database
-    LeaseMgrPtr lmptr;
-    ASSERT_NO_THROW(lmptr = LeaseMgrFactory::create(connectionString(
-        VALID_TYPE, VALID_NAME, VALID_HOST, VALID_USER, VALID_PASSWORD)));
-    ASSERT_TRUE(lmptr);
+/// @brief Check conversion functions
+TEST_F(MySqlLeaseMgrTest, CheckTimeConversion) {
+    const time_t cltt = time(NULL);
+    const uint32_t valid_lft = 86400;       // 1 day
+    MYSQL_TIME expire;
+    uint32_t lease_time;
 
+    MySqlLeaseMgr::convertFromLeaseTime(cltt, valid_lft, expire, lease_time);
+    EXPECT_EQ(valid_lft, lease_time);
+    EXPECT_LE(2012, expire.year);       // Code was written in 2012
+    EXPECT_EQ(0, expire.second_part);
+    EXPECT_EQ(0, expire.neg);
+
+    // Convert back
+    time_t converted_cltt = 0;
+    uint32_t converted_valid_lft = 0;
+    MySqlLeaseMgr::convertToLeaseTime(expire, lease_time, converted_cltt,
+                                      converted_valid_lft);
+    EXPECT_EQ(cltt, converted_cltt);
+    EXPECT_EQ(valid_lft, converted_valid_lft);
+}
+
+/// @brief Check that getVersion() works
+TEST_F(MySqlLeaseMgrTest, CheckVersion) {
     // Check version
-    pair<uint32_t, uint32_t> version = lmptr->getVersion();
+    pair<uint32_t, uint32_t> version;
+    ASSERT_NO_THROW(version = lmptr_->getVersion());
     EXPECT_EQ(0, version.first);
     EXPECT_EQ(1, version.second);
+}
+
+
+/// @brief Compare Lease4 Structure
+bool
+compareLease6(const Lease6Ptr& l1, const Lease6Ptr& l2) {
+    return (
+        l1->type_ == l2->type_ &&
+        l1->addr_ == l2->addr_ &&
+        l1->prefixlen_ == l2->prefixlen_ &&
+        l1->iaid_ == l2->iaid_ &&
+        l1->hwaddr_ == l2->hwaddr_ &&
+        *l1->duid_ == *l2->duid_ &&
+        l1->preferred_lft_ == l2->preferred_lft_ &&
+        l1->valid_lft_ == l2->valid_lft_ &&
+        l1->cltt_ == l2->cltt_ &&
+        l1->subnet_id_ == l2->subnet_id_
+        );
+}
+
+void
+detailCompareLease6(const Lease6Ptr& l1, const Lease6Ptr& l2) {
+    EXPECT_EQ(l1->type_, l2->type_);
+    EXPECT_EQ(l1->addr_, l2->addr_);
+    EXPECT_EQ(l1->prefixlen_, l2->prefixlen_);
+    EXPECT_EQ(l1->iaid_, l2->iaid_);
+    EXPECT_TRUE(l1->hwaddr_ == l2->hwaddr_);
+    EXPECT_TRUE(*l1->duid_ == *l2->duid_);
+    EXPECT_EQ(l1->preferred_lft_, l2->preferred_lft_);
+    EXPECT_EQ(l1->valid_lft_, l2->valid_lft_);
+    EXPECT_EQ(l1->cltt_, l2->cltt_);
+    EXPECT_EQ(l1->subnet_id_, l2->subnet_id_);
+}
+
+/// @brief Initialize Lease
+///
+/// Initializes the unused fields in a lease to known values for
+/// testing purposes.
+void initializeUnusedLease6(Lease6Ptr& lease) {
+    lease->t1_ = 0;                             // Not saved
+    lease->t2_ = 0;                             // Not saved
+    lease->fixed_ = false;                      // Unused
+    lease->hostname_ = std::string("");         // Unused
+    lease->fqdn_fwd_ = false;                   // Unused
+    lease->fqdn_rev_ = false;                   // Unused
+    lease->comments_ = std::string("");         // Unused
+}
+
+/// @brief Check individual Lease6 methods
+///
+/// Checks that the add/update/delete works.  All are done within one
+/// test so that "rollback" can be used to remove trace of the tests
+/// from the database.
+///
+/// Tests where a collection of leases can be returned are in the test
+/// Lease6Collection.
+TEST_F(MySqlLeaseMgrTest, BasicLease6) {
+
+    // Define the leases being used for testing.
+    const IOAddress L1_ADDRESS(std::string("2001:db8::1"));
+    Lease6Ptr l1(new Lease6());
+    initializeUnusedLease6(l1);
+
+    l1->type_ = Lease6::LEASE_IA_TA;
+    l1->addr_ = L1_ADDRESS;
+    l1->prefixlen_ = 0;
+    l1->iaid_ = 42;
+    l1->hwaddr_ = std::vector<uint8_t>(6, 0x42);     // Six hex 42's
+    l1->duid_ = boost::shared_ptr<DUID>(new DUID(vector<uint8_t>(8, 0x42)));
+    l1->preferred_lft_ = 3600;  // Preferred lifetime
+    l1->valid_lft_ = 3600;      // Actual lifetime
+    l1->cltt_ = 123456;         // Current time of day
+    l1->subnet_id_ = 73;        // Arbitrary number
+
+    const IOAddress L2_ADDRESS(std::string("2001:db8::2"));
+    Lease6Ptr l2(new Lease6());
+    initializeUnusedLease6(l2);
+
+    l2->type_ = Lease6::LEASE_IA_TA;
+    l2->addr_ = L1_ADDRESS;
+    l2->prefixlen_ = 0;
+    l2->iaid_ = 89;
+    l2->hwaddr_ = std::vector<uint8_t>(6, 0xf43);     // Six hex 42's
+    l2->duid_ = boost::shared_ptr<DUID>(new DUID(vector<uint8_t>(8, 0x3a)));
+    l2->preferred_lft_ = 1800;  // Preferred lifetime
+    l2->valid_lft_ = 5400;      // Actual lifetime
+    l2->cltt_ = 234567;         // Current time of day
+    l2->subnet_id_ = l1->subnet_id_;    // Same as l1
+
+    // Sanity check that the leases are different
+    ASSERT_FALSE(compareLease6(l1, l2));
+
+    // Start the tests.  Add the first lease to the database.  Then read it
+    // back to see whether it is what we think it is.
+    Lease6Ptr l_returned;
+
+    ASSERT_TRUE(lmptr_->addLease(l1));
+    l_returned = lmptr_->getLease6(L1_ADDRESS);
+    EXPECT_TRUE(l_returned);
+    detailCompareLease6(l1, l_returned);
+/*
+    // Delete the lease and check that it has been deleted.
+    EXPECT_TRUE(lmptr_->deleteLease6(L1_ADDRESS));
+    l_returned = lmptr_->getLease6(L1_ADDRESS);
+    EXPECT_FALSE(l_returned);
+
+    // Add the address again and check that we can't add it a second time
+    ASSERT_TRUE(lmptr_->addLease(l1));
+    ASSERT_FALSE(lmptr_->addLease(l1));
+
+    // Add the second lease
+    ASSERT_TRUE(lmptr_->addLease(l2));
+
+    // Finally, delete the lease and check we can't delete it again.
+    EXPECT_TRUE(lmptr_->deleteLease6(L1_ADDRESS));
+    EXPECT_FALSE(lmptr_->deleteLease6(L1_ADDRESS));
+    */
 }
 
 }; // end of anonymous namespace
