b4b800d0ce3b8d0bbc21b4332a26ca5e8265fa1c
Michal 'vorner' Vaner <michal.vaner@nic.cz>
2012-10-23 17:00:29 +0200

    [2209] Hack the ZoneTableSegment into MemoryClient
    This branch needs the MemoryClient to have its Zonetablesegment. But it
    doesn't have one now and it is not the task of this branch to put it in
    there. So I added it inside in somewhat ad-hoc manner.
    
    This is hairy and adds bunch of temporary methods. If there's a better
    approach, let's use it. But taking this for now to be cleaned up in some
    follow-up ticket.

--- src/lib/datasrc/memory/zone_table_segment.h
+++ src/lib/datasrc/memory/zone_table_segment.h
@@ -121,6 +121,18 @@ public:
     /// \return Returns a ZoneTableSegment object
     static ZoneTableSegment* create(const isc::data::Element& config);
 
+    /// \brief Temporary/Testing version of create.
+    ///
+    /// This exists as a temporary solution during the migration phase
+    /// towards using the ZoneTableSegment. It doesn't take a config,
+    /// but a memory segment instead. If you can, you should use the
+    /// other version, this one will be gone soon.
+    ///
+    /// \param segment The memory segment to use.
+    /// \return Returns a new ZoneTableSegment object.
+    /// \todo Remove this method.
+    static ZoneTableSegment* create(isc::util::MemorySegment& segment);
+
     /// \brief Destroy a ZoneTableSegment
     ///
     /// This method destroys the passed ZoneTableSegment. It must be
