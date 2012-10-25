49cebddf55b71e7ddac8ee30518dfc9c13dcff66
Yoshitaka Aharen <aharen@jprs.co.jp>
2012-10-25 20:36:08 +0900

    [2155] removed inline keywords from methods inside class definition

--- src/bin/auth/statistics.h
+++ src/bin/auth/statistics.h
@@ -62,7 +62,7 @@ public:
     /// This constructor is mostly exception free. But it may still throw
     /// a standard exception if memory allocation fails inside the method.
     ///
-    inline QRAttributes() :
+    QRAttributes() :
     req_ip_version_(0), req_transport_protocol_(0),
     req_opcode_(0),
     req_is_edns_0_(false), req_is_edns_badver_(false),
@@ -77,36 +77,36 @@ public:
     ///
     /// This method never throws an exception.
     ///
-    inline ~QRAttributes() {};
+    ~QRAttributes() {};
     /// \brief Set query opcode.
     /// \throw None
-    inline void setQueryOpCode(const int opcode) {
+    void setQueryOpCode(const int opcode) {
         req_opcode_ = opcode;
     };
     /// \brief Set IP version carrying a query.
     /// \throw None
-    inline void setQueryIPVersion(const int ip_version) {
+    void setQueryIPVersion(const int ip_version) {
         req_ip_version_ = ip_version;
     };
     /// \brief Set transport protocol carrying a query.
     /// \throw None
-    inline void setQueryTransportProtocol(const int transport_protocol) {
+    void setQueryTransportProtocol(const int transport_protocol) {
         req_transport_protocol_ = transport_protocol;
     };
     /// \brief Set query EDNS attributes.
     /// \throw None
-    inline void setQueryEDNS(const bool is_edns_0, const bool is_edns_badver) {
+    void setQueryEDNS(const bool is_edns_0, const bool is_edns_badver) {
         req_is_edns_0_ = is_edns_0;
         req_is_edns_badver_ = is_edns_badver;
     };
     /// \brief Set query DO bit.
     /// \throw None
-    inline void setQueryDO(const bool is_dnssec_ok) {
+    void setQueryDO(const bool is_dnssec_ok) {
         req_is_dnssec_ok_ = is_dnssec_ok;
     };
     /// \brief Set query TSIG attributes.
     /// \throw None
-    inline void setQuerySig(const bool is_tsig, const bool is_sig0,
+    void setQuerySig(const bool is_tsig, const bool is_sig0,
                             const bool is_badsig)
     {
         req_is_tsig_ = is_tsig;
@@ -115,22 +115,22 @@ public:
     };
     /// \brief Set zone origin.
     /// \throw None
-    inline void setOrigin(const std::string& origin) {
+    void setOrigin(const std::string& origin) {
         zone_origin_ = origin;
     };
     /// \brief Set if the answer was sent.
     /// \throw None
-    inline void answerWasSent() {
+    void answerWasSent() {
         answer_sent_ = true;
     };
     /// \brief Set if the response is truncated.
     /// \throw None
-    inline void setResponseTruncated(const bool is_truncated) {
+    void setResponseTruncated(const bool is_truncated) {
         res_is_truncated_ = is_truncated;
     };
     /// \brief Reset attributes.
     /// \throw None
-    inline void reset() {
+    void reset() {
         req_ip_version_ = 0;
         req_transport_protocol_ = 0;
         req_opcode_ = 0;
