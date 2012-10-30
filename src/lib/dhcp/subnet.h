31a62f3cf00eee5df06b1847e80ee89ec585b0ef
Tomek Mrugalski <tomasz@isc.org>
2012-10-30 18:19:50 +0100

    [2414] get(), toText() descriptions updated.

--- src/lib/dhcp/subnet.h
+++ src/lib/dhcp/subnet.h
@@ -279,10 +279,16 @@ public:
     /// @return unique ID for that subnet
     SubnetID getID() const { return (id_); }
 
+    /// @brief returns subnet parameters (prefix and prefix length)
+    ///
+    /// @return (prefix, prefix length) pair
     std::pair<isc::asiolink::IOAddress, uint8_t> get() {
         return (std::pair<isc::asiolink::IOAddress, uint8_t>(prefix_, prefix_len_));
     }
 
+    /// @brief returns textual representation of the subnet (e.g. "2001:db8::/64")
+    ///
+    /// @return textual representation
     virtual std::string toText();
 
 protected:
