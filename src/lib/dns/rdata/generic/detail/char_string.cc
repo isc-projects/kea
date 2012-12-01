6edf83daa67ac95bb45c2975e0511a830e64ac59
JINMEI Tatuya <jinmei@isc.org>
2012-11-30 23:50:33 -0800

    [2442] handled various corner cases

--- src/lib/dns/rdata/generic/detail/char_string.cc
+++ src/lib/dns/rdata/generic/detail/char_string.cc
@@ -12,10 +12,19 @@
 // OR OTHER TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR
 // PERFORMANCE OF THIS SOFTWARE.
 
+#include <exceptions/exceptions.h>
+
+#include <dns/rdata.h>
 #include <dns/master_lexer.h>
 #include <dns/rdata/generic/detail/char_string.h>
 
+#include <boost/lexical_cast.hpp>
+
+#include <cassert>
+#include <cctype>
+#include <cstring>
 #include <vector>
+
 #include <stdint.h>
 
 namespace isc {
@@ -24,18 +33,65 @@ namespace rdata {
 namespace generic {
 namespace detail {
 
+namespace {
+// Convert a DDD form to the corresponding integer
+int
+decimalToNumber(const char* s, const char* s_end) {
+    if (s_end - s < 3) {
+        isc_throw(InvalidRdataText, "Escaped digits too short");
+    }
+
+    // Make a nul-terminated copy of the 'DDD' for lexical_cast
+    char buf[4];
+    std::memcpy(buf, s, 3);
+    buf[3] = 0;
+
+    try {
+        const int i = boost::lexical_cast<int>(buf);
+        if (i > 255) {
+            isc_throw(InvalidRdataText, "Escaped digits too large: " << buf);
+        }
+        return (i);
+    } catch (const boost::bad_lexical_cast&) {
+        isc_throw(InvalidRdataText,
+                  "Invalid form for escaped digits: " << buf);
+    }
+}
+}
+
 void
 strToCharString(const MasterToken::StringRegion& str_region,
                 CharString& result)
 {
+    // make a space for the 1-byte length field; filled in at the end
     result.push_back(0);
-    size_t n = str_region.len;
+
+    bool escape = false;
     const char* s = str_region.beg;
-    while (n-- != 0) {
-        const int c = (*s++) & 0xff;
+    const char* const s_end = str_region.beg + str_region.len;
+
+    for (size_t n = str_region.len; n != 0; --n, ++s) {
+        int c = (*s & 0xff);
+        if (escape && std::isdigit(c) != 0) {
+            c = decimalToNumber(s, s_end);
+            assert(n >= 3);
+            n -= 2;
+            s += 2;
+        } else if (!escape && c == '\\') {
+            escape = true;
+            continue;
+        }
+        escape = false;
         result.push_back(c);
     }
-    result[0] = str_region.len; // FIXME: this is not always correct
+    if (escape) {               // terminated by non-escaped '\'
+        isc_throw(InvalidRdataText, "character-string ends with '\\'");
+    }
+    if (result.size() > MAX_CHARSTRING_LEN + 1) { // '+ 1' due to the len field
+        isc_throw(CharStringTooLong, "character-string is too long: " <<
+                  result.size() << " bytes");
+    }
+    result[0] = result.size() - 1;
 }
 
 } // end of detail
