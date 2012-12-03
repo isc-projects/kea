422b95742ab08fd6a3ec2aac678252a8dece0ed0
Mukund Sivaraman <muks@isc.org>
2012-12-04 04:13:57 +0530

    [2497] Return whether the string is quoted in characterstr::getNextCharacterString()

--- src/lib/dns/character_string.h
+++ src/lib/dns/character_string.h
@@ -39,9 +39,12 @@ namespace characterstr {
     /// \param input_iterator The iterator from which to start extracting,
     ///        the iterator will be updated to new position after the function
     ///        is returned
+    /// \param quoted If not \c NULL, returns \c true at this address if
+    ///        the string is quoted, \cfalse otherwise
     /// \return A std::string that contains the extracted <character-string>
     std::string getNextCharacterString(const std::string& input_str,
-                                       std::string::const_iterator& input_iterator);
+                                       std::string::const_iterator& input_iterator,
+                                       bool* quoted = NULL);
 
     /// Get a <character-string> from a input buffer
     ///
