adae095a4112d90bdb753c231d5a39b8022cfaef
JINMEI Tatuya <jinmei@isc.org>
2012-12-10 14:54:20 -0800

    [2470] handled some advanced/unusual cases of RRCollator.

--- src/lib/dns/rrcollator.cc
+++ src/lib/dns/rrcollator.cc
@@ -13,6 +13,7 @@
 // PERFORMANCE OF THIS SOFTWARE.
 
 #include <dns/name.h>
+#include <dns/rdataclass.h>
 #include <dns/rrcollator.h>
 #include <dns/rrclass.h>
 #include <dns/rrtype.h>
@@ -22,8 +23,11 @@
 
 #include <boost/bind.hpp>
 
+#include <algorithm>
+
 namespace isc {
 namespace dns {
+using namespace rdata;
 
 class RRCollator::Impl {
 public:
@@ -31,24 +35,47 @@ public:
 
     void addRR(const Name& name, const RRClass& rrclass,
                const RRType& rrtype, const RRTTL& rrttl,
-               const rdata::RdataPtr& rdata);
+               const RdataPtr& rdata);
 
     RRsetPtr current_rrset_;
     AddRRsetCallback callback_;
 };
 
+namespace {
+inline bool
+isSameType(RRType type1, const ConstRdataPtr& rdata1,
+           const ConstRRsetPtr& rrset)
+{
+    if (type1 != rrset->getType()) {
+        return (false);
+    }
+    if (type1 == RRType::RRSIG()) {
+        RdataIteratorPtr rit = rrset->getRdataIterator();
+        return (dynamic_cast<const generic::RRSIG&>(*rdata1).typeCovered()
+                == dynamic_cast<const generic::RRSIG&>(
+                    rit->getCurrent()).typeCovered());
+    }
+    return (true);
+}
+}
+
 void
 RRCollator::Impl::addRR(const Name& name, const RRClass& rrclass,
                         const RRType& rrtype, const RRTTL& rrttl,
-                        const rdata::RdataPtr& rdata)
+                        const RdataPtr& rdata)
 {
-    if (current_rrset_ && current_rrset_->getType() != rrtype) {
+    if (current_rrset_ && (!isSameType(rrtype, rdata, current_rrset_) ||
+                           current_rrset_->getClass() != rrclass ||
+                           current_rrset_->getName() != name)) {
         callback_(current_rrset_);
         current_rrset_.reset();
     }
 
     if (!current_rrset_) {
         current_rrset_ = RRsetPtr(new RRset(name, rrclass, rrtype, rrttl));
+    } else if (current_rrset_->getTTL() != rrttl) {
+        // RRs with different TTLs are given.  Smaller TTL should win.
+        current_rrset_->setTTL(std::min(current_rrset_->getTTL(), rrttl));
     }
     current_rrset_->addRdata(rdata);
 }
@@ -69,8 +96,10 @@ RRCollator::getCallback() {
 
 void
 RRCollator::finish() {
-    // TODO: case when !current_rrset_
-    impl_->callback_(impl_->current_rrset_);
+    if (impl_->current_rrset_) {
+        impl_->callback_(impl_->current_rrset_);
+        impl_->current_rrset_.reset();
+    }
 }
 
 } // end namespace dns
