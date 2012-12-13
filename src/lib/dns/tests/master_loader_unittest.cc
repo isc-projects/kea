214798a3c098f0243969bbbcf37168a9e196e1f3
Jelte Jansen <jelte@isc.org>
2012-12-13 21:20:57 +0100

    [2428] Some minor cleanups

--- src/lib/dns/tests/master_loader_unittest.cc
+++ src/lib/dns/tests/master_loader_unittest.cc
@@ -284,7 +284,7 @@ struct ErrorCase {
     // so we see the $ is actually special.
     { "$UNKNOWN 3600    IN  A   192.0.2.1", "Unknown $ directive" },
     { "$INCLUD " TEST_DATA_SRCDIR "/example.org", "Include too short" },
-    { "$INCLUDES " TEST_DATA_SRCDIR "/example.org", "Include too short" },
+    { "$INCLUDES " TEST_DATA_SRCDIR "/example.org", "Include too long" },
     { "$INCLUDE", "Missing include path" },
     { "$INCLUDE /file/not/found", "Include file not found" },
     { "$INCLUDE /file/not/found and here goes bunch of garbage",
