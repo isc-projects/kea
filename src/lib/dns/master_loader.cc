4d6495aa80132d3a67ad4ef9aa76d75683772a80
Mukund Sivaraman <muks@isc.org>
2012-12-17 12:16:01 +0530

    [2565] Avoid redundant construction in parseRRParams()

--- src/lib/dns/master_loader.cc
+++ src/lib/dns/master_loader.cc
@@ -210,13 +210,15 @@ private:
             // after the RR class below.
         }
 
-        boost::scoped_ptr<RRClass> rrclass;
-        try {
-            rrclass.reset(new RRClass(rrparam_token.getString()));
+        RRClass rrclass(zone_class_);
+        if (rrclass.fromText(rrparam_token.getString())) {
+            if (rrclass != zone_class_) {
+                // It doesn't really matter much what type of exception
+                // we throw, we catch it just below.
+                isc_throw(isc::BadValue, "Class mismatch: " << rrclass <<
+                          "vs. " << zone_class_);
+            }
             rrparam_token = lexer_.getNextToken(MasterToken::STRING);
-        } catch (const InvalidRRClass&) {
-            // If it's not an rrclass here, use the zone's class.
-            rrclass.reset(new RRClass(zone_class_));
         }
 
         // If we couldn't parse TTL earlier in the stream (above), try
@@ -225,11 +227,6 @@ private:
             setCurrentTTL(rrparam_token.getString())) {
             explicit_ttl = true;
             rrparam_token = lexer_.getNextToken(MasterToken::STRING);
-        }
-
-        if (*rrclass != zone_class_) {
-            isc_throw(InternalException, "Class mismatch: " << *rrclass <<
-                      "vs. " << zone_class_);
         }
 
         // Return the current string token's value as the RRType.
