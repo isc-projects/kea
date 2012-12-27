072dcc10a79f464352be0d5328fe588cc2c9801a
Mukund Sivaraman <muks@isc.org>
2012-12-27 16:30:01 +0530

    [2565] Throw InternalException instead of isc::BadValue

--- src/lib/dns/master_loader.cc
+++ src/lib/dns/master_loader.cc
@@ -214,9 +214,7 @@ private:
             RRClass::createFromText(rrparam_token.getString());
         if (rrclass) {
             if (*rrclass != zone_class_) {
-                // It doesn't really matter much what type of exception
-                // we throw, we catch it just below.
-                isc_throw(isc::BadValue, "Class mismatch: " << rrclass <<
+                isc_throw(InternalException, "Class mismatch: " << rrclass <<
                           "vs. " << zone_class_);
             }
             rrparam_token = lexer_.getNextToken(MasterToken::STRING);
