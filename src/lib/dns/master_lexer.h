f09298e6e1512b32462f41a4ced3dcc1950e18aa
JINMEI Tatuya <jinmei@isc.org>
2013-01-10 15:39:38 -0800

    [2572] fixed a typo: push vs pop

--- src/lib/dns/master_lexer.h
+++ src/lib/dns/master_lexer.h
@@ -499,7 +499,7 @@ public:
     /// this way may not make much sense; it can only give an informational
     /// hint of the progress.
     ///
-    /// Note also that if a source is pushed, this method will normally return
+    /// Note also that if a source is popped, this method will normally return
     /// a smaller number by definition.  Likewise, the conceptual "read
     /// cursor" would move backward after a call to \c ungetToken(), in which
     /// case this method will return a smaller value, too.
