12807b1d6e3a0846198bcc062978c077ac101b91
Mukund Sivaraman <muks@isc.org>
2013-01-28 09:28:26 +0530

    [2435] Make minor documentation updates

--- src/lib/dns/rrset_collection_base.h
+++ src/lib/dns/rrset_collection_base.h
@@ -62,18 +62,18 @@ public:
     /// refined to clarify this point in the future, and perhaps, provide
     /// additional API for these RRType.
     ///
-    /// As for RRSIG, there are such fundamental open questions.  For
-    /// example, it's not clear whether we want to return all RRSIGs
-    /// of the given name covering any RR types (in which case, we
-    /// need to figure out how), or we need to extend the interface so
-    /// we can specify the covered.  A specific derived implementation
-    /// may return something if type RRSIG is specified, but this is
-    /// not specified here at the base class level, i.e., for RRSIGs
-    /// the behavior is undefined.
+    /// As for RRSIG, there are some fundamental open questions.  For
+    /// example, it's not clear whether we want to return all RRSIGs of
+    /// the given name covering any RR types (in which case, we need to
+    /// figure out how), or we need to extend the interface so we can
+    /// specify the covered type.  A specific derived implementation may
+    /// return something if type RRSIG is specified, but this is not
+    /// specified here at the base class level. So, for RRSIGs the
+    /// behavior should be assumed as undefined.
     ///
     /// As for NSEC3, it's not clear whether owner names (which included
     /// hashed labels) are the best choice of search key, because in many
-    /// cases what the application wants to find is an NSEC3 that has the
+    /// cases, what the application wants to find is an NSEC3 that has the
     /// hash of some particular "normal" domain names.  Also, if the underlying
     /// implementation encapsulates a single zone, NSEC3 records conceptually
     /// belong to a separate name space, which may cause implementation
