0da895c79de752795279c4babfb7578d409c6d72
Jelte Jansen <jelte@isc.org>
2013-02-20 16:35:45 +0100

    [2713] Add -u/--user and -p/--password arguments
    But essentially, this is a full rewrite of the tool, since to handle it nicely I had to get rid of most of the functions and make a somewhat nicer class of it.
    
    Also added a -q option while i was at it

--- src/bin/usermgr/tests/b10-cmdctl-usermgr_test.py
+++ src/bin/usermgr/tests/b10-cmdctl-usermgr_test.py
@@ -13,10 +13,12 @@
 # NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION
 # WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
 
-import os
-import unittest
-import subprocess
+import csv
+from hashlib import sha1
 import imp
+import os
+import subprocess
+import unittest
 
 def run(command):
     """
@@ -44,9 +46,28 @@ class TestUserMgr(unittest.TestCase):
 
     def check_output_file(self, expected_content):
         self.assertTrue(os.path.exists(self.OUTPUT_FILE))
-        with open(self.OUTPUT_FILE, 'r') as f:
-            content = f.readlines()
-        self.assertEqual(expected_content, content)
+
+        csv_entries = []
+        with open(self.OUTPUT_FILE) as csvfile:
+            reader = csv.reader(csvfile)
+            for row in reader:
+                csv_entries.append(row)
+
+        self.assertEqual(len(expected_content), len(csv_entries))
+        csv_entries.reverse()
+        for expected_entry in expected_content:
+            expected_name = expected_entry[0]
+            expected_pass = expected_entry[1]
+
+            csv_entry = csv_entries.pop()
+            entry_name = csv_entry[0]
+            entry_salt = csv_entry[2]
+            entry_hash = csv_entry[1]
+
+            self.assertEqual(expected_name, entry_name)
+            expected_hash =\
+                sha1((expected_pass + entry_salt).encode()).hexdigest()
+            self.assertEqual(expected_hash, entry_hash)
 
     def run_check(self, expected_returncode, expected_stdout, expected_stderr, command):
         """
@@ -59,11 +80,11 @@ class TestUserMgr(unittest.TestCase):
                          May be None, in which case the check is skipped.
         """
         (returncode, stdout, stderr) = run(command)
-        self.assertEqual(expected_returncode, returncode, " ".join(command))
-        if expected_stdout is not None:
-            self.assertEqual(expected_stdout, stdout.decode())
         if expected_stderr is not None:
             self.assertEqual(expected_stderr, stderr.decode())
+        if expected_stdout is not None:
+            self.assertEqual(expected_stdout, stdout.decode())
+        self.assertEqual(expected_returncode, returncode, " ".join(command))
 
     def test_help(self):
         self.run_check(0,
@@ -74,9 +95,57 @@ Options:
   -h, --help            show this help message and exit
   -f OUTPUT_FILE, --file=OUTPUT_FILE
                         Specify the file to append user name and password
+  -u USERNAME, --username=USERNAME
+                        Specify username to add
+  -p PASSWORD, --password=PASSWORD
+                        Specify password to add
+  -q, --quiet           Quiet mode, don't print any output
 ''',
                        '',
                        [self.TOOL, '-h'])
+
+    def test_create_users(self):
+        """
+        Test that a file is created, and users are added.
+        Also tests quiet mode for adding a user to an existing file.
+        """
+        # content is a list of (user, pass) tuples
+        expected_content = []
+
+        # Creating a file
+        self.run_check(0,
+                       'Using accounts file: test_users.csv\n'
+                       'User added\n\n',
+                       '',
+                       [ self.TOOL,
+                         '-f', self.OUTPUT_FILE,
+                         '-u', 'user1',
+                         '-p', 'pass1' ])
+        expected_content.append(('user1', 'pass1'))
+        self.check_output_file(expected_content)
+
+        # Add to existing file
+        self.run_check(0,
+                       'Using accounts file: test_users.csv\n'
+                       'User added\n\n',
+                       '',
+                       [ self.TOOL,
+                         '-f', self.OUTPUT_FILE,
+                         '-u', 'user2',
+                         '-p', 'pass2' ])
+        expected_content.append(('user2', 'pass2'))
+        self.check_output_file(expected_content)
+
+        # Quiet mode
+        self.run_check(0,
+                       '',
+                       '',
+                       [ self.TOOL, '-q',
+                         '-f', self.OUTPUT_FILE,
+                         '-u', 'user3',
+                         '-p', 'pass3' ])
+        expected_content.append(('user3', 'pass3'))
+        self.check_output_file(expected_content)
 
     def test_default_file(self):
         """
