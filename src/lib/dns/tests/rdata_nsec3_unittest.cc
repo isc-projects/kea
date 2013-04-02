e9af9a58681b32e8fe8d85b98e6254fa2fc9a818
Mukund Sivaraman <muks@isc.org>
2013-04-02 11:06:00 +0530

    [2387] Remove unused includes

--- src/lib/dns/tests/rdata_nsec3_unittest.cc
+++ src/lib/dns/tests/rdata_nsec3_unittest.cc
@@ -17,7 +17,6 @@
 #include <exceptions/exceptions.h>
 
 #include <util/buffer.h>
-#include <util/encode/hex.h>
 #include <dns/exceptions.h>
 #include <dns/messagerenderer.h>
 #include <dns/rdata.h>
@@ -35,7 +34,6 @@ using namespace std;
 using namespace isc;
 using namespace isc::dns;
 using namespace isc::util;
-using namespace isc::util::encode;
 using namespace isc::dns::rdata;
 
 namespace {
