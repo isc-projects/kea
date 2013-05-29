69d9228eaa8c48db254e6f56df8cc9a2a51a351b
Stephen Morris <stephen@isc.org>
2013-05-29 18:26:34 +0100

    [2974] Added per-library packet context.

--- src/lib/util/hooks/callout_handle.cc
+++ src/lib/util/hooks/callout_handle.cc
@@ -13,10 +13,10 @@
 // PERFORMANCE OF THIS SOFTWARE.
 
 #include <util/hooks/callout_handle.h>
+#include <util/hooks/library_handle.h>
 
-#include <algorithm>
-#include <functional>
 #include <string>
+#include <utility>
 #include <vector>
 
 using namespace std;
@@ -25,14 +25,91 @@ using namespace isc::util;
 namespace isc {
 namespace util {
 
-// return the name of all context items.
+// Return the name of all argument items.
 
 vector<string>
 CalloutHandle::getArgumentNames() const {
 
     vector<string> names;
-    ArgumentCollection::const_iterator i;
-    for (i = arguments_.begin(); i != arguments_.end(); ++i) {
+    for (ElementCollection::const_iterator i = arguments_.begin();
+         i != arguments_.end(); ++i) {
+        names.push_back(i->first);
+    }
+
+    return (names);
+}
+
+// Return the "current" library handle.
+
+LibraryHandle&
+CalloutHandle::getLibraryHandle() const {
+    boost::shared_ptr<LibraryHandle> handle =
+        library_collection_->getLibraryHandle();
+
+    // Return refernce to this library handle.  This remains valid even
+    // after this method returns, because this object maintains a shared
+    // pointer to the LibraryHandleCollection, which in turn maintains
+    // a shared pointer to the LibraryHandle in question.
+
+    return (*handle);
+}
+
+// Check the current library index.
+
+int
+CalloutHandle::getLibraryIndex() const {
+    int curidx = library_collection_->getLibraryIndex();
+    if (curidx < 0) {
+        isc_throw(InvalidIndex, "current library handle index is not valid");
+    }
+
+    return (curidx);
+}
+
+// Return the context for the currently pointed-to library.  This version is
+// used by the "setContext()" method and creates a context for the current
+// library if it does not exist.
+
+CalloutHandle::ElementCollection&
+CalloutHandle::getContextForLibrary() {
+
+    int libindex = getLibraryIndex();
+
+    // Access a reference to the element collection for the given index,
+    // creating a new element collection if necessary, and return it.
+    return (context_collection_[libindex]);
+}
+
+// The "const" version of the above, used by the "getContext()" method.  If
+// the context for the current library doesn't exist, throw a
+// "NoSuchCalloutContext" exception.
+
+const CalloutHandle::ElementCollection&
+CalloutHandle::getContextForLibrary() const {
+
+    int libindex = getLibraryIndex();
+    ContextCollection::const_iterator libcontext =
+        context_collection_.find(libindex);
+    if (libcontext == context_collection_.end()) {
+        isc_throw(NoSuchCalloutContext, "unable to find callout context "
+                  "associated with the current library handle");
+    }
+
+    // Return a reference to the context's element collection.
+    return (libcontext->second);
+}
+
+// Return the name of all items in the context associated with the current]
+// library.
+
+vector<string>
+CalloutHandle::getContextNames() const {
+
+    vector<string> names;
+
+    const ElementCollection& elements = getContextForLibrary();
+    for (ElementCollection::const_iterator i = elements.begin();
+         i != elements.end(); ++i) {
         names.push_back(i->first);
     }
 
