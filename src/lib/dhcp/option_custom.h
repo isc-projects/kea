3994a5ce7acd9b7726fb9956156bd810c0ac406f
Michal 'vorner' Vaner <vorner@vorner.cz>
2013-06-12 12:09:41 +0200

    [2726] Suppress false positive cppcheck warning

--- src/lib/dhcp/option_custom.h
+++ src/lib/dhcp/option_custom.h
@@ -309,6 +309,8 @@ private:
     /// @tparam data type to be validated.
     ///
     /// @throw isc::dhcp::InvalidDataType if the type is invalid.
+    //cppcheck-suppress unusedPrivateFunction It's used, confusion because of
+    //templates
     template<typename T>
     void checkDataType(const uint32_t index) const;
 
