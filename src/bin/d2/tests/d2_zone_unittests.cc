c24ccb6157ccb1acaee8d395933effeebdbc584d
Marcin Siodelski <marcin@isc.org>
2013-06-27 13:22:18 +0200

    [2976] Changes as a result of code review.

--- src/bin/d2/tests/d2_zone_unittests.cc
+++ src/bin/d2/tests/d2_zone_unittests.cc
@@ -23,24 +23,34 @@ using namespace isc::dns;
 
 namespace {
 
+// This test verifies that Zone object is created and its constructor sets
+// appropriate values for its members.
 TEST(D2ZoneTest, constructor) {
+    // Create first object.
     D2Zone zone1(Name("example.com"), RRClass::ANY());
     EXPECT_EQ("example.com.", zone1.getName().toText());
     EXPECT_EQ(RRClass::ANY().getCode(), zone1.getClass().getCode());
-
+    // Create another object to make sure that constructor doesn't assign
+    // fixed values, but they change when constructor's parameters change.
     D2Zone zone2(Name("foo.example.com"), RRClass::IN());
     EXPECT_EQ("foo.example.com.", zone2.getName().toText());
     EXPECT_EQ(RRClass::IN().getCode(), zone2.getClass().getCode());
 }
 
+// This test verifies that toText() function returns text representation of
+// of the zone in expected format.
 TEST(D2ZoneTest, toText) {
+    // Create first object.
     D2Zone zone1(Name("example.com"), RRClass::ANY());
     EXPECT_EQ("example.com. ANY SOA\n", zone1.toText());
-
+    // Create another object with different parameters to make sure that the
+    // function's output changes accordingly.
     D2Zone zone2(Name("foo.example.com"), RRClass::IN());
     EXPECT_EQ("foo.example.com. IN SOA\n", zone2.toText());
 }
 
+// This test verifies that the equality and inequality operators behave as
+// expected.
 TEST(D2ZoneTest, compare) {
     const Name a("a"), b("b");
     const RRClass in(RRClass::IN()), any(RRClass::ANY());
