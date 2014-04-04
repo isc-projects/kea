97879734a91f11b3b2bcbe4e44122688e01f9f36
Marcin Siodelski <marcin@isc.org>
2014-04-04 14:09:19 +0200

    [master] Removed unused primary_separator_ member from CSVFile class.

--- src/lib/util/csv_file.cc
+++ src/lib/util/csv_file.cc
@@ -80,8 +80,7 @@ CSVRow::checkIndex(const size_t at) const {
 }
 
 CSVFile::CSVFile(const std::string& filename)
-    : primary_separator_(','), filename_(filename), fs_(), cols_(0),
-      read_msg_() {
+    : filename_(filename), fs_(), cols_(0), read_msg_() {
 }
 
 CSVFile::~CSVFile() {
