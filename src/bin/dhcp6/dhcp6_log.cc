4371623f9b3b0626f83de9bed185af25093f1a65
Marcin Siodelski <marcin@isc.org>
2014-07-02 10:28:04 +0200

    [3414] Removed references to b10-dhcp in the Kea documentation.

--- src/bin/dhcp6/dhcp6_log.cc
+++ src/bin/dhcp6/dhcp6_log.cc
@@ -12,7 +12,7 @@
 // OR OTHER TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR
 // PERFORMANCE OF THIS SOFTWARE.
 
-/// Defines the logger used by the top-level component of b10-dhcp6.
+/// Defines the logger used by the top-level component of kea-dhcp6.
 
 #include <dhcp6/dhcp6_log.h>
 
