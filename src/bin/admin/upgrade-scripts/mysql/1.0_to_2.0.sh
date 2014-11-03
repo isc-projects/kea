2dda9846366e3cd554cddd47f1092c4661be0b9d
Tomek Mrugalski <tomasz@isc.org>
2014-11-03 21:44:04 +0100

    [3599] kea-admin, mysql 1.0 to 2.0 upgrade added.

---
+++ src/bin/admin/upgrade-scripts/mysql/1.0_to_2.0.sh
@@ -0,0 +1,22 @@
+mysql "$@" <<EOF
+ALTER TABLE lease6
+    ADD COLUMN hwaddr varbinary(2),
+    ADD COLUMN hwtype smallint unsigned,
+    ADD COLUMN hwaddr_source int unsigned;
+
+CREATE TABLE lease6_hwaddr_source (
+    hwaddr_source INT PRIMARY KEY NOT NULL,
+    name VARCHAR(40)
+);
+
+-- See src/lib/dhcp/dhcp/pkt.h for detailed explanation
+INSERT INTO lease6_hwaddr_source VALUES (1, "HWADDR_SOURCE_RAW");
+INSERT INTO lease6_hwaddr_source VALUES (2, "HWADDR_SOURCE_IPV6_LINK_LOCAL");
+INSERT INTO lease6_hwaddr_source VALUES (4, "HWADDR_SOURCE_DUID");
+INSERT INTO lease6_hwaddr_source VALUES (8, "HWADDR_SOURCE_CLIENT_ADDR_RELAY_OPTION");
+INSERT INTO lease6_hwaddr_source VALUES (16, "HWADDR_SOURCE_REMOTE_ID");
+INSERT INTO lease6_hwaddr_source VALUES (32, "HWADDR_SOURCE_SUBSCRIBER_ID");
+INSERT INTO lease6_hwaddr_source VALUES (64, "HWADDR_SOURCE_DOCSIS");
+
+UPDATE schema_version SET version="2", minor="0";
+EOF
