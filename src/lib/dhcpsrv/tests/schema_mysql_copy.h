ab0365c68dfa536cd56f3a2b52c25fc295e8b253
Tomek Mrugalski <tomasz@isc.org>
2014-11-06 19:36:46 +0100

    [3556] HWAddr support in MySQL implemented.

--- src/lib/dhcpsrv/tests/schema_mysql_copy.h
+++ src/lib/dhcpsrv/tests/schema_mysql_copy.h
@@ -35,6 +35,7 @@ const char* destroy_statement[] = {
     "DROP TABLE lease4",
     "DROP TABLE lease6",
     "DROP TABLE lease6_types",
+    "DROP TABLE lease6_hwaddr_source",
     "DROP TABLE schema_version",
     NULL
 };
@@ -92,6 +93,27 @@ const char* create_statement[] = {
 
     "INSERT INTO schema_version VALUES (1, 0)",
     "COMMIT",
+
+    "ALTER TABLE lease6 "
+    "ADD COLUMN hwaddr varbinary(20),"
+    "ADD COLUMN hwtype smallint unsigned,"
+    "ADD COLUMN hwaddr_source int unsigned;",
+
+    "CREATE TABLE lease6_hwaddr_source ("
+    "hwaddr_source INT PRIMARY KEY NOT NULL,"
+    "name VARCHAR(40) )",
+
+    // We should probably comment those statements. They are not used by the
+    // tests and they slow execution down by 11 seconds on my beefy desktop.
+    "INSERT INTO lease6_hwaddr_source VALUES (1, \"HWADDR_SOURCE_RAW\");",
+    "INSERT INTO lease6_hwaddr_source VALUES (2, \"HWADDR_SOURCE_IPV6_LINK_LOCAL\");",
+    "INSERT INTO lease6_hwaddr_source VALUES (4, \"HWADDR_SOURCE_DUID\");",
+    "INSERT INTO lease6_hwaddr_source VALUES (8, \"HWADDR_SOURCE_CLIENT_ADDR_RELAY_OPTION\");",
+    "INSERT INTO lease6_hwaddr_source VALUES (16, \"HWADDR_SOURCE_REMOTE_ID\");",
+    "INSERT INTO lease6_hwaddr_source VALUES (32, \"HWADDR_SOURCE_SUBSCRIBER_ID\");",
+    "INSERT INTO lease6_hwaddr_source VALUES (64, \"HWADDR_SOURCE_DOCSIS\");",
+
+    "UPDATE schema_version SET version=\"2\", minor=\"0\";",
 
     NULL
 };
