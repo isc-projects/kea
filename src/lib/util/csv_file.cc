fbce7dd40fdc965ea439746797dcebb9cb83836e
Marcin Siodelski <marcin@isc.org>
2015-01-12 12:38:47 +0100

    [3671] Added unit test for loading v6 leases from multiple files.
    Also, prevented opening the optional lease files if they don't exist.

--- src/lib/util/csv_file.cc
+++ src/lib/util/csv_file.cc
@@ -1,4 +1,4 @@
-// Copyright (C) 2014 Internet Systems Consortium, Inc. ("ISC")
+// Copyright (C) 2014-2015 Internet Systems Consortium, Inc. ("ISC")
 //
 // Permission to use, copy, modify, and/or distribute this software for any
 // purpose with or without fee is hereby granted, provided that the above
@@ -95,6 +95,17 @@ CSVFile::close() {
         fs_->close();
         fs_.reset();
     }
+}
+
+bool
+CSVFile::exists() const {
+    std::ifstream fs(filename_);
+    if (fs.good()) {
+        fs.close();
+        return (true);
+    }
+    fs.close();
+    return (false);
 }
 
 void
