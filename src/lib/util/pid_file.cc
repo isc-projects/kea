852a54c50aa32ba5c2d9b3ba117d7522ddd1839d
Shawn Routhier <sar@isc.org>
2015-01-27 22:14:59 -0800

    [trac3687] Update per first set of review comments

--- src/lib/util/pid_file.cc
+++ src/lib/util/pid_file.cc
@@ -28,14 +28,14 @@ PIDFile::~PIDFile() {
 }
 
 bool
-PIDFile::check() {
+PIDFile::check() const {
     std::ifstream fs(filename_.c_str());
     int pid;
     bool good;
 
     // If we weren't able to open the file treat
     // it as if the process wasn't running
-    if (fs.is_open() == false) {
+    if (!fs.is_open()) {
         return (false);
     }
 
@@ -44,9 +44,14 @@ PIDFile::check() {
     good = fs.good();
     fs.close();
 
-    // Treat being unable to read a pid the same as if we
-    // got a pid and the process is still running.
-    if ((good == false) || (kill(pid, 0) == 0)) {
+    // If we weren't able to read a pid send back an execption
+    if (!good) {
+        isc_throw(PIDCantReadPID, "Unable to read PID from file '"
+                  << filename_ << "'");
+    }
+
+    // If the process is still running return true
+    if (kill(pid, 0) == 0) {
         return (true);
     }
 
@@ -55,28 +60,32 @@ PIDFile::check() {
 }
 
 void
-PIDFile::write() {
+PIDFile::write() const {
     write(getpid());
 }
 
 void
-PIDFile::write(int pid) {
-    std::ofstream fs(filename_.c_str());
+PIDFile::write(int pid) const {
+  std::ofstream fs(filename_.c_str(), std::ofstream::trunc);
 
-    if (fs.is_open() == false) {
+    if (!fs.is_open()) {
         isc_throw(PIDFileError, "Unable to open PID file '"
                   << filename_ << "' for write");
     }
 
     // File is open, write the pid.
     fs << pid << std::endl;
+    if (!fs.good()) {
+        isc_throw(PIDFileError, "Unable to write to PID file '"
+                  << filename_ << "'");
+    }
 
     // That's it
     fs.close();
 }
 
 void
-PIDFile::deleteFile() {
+PIDFile::deleteFile() const {
     if (remove(filename_.c_str()) != 0) {
         isc_throw(PIDFileError, "Unable to delete PID file '"
                   << filename_ << "'");
