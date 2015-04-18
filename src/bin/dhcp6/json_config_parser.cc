b4de860066000b104312ab2b5f2e29598a4e1cbc
Francis Dupont <fdupont@isc.org>
2015-04-18 15:01:25 +0200

    [master] JSON summary trivial typo

--- src/bin/dhcp6/json_config_parser.cc
+++ src/bin/dhcp6/json_config_parser.cc
@@ -870,7 +870,7 @@ configureDhcp6Server(Dhcpv6Srv&, isc::data::ConstElementPtr config_set) {
     }
 
     LOG_INFO(dhcp6_logger, DHCP6_CONFIG_COMPLETE)
-        .arg(CfgMgr::instance().getCurrentCfg()->
+        .arg(CfgMgr::instance().getStagingCfg()->
              getConfigSummary(SrvConfig::CFGSEL_ALL6));
 
     // Everything was fine. Configuration is successful.
