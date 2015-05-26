8f37cdf440e1531afce2d0305eaf882beb60495b
Tomek Mrugalski <tomasz@isc.org>
2015-05-26 20:16:02 +0200

    [3880] Initial implementation:
     - CommandMgr class implemented
     - unit-tests for CommandMgr implemented
     - Stub implementation for CommandSocketFactory

---
+++ src/lib/config/command_mgr.cc
@@ -0,0 +1,130 @@
+// Copyright (C) 2015 Internet Systems Consortium, Inc. ("ISC")
+//
+// Permission to use, copy, modify, and/or distribute this software for any
+// purpose with or without fee is hereby granted, provided that the above
+// copyright notice and this permission notice appear in all copies.
+//
+// THE SOFTWARE IS PROVIDED "AS IS" AND ISC DISCLAIMS ALL WARRANTIES WITH
+// REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF MERCHANTABILITY
+// AND FITNESS.  IN NO EVENT SHALL ISC BE LIABLE FOR ANY SPECIAL, DIRECT,
+// INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES WHATSOEVER RESULTING FROM
+// LOSS OF USE, DATA OR PROFITS, WHETHER IN AN ACTION OF CONTRACT, NEGLIGENCE
+// OR OTHER TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR
+// PERFORMANCE OF THIS SOFTWARE.
+
+#include <config/command_mgr.h>
+#include <config/command_socket_factory.h>
+#include <cc/data.h>
+#include <boost/bind.hpp>
+
+namespace isc {
+namespace config {
+
+CommandMgr::CommandMgr() {
+    registerCommand("list-commands",
+        boost::bind(&CommandMgr::listCommandsHandler, this, _1, _2));
+}
+
+void CommandMgr::configureCtrlSocket(const isc::data::ConstElementPtr& socket_info) {
+    if (socket_info_) {
+        isc_throw(CommandSocketError, "There is already a control socket open");
+    }
+
+    socket_ = CommandSocketFactory::create(socket_info);
+    socket_info_ = socket_info;
+
+    /// @todo: install socket in IfaceMgr
+    ///CommandSocketFactory::install(socket_, socket_info);
+}
+
+void CommandMgr::closeCtrlSocket() {
+    CommandSocketFactory::close(socket_, socket_info_);
+
+    socket_ = 0;
+    socket_info_.reset();
+}
+
+CommandMgr&
+CommandMgr::instance() {
+    static CommandMgr cmd_mgr;
+    return (cmd_mgr);
+}
+
+void CommandMgr::registerCommand(const std::string& cmd, CommandHandler handler) {
+
+    if (!handler) {
+        isc_throw(InvalidCommandHandler, "Specified command handler is NULL");
+    }
+
+    HandlerContainer::const_iterator it = handlers_.find(cmd);
+    if (it != handlers_.end()) {
+        isc_throw(InvalidCommandName, "Handler for command '" << cmd
+                  << "' is already installed.");
+    }
+
+    handlers_.insert(make_pair(cmd, handler));
+}
+
+void CommandMgr::deregisterCommand(const std::string& cmd) {
+    if (cmd == "list-commands") {
+        isc_throw(InvalidCommandName,
+                  "Can't uninstall internal command 'list-commands'");
+    }
+
+    HandlerContainer::iterator it = handlers_.find(cmd);
+    if (it == handlers_.end()) {
+        isc_throw(InvalidCommandName, "Handler for command '" << cmd
+                  << "' not found.");
+    }
+    handlers_.erase(it);
+}
+
+void CommandMgr::deregisterAll() {
+    handlers_.clear();
+    registerCommand("list-commands",
+        boost::bind(&CommandMgr::listCommandsHandler, this, _1, _2));
+}
+
+isc::data::ConstElementPtr
+CommandMgr::processCommand(const isc::data::ConstElementPtr& cmd) {
+    if (!cmd) {
+        return (createAnswer(CONTROL_RESULT_ERROR,
+                             "Command processing failed: NULL command parameter"));
+    }
+
+    try {
+        isc::data::ConstElementPtr arg;
+        std::string name = parseCommand(arg, cmd);
+
+        HandlerContainer::const_iterator it = handlers_.find(name);
+        if (it == handlers_.end()) {
+            // Ok, there's no such command.
+            return (createAnswer(CONTROL_RESULT_ERROR,
+                                 "'" + name + "' command not supported."));
+        }
+
+        // Call the actual handler and return whatever it returned
+        return (it->second(name, arg));
+
+    } catch (const Exception& e) {
+        return (createAnswer(CONTROL_RESULT_ERROR,
+                             std::string("Error during command processing:")
+                             + e.what()));
+    }
+
+}
+
+isc::data::ConstElementPtr
+CommandMgr::listCommandsHandler(const std::string& name,
+                                const isc::data::ConstElementPtr& params) {
+    using namespace isc::data;
+    ElementPtr commands = Element::createList();
+    for (HandlerContainer::const_iterator it = handlers_.begin();
+         it != handlers_.end(); ++it) {
+        commands->add(Element::create(it->first));
+    }
+    return (createAnswer(CONTROL_RESULT_SUCCESS, commands));
+}
+
+}; // end of isc::config
+}; // end of isc
---
+++ src/lib/config/command_mgr.h
@@ -0,0 +1,171 @@
+// Copyright (C) 2015 Internet Systems Consortium, Inc. ("ISC")
+//
+// Permission to use, copy, modify, and/or distribute this software for any
+// purpose with or without fee is hereby granted, provided that the above
+// copyright notice and this permission notice appear in all copies.
+//
+// THE SOFTWARE IS PROVIDED "AS IS" AND ISC DISCLAIMS ALL WARRANTIES WITH
+// REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF MERCHANTABILITY
+// AND FITNESS.  IN NO EVENT SHALL ISC BE LIABLE FOR ANY SPECIAL, DIRECT,
+// INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES WHATSOEVER RESULTING FROM
+// LOSS OF USE, DATA OR PROFITS, WHETHER IN AN ACTION OF CONTRACT, NEGLIGENCE
+// OR OTHER TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR
+// PERFORMANCE OF THIS SOFTWARE.
+
+#ifndef COMMAND_MGR_H
+#define COMMAND_MGR_H
+
+#include <config/command_interpreter.h>
+#include <cc/data.h>
+#include <boost/noncopyable.hpp>
+#include <boost/any.hpp>
+#include <string>
+#include <map>
+
+namespace isc {
+namespace config {
+
+/// @brief CommandMgr exception indicating that the handler specified is not valid
+class InvalidCommandHandler : public Exception {
+public:
+    InvalidCommandHandler(const char* file, size_t line, const char* what) :
+        isc::Exception(file, line, what) { };
+};
+
+/// @brief CommandMgr exception indicating that the command name is not valid
+class InvalidCommandName : public Exception {
+public:
+    InvalidCommandName(const char* file, size_t line, const char* what) :
+        isc::Exception(file, line, what) { };
+};
+
+/// @brief An exception indicating that operation on the command socket failed
+class CommandSocketError : public Exception {
+public:
+    CommandSocketError(const char* file, size_t line, const char* what) :
+        isc::Exception(file, line, what) { };
+};
+
+
+
+/// @brief Commands Manager, responsible for processing external commands
+///
+/// Commands Manager is a generic interface for handling external commands.
+/// Commands can be received over control sockets. Currently unix socket is
+/// supported, but additional type (udp, tcp, https etc.) may be added later.
+/// The commands and responses are sent in JSON format.
+/// See http://kea.isc.org/wiki/StatsDesign for details.
+///
+/// In general, the command has the following format:
+/// {
+///     "command": "statistic-get",
+///     "arguments": {
+///         "name": "received-packets"
+///     }
+/// }
+///
+/// And the response is:
+///
+/// {
+///     "result": 0,
+///     "observations": {
+///         "received-packets": [ [ 1234, "2015-04-15 12:34:45.123" ] ]
+///     }
+/// }
+///
+/// CommandsMgr does not implement the commands (except one, "commands-list")
+/// itself, but rather provides an interface (see @ref registerCommand,
+/// @ref deregisterCommand, @ref processCommand) for other components to use
+/// it. The @ref CommandHandler type is specified in a way to easily use
+/// existing command handlers in DHCPv4 and DHCPv6 components.
+class CommandMgr : public boost::noncopyable {
+public:
+
+    /// @brief Defines command handler type
+    ///
+    /// Command handlers are expected to use this format.
+    /// @param name name of the commands
+    /// @param params parameters specific to the command
+    /// @return response (created with createAnswer())
+    typedef boost::function<isc::data::ConstElementPtr (const std::string& name,
+        const isc::data::ConstElementPtr& params)> CommandHandler;
+
+    /// @brief CommandMgr is a singleton class. This method returns reference
+    /// to its sole instance.
+    ///
+    /// @return the only existing instance of the manager
+    static CommandMgr& instance();
+
+    /// @brief Configured control socket with paramters specified in socket_info
+    ///
+    /// Currently supported types are:
+    /// - unix (required parameters: socket-type: unix, socket-name:/unix/path)
+    ///
+    /// @throw CommandSocketError if socket creation fails
+    ///
+    /// @param socket_info describes control socket parameters
+    void configureCtrlSocket(const isc::data::ConstElementPtr& socket_info);
+
+    /// @brief Shuts down any open control sockets
+    void closeCtrlSocket();
+
+    /// @brief Registers specified command handler for a given command
+    ///
+    /// @param cmd name of the command to be handled
+    /// @param handler pointer to the method that will handle the command
+    void registerCommand(const std::string& cmd, CommandHandler handler);
+
+    /// @brief Deregisters specified command handler
+    ///
+    /// @param cmd name of the command that's no longer handled
+    void deregisterCommand(const std::string& cmd);
+
+    /// @brief Triggers command processing
+    ///
+    /// This method processes specified command. The command is specified using
+    /// a single Element. See @ref CommandMgr for description of its syntax.
+    /// Typically, this method is called internally, when there's a new data
+    /// received over control socket. However, in some cases (e.g. signal received)
+    /// it may be called by external code explicitly. Hence this method is public.
+    isc::data::ConstElementPtr processCommand(const isc::data::ConstElementPtr& cmd);
+
+    /// @brief Auxiliary method that removes all installed commands.
+    ///
+    /// The only unwipeable method is list-commands, which is internally
+    /// handled at all times.
+    void deregisterAll();
+
+private:
+
+    /// @brief Private constructor
+    ///
+    /// Registers internal 'list-commands' command.
+    CommandMgr();
+
+    /// @brief 'list-commands' command handler
+    ///
+    /// This method implements command 'list-commands'. It returns a list of all
+    /// currently supported commands.
+    /// @param name name of the command (should always be 'list-commands')
+    /// @param params additional parameters (ignored)
+    /// @return structure that includes all currently supported commands
+    isc::data::ConstElementPtr
+    listCommandsHandler(const std::string& name,
+                        const isc::data::ConstElementPtr& params);
+
+    typedef std::map<std::string, CommandHandler> HandlerContainer;
+
+    /// @brief Container for command handlers
+    HandlerContainer handlers_;
+
+    /// @brief Socket file descriptor
+    int socket_;
+
+    /// @brief Parameters for control socket
+    isc::data::ConstElementPtr socket_info_;
+};
+
+}; // end of isc::config namespace
+}; // end of isc namespace
+
+#endif
