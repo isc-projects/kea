43a7ef68706f3ea9d6afff5e1522683629f55206
Tomek Mrugalski <tomasz@isc.org>
2015-06-08 19:57:34 +0200

    [3880] Extra sanity check added.

--- src/lib/dhcp/iface_mgr.cc
+++ src/lib/dhcp/iface_mgr.cc
@@ -292,6 +292,10 @@ IfaceMgr::isDirectResponseSupported() const {
 
 void
 IfaceMgr::addExternalSocket(int socketfd, SocketCallback callback) {
+    if (socketfd < 0) {
+        isc_throw(BadValue, "Attempted to install callback for invalid socket "
+                  << socketfd);
+    }
     BOOST_FOREACH(SocketCallbackInfo s, callbacks_) {
         // There's such a socket description there already.
         // Update the callback and we're done
