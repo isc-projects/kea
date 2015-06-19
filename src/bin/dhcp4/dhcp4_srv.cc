50aaaa602f62f5864bcf7f99ecdef9291b88bc87
Francis Dupont <fdupont@isc.org>
2015-06-19 14:33:11 +0200

    [3882a] Updated server stuff at the exclusion of the extended version code itself

--- src/bin/dhcp4/dhcp4_srv.cc
+++ src/bin/dhcp4/dhcp4_srv.cc
@@ -46,6 +46,14 @@
 #include <cryptolink/cryptolink.h>
 #include <cfgrpt/config_report.h>
 
+#ifdef HAVE_MYSQL
+#include <dhcpsrv/mysql_lease_mgr.h>
+#endif
+#ifdef HAVE_PGSQL
+#include <dhcpsrv/pgsql_lease_mgr.h>
+#endif
+#include <dhcpsrv/memfile_lease_mgr.h>
+
 #include <asio.hpp>
 #include <boost/bind.hpp>
 #include <boost/foreach.hpp>
