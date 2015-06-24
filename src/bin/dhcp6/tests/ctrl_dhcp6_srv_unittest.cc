b183c1619fbe2a4e54612fd28b51b013274ae4e1
Francis Dupont <fdupont@isc.org>
2015-06-24 10:09:50 +0200

    [master] Some other/new s/unlink/remove/

--- src/bin/dhcp6/tests/ctrl_dhcp6_srv_unittest.cc
+++ src/bin/dhcp6/tests/ctrl_dhcp6_srv_unittest.cc
@@ -209,8 +209,8 @@ public:
         HooksManager::unloadLibraries();
 
         // Get rid of any marker files.
-        static_cast<void>(unlink(LOAD_MARKER_FILE));
-        static_cast<void>(unlink(UNLOAD_MARKER_FILE));
+        static_cast<void>(remove(LOAD_MARKER_FILE));
+        static_cast<void>(remove(UNLOAD_MARKER_FILE));
         IfaceMgr::instance().deleteAllExternalSockets();
         CfgMgr::instance().clear();
     }
