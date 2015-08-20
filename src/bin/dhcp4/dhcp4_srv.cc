1b22820f779af856a979b1cd55e446d5442e9265
Tomek Mrugalski <tomasz@isc.org>
2015-08-20 14:56:26 +0200

    [support8785] Select interruption by signal is now logged.
      One of the hypotheses for the #8785 issue is mis-handling of
      the SingalInterruptOnSelect exception. This message will
      be printed every time a signal is received during select().
    
    Conflicts:
    	src/bin/dhcp4/dhcp4_messages.mes
    	src/bin/dhcp4/dhcp4_srv.cc

--- src/bin/dhcp4/dhcp4_srv.cc
+++ src/bin/dhcp4/dhcp4_srv.cc
@@ -400,8 +400,8 @@ Dhcpv4Srv::run() {
         } catch (const SignalInterruptOnSelect) {
             // Packet reception interrupted because a signal has been received.
             // This is not an error because we might have received a SIGTERM,
-            // SIGINT or SIGHUP which are handled by the server. For signals
-            // that are not handled by the server we rely on the default
+            // SIGINT, SIGHUP or SIGCHILD which are handled by the server. For
+            // signals that are not handled by the server we rely on the default
             // behavior of the system.
             LOG_DEBUG(packet4_logger, DBG_DHCP4_DETAIL, DHCP4_BUFFER_WAIT_SIGNAL)
                 .arg(signal_set_->getNext());
