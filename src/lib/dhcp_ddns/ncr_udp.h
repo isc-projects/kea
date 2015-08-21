f3b50d9edb22eb3bdabdaacb6480252c8a098f81
Marcin Siodelski <marcin@isc.org>
2015-08-21 21:41:34 +0200

    [3970] Moved WatchSocket class to utilities.

--- src/lib/dhcp_ddns/ncr_udp.h
+++ src/lib/dhcp_ddns/ncr_udp.h
@@ -1,4 +1,4 @@
-// Copyright (C) 2013-2014 Internet Systems Consortium, Inc. ("ISC")
+// Copyright (C) 2013-2015 Internet Systems Consortium, Inc. ("ISC")
 //
 // Permission to use, copy, modify, and/or distribute this software for any
 // purpose with or without fee is hereby granted, provided that the above
@@ -112,8 +112,8 @@
 #include <asiolink/udp_endpoint.h>
 #include <asiolink/udp_socket.h>
 #include <dhcp_ddns/ncr_io.h>
-#include <dhcp_ddns/watch_socket.h>
 #include <util/buffer.h>
+#include <util/watch_socket.h>
 
 #include <boost/shared_array.hpp>
 
@@ -579,7 +579,7 @@ private:
     bool reuse_address_;
 
     /// @brief Pointer to WatchSocket instance supplying the "select-fd".
-    WatchSocketPtr watch_socket_;
+    util::WatchSocketPtr watch_socket_;
 };
 
 } // namespace isc::dhcp_ddns
