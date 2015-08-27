00ed4a1f44273cd2b15c09b9fcb523cff6626d05
Marcin Siodelski <marcin@isc.org>
2015-08-27 18:50:12 +0200

    [3970] Updated implementation of TimerMgr and make it almost ready.
    The TimerMgr now uses the IfaceMgr to register WatchSockets. It
    also comes with a full doxygen documentation and extensive
    commentary in the code.

--- src/lib/dhcpsrv/tests/timer_mgr_unittest.cc
+++ src/lib/dhcpsrv/tests/timer_mgr_unittest.cc
@@ -14,7 +14,9 @@
 
 #include <asio.hpp>
 #include <config.h>
+#include <dhcp/iface_mgr.h>
 #include <dhcpsrv/timer_mgr.h>
+#include <exceptions/exceptions.h>
 #include <boost/bind.hpp>
 #include <gtest/gtest.h>
 
@@ -35,6 +37,8 @@ private:
 
     /// @brief IO service used by the test fixture class.
     IOService io_service_;
+
+    bool timeout_;
 
 public:
 
@@ -64,6 +68,8 @@ public:
     /// be increased.
     void timerCallback(const std::string& timer_name);
 
+    boost::function<void ()> makeCallback(const std::string& timer_name);
+
     /// @brief Callback for timeout.
     void timeoutCallback();
 
@@ -78,12 +84,16 @@ public:
 void
 TimerMgrTest::SetUp() {
     calls_count_.clear();
-//    TimerMgr::instance().startThread();
+    timeout_ = false;
+    // Make sure there are no dangling threads.
+    TimerMgr::instance().stopThread();
 }
 
 void
 TimerMgrTest::TearDown() {
-//    TimerMgr::instance().stopThread();
+    // Make sure there are no dangling threads.
+    TimerMgr::instance().stopThread();
+    TimerMgr::instance().deregisterTimers();
 }
 
 void
@@ -92,9 +102,8 @@ TimerMgrTest::registerTimer(const std::string& timer_name, const long timer_inte
     TimerMgr& timer_mgr = TimerMgr::instance();
 
     ASSERT_NO_THROW(
-        timer_mgr.registerTimer(timer_name, boost::bind(&TimerMgrTest::timerCallback,
-                                                        this, timer_name),
-                                timer_interval, timer_mode)
+        timer_mgr.registerTimer(timer_name, makeCallback(timer_name), timer_interval,
+                                timer_mode)
     );
 
     calls_count_[timer_name] = 0;
@@ -107,7 +116,12 @@ TimerMgrTest::doWait(const long timeout) {
     timeout_timer.setup(boost::bind(&TimerMgrTest::timeoutCallback, this), timeout,
                         IntervalTimer::ONE_SHOT);
 
-    io_service_.run_one();
+    while (!timeout_) {
+        IfaceMgr::instance().receive6(0, timeout * 1000);
+        io_service_.get_io_service().poll_one();
+    }
+
+    timeout_ = false;
 }
 
 void
@@ -118,24 +132,70 @@ TimerMgrTest::timerCallback(const std::string& timer_name) {
     TimerMgr::instance().setup(timer_name);
 }
 
+boost::function<void ()>
+TimerMgrTest::makeCallback(const std::string& timer_name) {
+    return (boost::bind(&TimerMgrTest::timerCallback, this, timer_name));
+}
+
+
 void
 TimerMgrTest::timeoutCallback() {
     io_service_.stop();
+    io_service_.get_io_service().reset();
+    timeout_ = true;
 }
 
 TEST_F(TimerMgrTest, registerTimer) {
     TimerMgr& timer_mgr = TimerMgr::instance();
 
-    ASSERT_NO_FATAL_FAILURE(registerTimer("timer1", 1));
+    // Empty timer name is not allowed.
+    ASSERT_THROW(timer_mgr.registerTimer("", makeCallback("timer1"), 1,
+                                         IntervalTimer::ONE_SHOT),
+                 BadValue);
 
-    TimerMgr::instance().startThread();
-    TimerMgr::instance().setup("timer1");
+    // Add a timer with a correct name.
+    ASSERT_NO_THROW(timer_mgr.registerTimer("timer2", makeCallback("timer2"), 1,
+                                         IntervalTimer::ONE_SHOT));
+    // Adding the timer with the same name as the existing timer is not
+    // allowed.
+    ASSERT_THROW(timer_mgr.registerTimer("timer2", makeCallback("timer2"), 1,
+                                         IntervalTimer::ONE_SHOT),
+                 BadValue);
+
+    // Start worker thread.
+    ASSERT_NO_THROW(timer_mgr.startThread());
+}
+
+TEST_F(TimerMgrTest, scheduleTimers) {
+    TimerMgr& timer_mgr = TimerMgr::instance();
+
+    ASSERT_NO_FATAL_FAILURE(registerTimer("timer1", 1));
+    ASSERT_NO_FATAL_FAILURE(registerTimer("timer2", 5));
+
+    timer_mgr.startThread();
+    timer_mgr.setup("timer1");
+    timer_mgr.setup("timer2");
 
     doWait(500);
 
-    TimerMgr::instance().stopThread();
+    timer_mgr.stopThread();
 
     EXPECT_GT(calls_count_["timer1"], 1);
+    EXPECT_GT(calls_count_["timer2"], 5);
+
+    EXPECT_GT(calls_count_["timer1"], calls_count_["timer2"]);
+
+    ASSERT_NO_THROW(timer_mgr.deregisterTimer("timer1"));
+
+    unsigned int calls_count_timer1 = calls_count_["timer1"];
+    unsigned int calls_count_timer2 = calls_count_["timer2"];
+
+    timer_mgr.startThread();
+
+    doWait(500);
+
+    EXPECT_EQ(calls_count_timer1, calls_count_["timer1"]);
+    EXPECT_GT(calls_count_["timer2"], calls_count_timer2);
 }
 
 } // end of anonymous namespace
--- src/lib/dhcpsrv/timer_mgr.cc
+++ src/lib/dhcpsrv/timer_mgr.cc
@@ -13,10 +13,12 @@
 // PERFORMANCE OF THIS SOFTWARE.
 
 #include <asio.hpp>
+#include <asiolink/io_service.h>
+#include <dhcp/iface_mgr.h>
 #include <dhcpsrv/timer_mgr.h>
 #include <exceptions/exceptions.h>
 #include <boost/bind.hpp>
-#include <iostream>
+#include <utility>
 
 using namespace isc;
 using namespace isc::asiolink;
@@ -33,7 +35,15 @@ TimerMgr::instance() {
 }
 
 TimerMgr::TimerMgr()
-    : io_service_(new IOService()), thread_() {
+    : thread_(), registered_timers_() {
+}
+
+TimerMgr::~TimerMgr() {
+    // Stop the thread, but do not deregister any timers. Deregistering
+    // the timers could cause static deinitialization fiasco between the
+    // TimerMgr and IfaceMgr. By now, the caller should have deregistered
+    // the timers.
+    stopThread();
 }
 
 void
@@ -42,62 +52,191 @@ TimerMgr::registerTimer(const std::string& timer_name,
                         const long interval,
                         const IntervalTimer::Mode& scheduling_mode) {
 
+    // Timer name must not be empty.
     if (timer_name.empty()) {
         isc_throw(BadValue, "registered timer name must not be empty");
     }
 
+    // Must not register two timers under the same name.
     if (registered_timers_.find(timer_name) != registered_timers_.end()) {
         isc_throw(BadValue, "trying to register duplicate timer '"
                   << timer_name << "'");
     }
 
+    // Must not register new timer when the worker thread is running. Note
+    // that worker thread is using IO service and trying to register a new
+    // timer while IO service is being used would result in hang.
     if (thread_) {
         isc_throw(InvalidOperation, "unable to register new timer when the"
                   " timer manager's worker thread is running");
     }
 
-    IntervalTimerPtr interval_timer(new IntervalTimer(*io_service_));
+    // Create a structure holding the configuration for the timer. It will
+    // create the instance if the IntervalTimer and WatchSocket. It will
+    // also hold the callback, interval and scheduling mode parameters.
+    TimerInfo timer_info(getIOService(), callback, interval, scheduling_mode);
 
-    WatchSocket watch_socket;
-    TimerInfo timer_info(watch_socket, interval_timer, callback, interval,
-                         scheduling_mode);
+    // Register the WatchSocket in the IfaceMgr and register our own callback
+    // to be executed when the data is received over this socket.
+    IfaceMgr::instance().addExternalSocket(timer_info.watch_socket_->getSelectFd(),
+                                           boost::bind(&TimerMgr::ifaceMgrCallback,
+                                                       this, timer_name));
+
+    // Actually register the timer.
     registered_timers_.insert(std::pair<std::string, TimerInfo>(timer_name, timer_info));
 }
 
 void
 TimerMgr::deregisterTimer(const std::string& timer_name) {
+    // Find the timer with specified name.
+    TimerInfoMap::iterator timer_info_it = registered_timers_.find(timer_name);
+
+    // Check if the timer has been registered.
+    if (timer_info_it == registered_timers_.end()) {
+        isc_throw(BadValue, "unable to deregister non existing timer '"
+                  << timer_name << "'");
+    }
+
+    TimerInfo& timer_info = timer_info_it->second;
+
+    // Cancel any pending asynchronous operation and stop the timer.
+    timer_info.interval_timer_->cancel();
+
+    // Unregister the watch socket from the IfaceMgr.
+    IfaceMgr::instance().deleteExternalSocket(timer_info.watch_socket_->getSelectFd());
+
+    // Remove the timer.
+    registered_timers_.erase(timer_info_it);
+}
+
+void
+TimerMgr::deregisterTimers() {
+    // Copy the map holding timers configuration. This is required so as
+    // we don't cut the branch which we're sitting on when we will be
+    // erasing the timers. We're going to iterate over the register timers
+    // and remove them with the call to deregisterTimer function. But this
+    // function will remove them from the register_timers_ map. If we
+    // didn't work on the copy here, our iterator would invalidate. The
+    // TimerInfo structure is copyable and since it is using the shared
+    // pointers the copy is not expensive. Also this function is called when
+    // the process terminates so it is not critical for performance.
+    TimerInfoMap registered_timers_copy(registered_timers_);
+
+    // Iterate over the existing timers and deregister them.
+    for (TimerInfoMap::iterator timer_info_it = registered_timers_copy.begin();
+         timer_info_it != registered_timers_copy.end(); ++timer_info_it) {
+        deregisterTimer(timer_info_it->first);
+    }
 }
 
 void
 TimerMgr::setup(const std::string& timer_name) {
+   // Check if the specified timer exists.
    TimerInfoMap::const_iterator timer_info_it = registered_timers_.find(timer_name);
    if (timer_info_it == registered_timers_.end()) {
        isc_throw(BadValue, "unable to setup timer '" << timer_name << "': "
                  "no such timer registered");
    }
 
+   // Schedule the execution of the timer using the parameters supplied
+   // during the registration.
    const TimerInfo& timer_info = timer_info_it->second;
-   timer_info.interval_timer_->setup(timer_info.callback_, timer_info.interval_,
+   timer_info.interval_timer_->setup(boost::bind(&TimerMgr::timerCallback, this, timer_name),
+                                     timer_info.interval_,
                                      timer_info.scheduling_mode_);
 }
 
 void
-TimerMgr::startThread() {
-    thread_.reset(new Thread(boost::bind(&IOService::run, &getIOService())));
+TimerMgr::cancel(const std::string& timer_name) {
+    // Find the timer of our interest.
+    TimerInfoMap::const_iterator timer_info_it = registered_timers_.find(timer_name);
+    if (timer_info_it == registered_timers_.end()) {
+        isc_throw(BadValue, "unable to cancel timer '" << timer_name << "': "
+                  "no such timer registered");
+    }
+    // Cancel the timer.
+    timer_info_it->second.interval_timer_->cancel();
 }
 
 void
-TimerMgr::stopThread() {
-    if (thread_) {
-        io_service_->post(boost::bind(&IOService::stop, &getIOService()));
-        thread_->wait();
-        thread_.reset();
-        io_service_->get_io_service().reset();
+TimerMgr::startThread() {
+    // Do not start the thread if the thread is already there.
+    if (!thread_) {
+        // The thread will simply run IOService::run(), which is a blocking call
+        // to keep running handlers for all timers according to how they have
+        // been scheduled.
+        thread_.reset(new Thread(boost::bind(&IOService::run, &getIOService())));
     }
 }
 
 void
-TimerMgr::localCallback(const std::string& timer_name) const {
+TimerMgr::stopThread() {
+    // If thread is not running, this is no-op.
+    if (thread_) {
+        // Stop the IO Service. This will break the IOService::run executed in the
+        // worker thread. The thread will now terminate.
+        getIOService().post(boost::bind(&IOService::stop, &getIOService()));
+        // Wait for the thread to terminate.
+        thread_->wait();
+        // Set the thread pointer to NULL to indicate that the thread is not running.
+        thread_.reset();
+        // IO Service has to be reset so as we can call run() on it again if we
+        // desire (using the startThread()).
+        getIOService().get_io_service().reset();
+    }
+}
+IOService&
+TimerMgr::getIOService() const {
+    // The IO service is now created internally by the TimerMgr and we don't allow
+    // overriding it with anything as there are currently no use cases for it.
+    // It is possible that someone may want to obtain this instance to use it
+    // for something else too, so we return a reference to a static object.
+    // If we decide to allow setting the IO service object we will have to
+    // replace this static object with a shared pointer allocated in the
+    // class constructor.
+    static asiolink::IOService io_service;
+    return (io_service);
+}
+
+
+void
+TimerMgr::timerCallback(const std::string& timer_name) {
+    // Run callback. Value of true says "mark socket ready".
+    watchSocketCallback(timer_name, true);
+}
+
+void
+TimerMgr::ifaceMgrCallback(const std::string& timer_name) {
+    // Run callback. Value of false says "clear ready socket".
+    watchSocketCallback(timer_name, false);
+}
+
+void
+TimerMgr::watchSocketCallback(const std::string& timer_name, const bool mark_ready) {
+    // Find the specified timer setup.
+    TimerInfoMap::iterator timer_info_it = registered_timers_.find(timer_name);
+    if (timer_info_it != registered_timers_.end()) {
+        TimerInfo& timer_info = timer_info_it->second;
+        // This is 'true' when we're executing a callback for the elapsed timer.
+        // It is supposed to mark watch socket ready - write data to a socket to
+        // interrupt the execution of the select() function. This is executed
+        // from the worker thrad and will likely block the thread until the socket
+        // is cleared.
+        if (mark_ready) {
+            timer_info.watch_socket_->markReady();
+
+        } else {
+            // We're executing a callback function from the Interface Manager.
+            // This callback function is executed when the call to select() is
+            // interrupted as a result of receiving some data over the watch
+            // socket. We have to clear the socket which has been marked as
+            // ready. Then execute the callback function supplied by the
+            // TimerMgr user to perform custom actions on the expiration of
+            // the given timer.
+            timer_info.watch_socket_->clearReady();
+            timer_info.user_callback_();
+        }
+    }
 }
 
 } // end of namespace isc::dhcp
