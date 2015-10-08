a31ef84dcb9362b0817dfb590ebd1b4b7f88b148
Shane Kerr <shane@time-travellers.org>
2015-10-08 10:57:27 +0000

    Build changes for C++11 (and perhaps log4cplus 2.x)

--- src/lib/dhcpsrv/pgsql_lease_mgr.cc
+++ src/lib/dhcpsrv/pgsql_lease_mgr.cc
@@ -1690,7 +1690,7 @@ PgSqlLeaseMgr::getVersion() const {
 
     PQclear(r);
 
-    return make_pair<uint32_t, uint32_t>(version, minor);
+    return pair<uint32_t, uint32_t>(version, minor);
 }
 
 void
