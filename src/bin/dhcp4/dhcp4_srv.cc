eade7b958d9ad19943be84c8f42513995bcf5f71
JINMEI Tatuya <jinmei@wide.ad.jp>
2015-11-01 10:45:08 +0900

    [4108] unrelated cleanup: catch an exception by reference.
    also folded a too long line.

--- src/bin/dhcp4/dhcp4_srv.cc
+++ src/bin/dhcp4/dhcp4_srv.cc
@@ -448,13 +448,14 @@ Dhcpv4Srv::run() {
                     .arg(timeout);
             }
 
-        } catch (const SignalInterruptOnSelect) {
+        } catch (const SignalInterruptOnSelect&) {
             // Packet reception interrupted because a signal has been received.
             // This is not an error because we might have received a SIGTERM,
             // SIGINT, SIGHUP or SIGCHILD which are handled by the server. For
             // signals that are not handled by the server we rely on the default
             // behavior of the system.
-            LOG_DEBUG(packet4_logger, DBG_DHCP4_DETAIL, DHCP4_BUFFER_WAIT_SIGNAL)
+            LOG_DEBUG(packet4_logger, DBG_DHCP4_DETAIL,
+                      DHCP4_BUFFER_WAIT_SIGNAL)
                 .arg(signal_set_->getNext());
         } catch (const std::exception& e) {
             // Log all other errors.
