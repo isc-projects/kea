a108ff7904829e60761ab6855758bad161758347
Francis Dupont <fdupont@isc.org>
2016-02-23 22:47:08 +0100

    [4307] Fixed CID 1202722

--- src/bin/d2/main.cc
+++ src/bin/d2/main.cc
@@ -1,4 +1,4 @@
-// Copyright (C) 2013-2015 Internet Systems Consortium, Inc. ("ISC")
+// Copyright (C) 2013-2016 Internet Systems Consortium, Inc. ("ISC")
 //
 // This Source Code Form is subject to the terms of the Mozilla Public
 // License, v. 2.0. If a copy of the MPL was not distributed with this
@@ -24,12 +24,12 @@ using namespace std;
 int main(int argc, char* argv[]) {
     int ret = EXIT_SUCCESS;
 
-    // Instantiate/fetch the DHCP-DDNS application controller singleton.
-    DControllerBasePtr& controller = D2Controller::instance();
-
     // Launch the controller passing in command line arguments.
     // Exit program with the controller's return code.
     try  {
+        // Instantiate/fetch the DHCP-DDNS application controller singleton.
+        DControllerBasePtr& controller = D2Controller::instance();
+
         // 'false' value disables test mode.
         controller->launch(argc, argv, false);
     } catch (const VersionMessage& ex) {
