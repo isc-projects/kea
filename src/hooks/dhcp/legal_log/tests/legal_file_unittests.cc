5ee6082fc7001f4c983f2042e5c68e603c62fb25
Thomas Markwalder <tmark@isc.org>
2016-05-05 15:34:43 -0400

    [4298] Test refactoring and added v4 callout tests
    src/hooks/dhcp/legal_log/lease4_cos.cc
        lease4_select()
        lease4_renew( -) Added try-catch on call to getContext()
    
    src/hooks/dhcp/legal_log/tests/test_utils.h
        New file which contains common test classes
    
    src/hooks/dhcp/legal_log/tests/legal_file_unittests.cc
        Removed common test class(es) now in test_utils.h
    
    src/hooks/dhcp/legal_log/tests/legal_log4_unittests.cc
        Now uses common test classes and added lease4 callout tests

--- src/hooks/dhcp/legal_log/tests/legal_file_unittests.cc
+++ src/hooks/dhcp/legal_log/tests/legal_file_unittests.cc
@@ -4,187 +4,21 @@
 // License, v. 2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
+/// @file Contains tests that exercise the basic functionality of the
+// class LegalFile.  These tests are independent of the hooks framework.
+
 #include <exceptions/exceptions.h>
-#include <legal_file.h>
+#include <test_utils.h>
 
 #include <gtest/gtest.h>
 
-#include "boost/date_time/posix_time/posix_time.hpp"
-#include <locale>
-#include <sys/stat.h>
+#include <boost/date_time/posix_time/posix_time.hpp>
 
 using namespace std;
 using namespace isc;
 using namespace legal_log;
 
 namespace {
-
-/// @brief Derivation of LegalFile which provides an overridden date mechanism
-/// This class overrides LegalFile::today() with an implementation that returns
-/// fixed value, rather than based on the actual system time.  This simplifies
-/// testing file rotation.
-class TestableLegalFile : public LegalFile {
-public:
-    /// @brief Constructor
-    ///
-    /// Create a LegalFile for the given file name without opening the file.
-    /// @param today - The date with which to override LegalFile::today()
-    ///
-    /// @throw LegalFileError if given file name is empty.
-    TestableLegalFile(boost::gregorian::date today)
-        : LegalFile(TEST_DATA_BUILDDIR, "legal"), today_(today) {
-    }
-
-    /// @brief Destructor.
-    ////
-    /// The destructor does call the close method.
-    virtual ~TestableLegalFile() {};
-
-    /// @brief Overrides LegalFile::today()
-    /// @return value of member variable today_
-    virtual boost::gregorian::date today() {
-        return (today_);
-    }
-
-    /// @brief Overrides LegalFile::now()
-    /// @return
-    virtual time_t now() {
-        struct tm now_tm = boost::gregorian::to_tm(today_);
-        return (mktime(&now_tm));
-    }
-
-    /// @brief Sets the override date value
-    /// @param - new value for the override date
-    void setToday(boost::gregorian::date day) {
-        today_ = day;
-    }
-    /// @brief Override date value
-    boost::gregorian::date today_;
-};
-
-/// @brief Test fixture for testing LegalFile.
-/// It provides tools for erasing test files, altering date values,
-/// generating file names, checking file existance and content.
-class LegalFileTest : public ::testing::Test {
-public:
-    /// @brief Constructor
-    /// Fetches the current day and removes files that may be left
-    /// over from previous tests
-    LegalFileTest() : today_(boost::gregorian::day_clock::local_day()) {
-        wipeFiles();
-    }
-
-    /// @brief Destructor
-    /// Removes files that may be left over from previous tests
-    virtual ~LegalFileTest() {
-        wipeFiles();
-    }
-
-    /// @brief Removes files that may be left over from previous tests
-    void wipeFiles() {
-        ::remove((genName(today_)).c_str());
-        ::remove((genName(adjustDay(today_, 1))).c_str());
-    }
-
-    /// @brief Returns a new date by adding given days to a given date
-    /// @param org_day - date to adjust
-    /// @param days - the number of days to add (may be negative)
-    /// @return - the new date
-    boost::gregorian::date adjustDay(const boost::gregorian::date& org_day,
-                                       int days) {
-        boost::gregorian::date_duration dd(days);
-        return (org_day + dd);
-    }
-
-    /// @brief Checks if the given file exists
-    ///
-    /// @return true if the file exists, false if it does not
-    bool fileExists(const std::string& filename) {
-        struct stat statbuf;
-        if (stat(filename.c_str(), &statbuf) == 0) {
-            return (true);
-        }
-
-        int sav_error = errno;
-        if (errno == ENOENT) {
-            return (false);
-        }
-
-        ADD_FAILURE() << "fileExists error - filename: " << filename
-            << " error: " << strerror(sav_error);
-        return (false);
-    }
-
-    /// @brief Generate a file name based on the given date
-    ///
-    /// Uses the same formatting as LegalFile to build file names
-    ///
-    /// @param day - date to use in the file name
-    /// @return - the generated file name
-    std::string genName(const boost::gregorian::date& day)  {
-        boost::gregorian::date::ymd_type ymd = day.year_month_day();
-        std::ostringstream stream;
-        stream << TEST_DATA_BUILDDIR << "/" << "legal" << "." << ymd.year
-               << std::right << std::setfill('0') << std::setw(2)
-               << ymd.month.as_number()
-               << ymd.day << ".txt";
-        return(stream.str());
-    }
-
-    /// @brief Check a file's contents against an expected set of lines
-    ///
-    /// Passes if the given file's content matches. Fails otherwise.
-    ///
-    /// @param file_name - name of the file to read
-    /// @param expected_lines - a vector of the lines expected to be found
-    /// in the file (entries DO NOT include EOL)
-    void checkFileLines(const std::string& file_name,
-                        const std::string& now_string,
-                        const std::vector<std::string>& expected_lines) {
-        std::ifstream is;
-        is.open(file_name);
-        ASSERT_TRUE(is.good()) << "Could not open file: " << file_name;
-
-        int i = 0;
-        while (!is.eof()) {
-            char buf[128];
-
-            is.getline(buf, sizeof(buf));
-            if (is.gcount() > 0) {
-                ASSERT_TRUE(i < expected_lines.size())
-                        << "Too many entries in file: " << file_name;
-
-                std::string cmp_line = now_string + " " + expected_lines[i];
-                ASSERT_EQ(cmp_line, buf)
-                        << "line mismatch in: " << file_name
-                        << " at line:" << i;
-
-                ++i;
-            }
-        }
-
-        ASSERT_EQ(i, expected_lines.size())
-                    << "Not enough entries in file: " << file_name;
-    }
-
-    /// @brief Returns the current timezone abbreviate as a string
-    std::string getTimezone()  {
-        char buffer[16];
-        time_t curtime;
-        time(&curtime);
-        struct tm* timeinfo;
-        timeinfo = localtime(&curtime);
-
-        strftime(buffer, sizeof(buffer), "%Z", timeinfo);
-        return (std::string(buffer));
-    }
-
-    /// @brief The current date
-    boost::gregorian::date today_;
-};
-
-/// @brief Defines a pointer to a TestableLegalFile
-typedef boost::shared_ptr<TestableLegalFile> TestableLegalFilePtr;
 
 /// @brief Tests the LegalFile constructor.
 TEST_F(LegalFileTest, invalidConstruction) {
--- src/hooks/dhcp/legal_log/tests/legal_log4_unittests.cc
+++ src/hooks/dhcp/legal_log/tests/legal_log4_unittests.cc
@@ -4,24 +4,41 @@
 // License, v. 2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
+/// @file This file contains tests which verify DHCPv4 legal file entry
+/// generation as well as tests which exercise the v4 callouts, themselves.
 #include <exceptions/exceptions.h>
 
 #include <dhcp/pkt4.h>
 #include <dhcpsrv/lease.h>
+#include <hooks/callout_manager.h>
+#include <hooks/library_handle.h>
+#include <hooks/hooks.h>
 #include <dhcp/option.h>
 #include <dhcp/option_custom.h>
 #include <dhcp/option_int_array.h>
+#include <test_utils.h>
 
 #include <gtest/gtest.h>
 
 using namespace std;
 using namespace isc;
 using namespace dhcp;
+using namespace hooks;
+using namespace legal_log;
+
+extern LegalFilePtr legal_file;
 
 extern std::string genLease4Entry(Pkt4Ptr query, Lease4Ptr lease, bool renewal);
 
-namespace {
+extern "C" {
+extern int load(LibraryHandle& handle);
+extern int unload();
+extern int pkt4_receive(CalloutHandle& handle);
+extern int lease4_select(CalloutHandle& handle);
+extern int lease4_renew(CalloutHandle& handle);
+}
 
+namespace {
 
 /// @brief Hardware address used by different tests.
 const uint8_t HWADDR[] = {0x08, 0x00, 0x2b, 0x02, 0x3f, 0x4e};
@@ -48,7 +65,7 @@ Lease4Ptr createLease4(const std::string& addr_str, uint32_t valid_lifetime,
     return(lease);
 };
 
-// Verifies DHCPv4 entries for directly connected clients
+// Verifies legal entry content for directly connected clients
 TEST(Lease4EntryTest, directClient) {
     HWAddrPtr hwaddr(new HWAddr(HWADDR, sizeof(HWADDR), HTYPE_ETHER));
     Lease4Ptr lease4 = createLease4("192.2.1.100", 6735, hwaddr, ClientIdPtr());
@@ -125,8 +142,8 @@ TEST(Lease4EntryTest, directClient) {
               entry);
 }
 
-// Verifies DHCPv4 entries for directly connected clients
-// Checks with and with RAI and its suboptions
+// Verifies legal entry content for relayed clients
+// Checks with and without RAI and its suboptions
 TEST(Lease4EntryTest, relayedClient) {
     HWAddrPtr hwaddr(new HWAddr(HWADDR, sizeof(HWADDR), HTYPE_ETHER));
     Lease4Ptr lease4 = createLease4("192.2.1.100", 6735, hwaddr, ClientIdPtr());
@@ -209,6 +226,164 @@ TEST(Lease4EntryTest, relayedClient) {
               " connected via relay at address: 192.2.16.33"
               ", identified by remote-id: 87:f6:79:77:ef",
               entry);
+}
+
+// Verifies that the load callout instantiates and opens the LegalFile and
+// that unload() resets it.
+// @todo When 4297 is complete, will need to expand this based on
+// parameters for path, basename, etc.  NOTE THAT once 4297 is done this
+// test must not use the default values (default_legal_path and default_legal_base)
+// as we do not want to risk altering a LIVE file.
+TEST_F(CalloutTest, loadAndUnload) {
+    LibraryHandle library(getCalloutManager().get());
+
+    // Invoke the callout.  It should succeed.
+    int ret;
+    EXPECT_NO_THROW(ret = load(library));
+    EXPECT_EQ(0, ret);
+
+    // Verify the legal_file was created and is open
+    ASSERT_TRUE(legal_file);
+    EXPECT_TRUE(legal_file->isOpen());
+
+    // Save the filename so we can remove it after unload.
+    std::string fname = legal_file->getFileName();
+
+    EXPECT_NO_THROW(ret = unload());
+    EXPECT_EQ(0, ret);
+    EXPECT_FALSE(legal_file);
+
+    ::remove(fname.c_str());
+}
+
+// Verifies that the pkt4_receive callout caches DHCPREQUEST packets
+TEST_F(CalloutTest, pkt4_receive) {
+    CalloutHandle handle(getCalloutManager());
+
+    // Create a DHCPDISCOVER and use it as the callout query4 argument.
+    Pkt4Ptr pkt4(new Pkt4(DHCPDISCOVER, 0x77));
+    handle.setArgument("query4", pkt4);
+
+    // Invoke the callout. It should succeed but not cache the packet.
+    int ret;
+    ASSERT_NO_THROW(ret = pkt4_receive(handle));
+    EXPECT_EQ(0, ret);
+
+    // Verify the context does NOT contain the discover.
+    Pkt4Ptr from_context;
+    ASSERT_THROW(handle.getContext("query4", from_context), NoSuchCalloutContext);
+
+    // Create a DHCPREQUEST and use it as the callout query4 argument.
+    pkt4.reset(new Pkt4(DHCPREQUEST, 0x77));
+    handle.setArgument("query4", pkt4);
+
+    // Invoke the callout. It should succeed and cache the packet.
+    ASSERT_NO_THROW(ret = pkt4_receive(handle));
+    EXPECT_EQ(0, ret);
+
+    // Verify the request was cached.
+    ASSERT_NO_THROW(handle.getContext("query4", from_context));
+    ASSERT_TRUE(from_context);
+    EXPECT_EQ(pkt4, from_context);
+}
+
+// Verifies that the lease4_select callout
+// -# Detects no LegalFile instance
+// -# Detects no Pkt4 in the callout context
+// -# Verifies the file content when given a Pkt4 and Lease4
+// Note we don't bother testing multple entries or rotation as this is done
+// during LegalFile testing.
+TEST_F(CalloutTest, lease4_select) {
+    // Make a callout handle
+    CalloutHandle handle(getCalloutManager());
+
+    // We should fail if there's no legal_file
+    int ret;
+    ASSERT_NO_THROW(ret = lease4_select(handle));
+    EXPECT_EQ(1, ret);
+
+    // Create the legal file
+    TestableLegalFilePtr tfile;
+    ASSERT_NO_THROW(tfile.reset(new TestableLegalFile(today_)));
+    legal_file = tfile;
+
+    // We should fail if there's a legal file but no context
+    // has no packet
+    ASSERT_NO_THROW(ret = lease4_select(handle));
+    EXPECT_EQ(1, ret);
+
+    // Make a packet and add it to the context
+    Pkt4Ptr pkt4(new Pkt4(DHCPREQUEST, 0x77));
+    handle.setContext("query4", pkt4);
+
+    // Make a lease and add it to the arguments
+    HWAddrPtr hwaddr(new HWAddr(HWADDR, sizeof(HWADDR), HTYPE_ETHER));
+    Lease4Ptr lease4 = createLease4("192.2.1.100", 6735, hwaddr, ClientIdPtr());
+    handle.setArgument("lease4", lease4);
+
+    // Call should succeed
+    ASSERT_NO_THROW(ret = lease4_select(handle));
+    EXPECT_EQ(0, ret);
+
+    // Close it to flush any unwritten data
+    tfile->close();
+
+    // Verify that the file content.
+    std::vector<std::string>lines;
+    lines.push_back("Address: 192.2.1.100 has been assigned for 1 hrs 52 min 15 secs"
+                    " to a device with hardware address:"
+                    " hwtype=1 08:00:2b:02:3f:4e");
+    std::string today_now_string = legal_file->getNowString();
+    checkFileLines(genName(today_), today_now_string, lines);
+}
+
+// Verifies that the lease4_renew callout
+// -# Detects no LegalFile instance
+// -# Detects no Pkt4 in the callout context
+// -# Verifies the file content when given a Pkt4 and Lease4
+// Note we don't bother testing multple entries or rotation as this is done
+// during LegalFile testing.
+TEST_F(CalloutTest, lease4_renew) {
+    // Make a callout handle
+    CalloutHandle handle(getCalloutManager());
+
+    // We should fail if there's no legal_file
+    int ret;
+    ASSERT_NO_THROW(ret = lease4_renew(handle));
+    EXPECT_EQ(1, ret);
+
+    // Create the legal file
+    TestableLegalFilePtr tfile;
+    ASSERT_NO_THROW(tfile.reset(new TestableLegalFile(today_)));
+    legal_file = tfile;
+
+    // We should fail if there's a legal file but no context
+    // has no packet
+    ASSERT_NO_THROW(ret = lease4_renew(handle));
+    EXPECT_EQ(1, ret);
+
+    // Make a packet and add it to the context
+    Pkt4Ptr pkt4(new Pkt4(DHCPREQUEST, 0x77));
+    handle.setContext("query4", pkt4);
+
+    // Make a lease and add it to the arguments
+    HWAddrPtr hwaddr(new HWAddr(HWADDR, sizeof(HWADDR), HTYPE_ETHER));
+    Lease4Ptr lease4 = createLease4("192.2.1.100", 6735, hwaddr, ClientIdPtr());
+    handle.setArgument("lease4", lease4);
+
+    // Call should succeed
+    ASSERT_NO_THROW(ret = lease4_renew(handle));
+    EXPECT_EQ(0, ret);
+
+    // Close it to flush any unwritten data
+    tfile->close();
+
+    std::vector<std::string>lines;
+    lines.push_back("Address: 192.2.1.100 has been renewed for 1 hrs 52 min 15 secs"
+                    " to a device with hardware address:"
+                    " hwtype=1 08:00:2b:02:3f:4e");
+    std::string today_now_string = legal_file->getNowString();
+    checkFileLines(genName(today_), today_now_string, lines);
 }
 
 } // end of anonymous namespace
