2fc5748d40356c45e07773230216a87a63dc2d8c
Thomas Markwalder <tmark@isc.org>
2016-05-09 13:28:50 -0400

    [4298] Added v6 callout tests
    src/hooks/dhcp/legal_log/lease6_cos.cc
        lease6_select()
        lease6_renew() - added try-catch to getContext() call
    
    src/hooks/dhcp/legal_log/tests/legal_log4_unittests.cc
     TEST_F(CalloutTest, pkt4_receive) - revamped to iterate
     over all possible message types
    
    src/hooks/dhcp/legal_log/tests/legal_log6_unittests.cc
        TEST_F(CalloutTest, pkt6_receive)
        TEST_F(CalloutTest, lease6_select)
        TEST_F(CalloutTest, lease6_renew) - new tests to
        verify v6 callouts

--- src/hooks/dhcp/legal_log/tests/legal_log6_unittests.cc
+++ src/hooks/dhcp/legal_log/tests/legal_log6_unittests.cc
@@ -4,6 +4,13 @@
 // License, v. 2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
+/// @file This file contains tests which verify DHCPv6 legal file entry
+/// generation as well as tests which exercise v6 callouts: pkt6_receive
+/// lease6_select, lease6_renew.  These tests assume the legal log library
+/// is linked in, not loaded.  This allows a great deal more flexiblity
+/// in testing, such as overriding and accessing the legal_file instance.
+/// The load and unload callouts are excercised in ../libloadtests, which
+/// actually uses the HooksManager to load and unload the library.
 #include <exceptions/exceptions.h>
 
 #include <dhcp/dhcp6.h>
@@ -12,6 +19,9 @@
 #include <dhcp/option.h>
 #include <dhcp/option_custom.h>
 #include <dhcp/option_int_array.h>
+#include <hooks/callout_manager.h>
+#include <hooks/hooks.h>
+#include <test_utils.h>
 
 #include <gtest/gtest.h>
 
@@ -21,6 +31,14 @@ using namespace dhcp;
 
 extern std::string genLease6Entry(Pkt6Ptr query, Lease6Ptr lease, bool renewal);
 extern std::string hwaddrSourceToString(uint32_t source);
+
+extern LegalFilePtr legal_file;
+
+extern "C" {
+extern int pkt6_receive(CalloutHandle& handle);
+extern int lease6_select(CalloutHandle& handle);
+extern int lease6_renew(CalloutHandle& handle);
+}
 
 namespace {
 
@@ -191,5 +209,145 @@ TEST(Lease6EntryTest, relayedClient) {
               ", identified by subscriber-id: 1a:2b:3c:4d:5e:6f",
               entry);
 }
+
+// Verifies that the pkt6_receive callout caches DHCPREQUEST packets
+TEST_F(CalloutTest, pkt6_receive) {
+
+    CalloutHandle handle(getCalloutManager());
+    for (int i = 0; i < 23; i++) {
+        // Create a v6 packet with current type.
+        Pkt6Ptr pkt6;
+        try {
+            pkt6.reset(new Pkt6(i, 0x77));
+        } catch (...) {
+            // invalid types won't construct, skip em
+            continue;
+        }
+        handle.setArgument("query6", pkt6);
+
+        // Invoke the callout which should always succeed.
+        int ret;
+        ASSERT_NO_THROW(ret = pkt6_receive(handle));
+        EXPECT_EQ(0, ret);
+
+        Pkt6Ptr from_context;
+        if (i == DHCPV6_REQUEST || i == DHCPV6_RENEW || i == DHCPV6_REBIND) {
+            // Verify the request was cached.
+            ASSERT_NO_THROW(handle.getContext("query6", from_context));
+            ASSERT_TRUE(from_context);
+            EXPECT_EQ(pkt6, from_context);
+            handle.deleteContext("query6");
+        } else {
+            // Verify the context does NOT contain the discover.
+            ASSERT_THROW(handle.getContext("query6", from_context),
+                         NoSuchCalloutContext) << " found when i is:" << i;
+        }
+    }
+}
+
+// Verifies that the lease6_select callout
+// -# Detects no LegalFile instance
+// -# Detects no Pkt6 in the callout context
+// -# Verifies the file content when given a Pkt6 and Lease6
+// Note we don't bother testing multple entries or rotation as this is done
+// during LegalFile testing.
+TEST_F(CalloutTest, lease6_select) {
+    // Make a callout handle
+    CalloutHandle handle(getCalloutManager());
+
+    // We should fail if there's no legal_file
+    int ret;
+    ASSERT_NO_THROW(ret = lease6_select(handle));
+    EXPECT_EQ(1, ret);
+
+    // Create the legal file
+    TestableLegalFilePtr tfile;
+    ASSERT_NO_THROW(tfile.reset(new TestableLegalFile(today_)));
+    legal_file = tfile;
+
+    // We should fail if there's a legal file but the context
+    // has no packet
+    ASSERT_NO_THROW(ret = lease6_select(handle));
+    EXPECT_EQ(1, ret);
+
+    // Make a packet and add it to the context
+    Pkt6Ptr pkt6(new Pkt6(DHCPV6_REQUEST, 0x77));
+    handle.setContext("query6", pkt6);
+
+    // Make a lease and add it to the arguments
+    DuidPtr duid1(new DUID(DUID1, sizeof(DUID1)));
+    HWAddrPtr hwaddr(new HWAddr(HWADDR, sizeof(HWADDR), HTYPE_ETHER));
+    hwaddr->source_ = HWAddr::HWADDR_SOURCE_RAW;
+    Lease6Ptr lease6 = createLease6(duid1, Lease::TYPE_NA, "2001:db8:1::", 0,
+                                    713, HWAddrPtr());
+    handle.setArgument("lease6", lease6);
+
+    // Call should succeed
+    ASSERT_NO_THROW(ret = lease6_select(handle));
+    EXPECT_EQ(0, ret);
+
+    // Close it to flush any unwritten data
+    tfile->close();
+
+    // Verify that the file content.
+    std::vector<std::string>lines;
+    lines.push_back("Address:2001:db8:1:: has been assigned for 0 hrs 11 min 53 secs"
+                    " to a device with DUID: 17:34:e2:ff:09:92:54");
+    std::string today_now_string = legal_file->getNowString();
+    checkFileLines(genName(today_), today_now_string, lines);
+}
+
+// Verifies that the lease6_renew callout
+// -# Detects no LegalFile instance
+// -# Detects no Pkt6 in the callout context
+// -# Verifies the file content when given a Pkt6 and Lease6
+// Note we don't bother testing multple entries or rotation as this is done
+// during LegalFile testing.
+TEST_F(CalloutTest, lease6_renew) {
+    // Make a callout handle
+    CalloutHandle handle(getCalloutManager());
+
+    // We should fail if there's no legal_file
+    int ret;
+    ASSERT_NO_THROW(ret = lease6_renew(handle));
+    EXPECT_EQ(1, ret);
+
+    // Create the legal file
+    TestableLegalFilePtr tfile;
+    ASSERT_NO_THROW(tfile.reset(new TestableLegalFile(today_)));
+    legal_file = tfile;
+
+    // We should fail if there's a legal file but the context
+    // has no packet
+    ASSERT_NO_THROW(ret = lease6_renew(handle));
+    EXPECT_EQ(1, ret);
+
+    // Make a packet and add it to the context
+    Pkt6Ptr pkt6(new Pkt6(DHCPV6_REBIND, 0x77));
+    handle.setContext("query6", pkt6);
+
+    // Make a lease and add it to the arguments
+    DuidPtr duid1(new DUID(DUID1, sizeof(DUID1)));
+    HWAddrPtr hwaddr(new HWAddr(HWADDR, sizeof(HWADDR), HTYPE_ETHER));
+    hwaddr->source_ = HWAddr::HWADDR_SOURCE_RAW;
+    Lease6Ptr lease6 = createLease6(duid1, Lease::TYPE_NA, "2001:db8:1::", 0,
+                                    713, HWAddrPtr());
+    handle.setArgument("lease6", lease6);
+
+    // Call should succeed
+    ASSERT_NO_THROW(ret = lease6_renew(handle));
+    EXPECT_EQ(0, ret);
+
+    // Close it to flush any unwritten data
+    tfile->close();
+
+    // Verify that the file content.
+    std::vector<std::string>lines;
+    lines.push_back("Address:2001:db8:1:: has been renewed for 0 hrs 11 min 53 secs"
+                    " to a device with DUID: 17:34:e2:ff:09:92:54");
+    std::string today_now_string = legal_file->getNowString();
+    checkFileLines(genName(today_), today_now_string, lines);
+}
+
 
 } // end of anonymous namespace
