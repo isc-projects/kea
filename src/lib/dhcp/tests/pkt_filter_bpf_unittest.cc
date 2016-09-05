3c0afa727a51c69999a68d0d477c9225a00697ec
Marcin Siodelski <marcin@isc.org>
2016-09-05 14:23:06 +0200

    [5003] Test local port of the received message in pkt filter tests.

--- src/lib/dhcp/tests/pkt_filter_bpf_unittest.cc
+++ src/lib/dhcp/tests/pkt_filter_bpf_unittest.cc
@@ -1,4 +1,4 @@
-// Copyright (C) 2014-2015 Internet Systems Consortium, Inc. ("ISC")
+// Copyright (C) 2014-2016 Internet Systems Consortium, Inc. ("ISC")
 //
 // This Source Code Form is subject to the terms of the Mozilla Public
 // License, v. 2.0. If a copy of the MPL was not distributed with this
@@ -192,6 +192,7 @@ TEST_F(PktFilterBPFTest, DISABLED_receive) {
 
     // Check if the received message is correct.
     testRcvdMessage(rcvd_pkt);
+    testRcvdMessageAddressPort(rcvd_pkt);
 }
 
 // This test verifies that if the packet is received over the raw
