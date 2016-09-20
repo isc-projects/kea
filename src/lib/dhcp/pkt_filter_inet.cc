55abe9b3ff79a61647613698b07bf5b671a2e171
Tomek Mrugalski <tomasz@isc.org>
2016-09-20 11:37:02 +0200

    [5009] Compilation fixes for Linux

--- src/lib/dhcp/pkt_filter_inet.cc
+++ src/lib/dhcp/pkt_filter_inet.cc
@@ -256,7 +256,7 @@ PktFilterInet::send(const Iface&, uint16_t sockfd,
     struct in_pktinfo* pktinfo =(struct in_pktinfo *)CMSG_DATA(cmsg);
     memset(pktinfo, 0, sizeof(struct in_pktinfo));
     pktinfo->ipi_ifindex = pkt->getIndex();
-    pktinfo->ipi_spec_dst.s_addr = htonl(pkt->getLocalAddr()); // set the source IP address
+    pktinfo->ipi_spec_dst.s_addr = htonl(pkt->getLocalAddr().toUint32()); // set the source IP address
     m.msg_controllen = CMSG_SPACE(sizeof(struct in_pktinfo));
 #endif
 
