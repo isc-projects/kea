cb8f8ae36e8236e8181dac7dee212295d47a5778
Thomas Markwalder <tmark@isc.org>
2016-12-08 14:56:17 -0500

    [5046] Fixed logger setup on set-config
    src/bin/dhcp6/ctrl_dhcp6_srv.cc
        ControlledDhcpv6Srv::commandSetConfigHandler()
        - Add logger config
        - Use processConfig() directly instead of config-reload command
    
    src/bin/dhcp6/kea_controller.cc
        configure(const std::string& file_name)
          - Remove logger config
          - Use set-config command instead of config-reload
    
    src/bin/dhcp6/tests/ctrl_dhcp6_srv_unittest.cc
        createUnixChannelServer()
        - added call to initLogger() to revert logging to unit test logger
    
    src/bin/dhcp6/tests/dhcp6_test_utils.cc
        BaseServerTest::~BaseServerTest() {
        - added call to initLogger() to revert logging to unit test logger

--- src/bin/dhcp6/tests/dhcp6_test_utils.cc
+++ src/bin/dhcp6/tests/dhcp6_test_utils.cc
@@ -10,6 +10,7 @@
 #include <dhcp6/tests/dhcp6_test_utils.h>
 #include <dhcp6/json_config_parser.h>
 #include <dhcp/tests/pkt_captures.h>
+#include <log/logger_support.h>
 #include <util/pointer_util.h>
 #include <cc/command_interpreter.h>
 #include <stats/stats_mgr.h>
@@ -46,6 +47,9 @@ BaseServerTest::~BaseServerTest() {
 
     // Revert to original data directory.
     CfgMgr::instance().setDataDir(original_datadir_);
+
+    // Revert to unit test logging
+    isc::log::initLogger();
 }
 
 Dhcpv6SrvTest::Dhcpv6SrvTest()
