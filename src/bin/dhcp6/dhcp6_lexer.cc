bae9fe5f29ede1341de0411de0249121b461fa25
Francis Dupont <fdupont@isc.org>
2017-02-01 14:29:08 +0000

    [trac5110] regen flex/bison files using flex 2.6.3

--- src/bin/dhcp6/dhcp6_lexer.cc
+++ src/bin/dhcp6/dhcp6_lexer.cc
@@ -4753,7 +4753,7 @@ Parser6Context::scanStringBegin(const std::string& str, ParserType parser_type)
     loc_.initialize(&file_);
     yy_flex_debug = trace_scanning_;
     YY_BUFFER_STATE buffer;
-    buffer = yy_scan_bytes(str.c_str(), str.size());
+    buffer = parser6__scan_bytes(str.c_str(), str.size());
     if (!buffer) {
         fatal("cannot scan string");
         // fatal() throws an exception so this can't be reached
