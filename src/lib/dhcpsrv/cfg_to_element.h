4f625bc49b5b3873fc07d6b81611edda91b733b0
Francis Dupont <fdupont@isc.org>
2017-03-03 00:48:05 +0100

    [1205a] Removed the flags parameter

--- src/lib/dhcpsrv/cfg_to_element.h
+++ src/lib/dhcpsrv/cfg_to_element.h
@@ -9,7 +9,6 @@
 
 #include <exceptions/exceptions.h>
 #include <cc/data.h>
-#include <stdint.h>
 
 namespace isc {
 
@@ -24,28 +23,6 @@ public:
 };
 
 namespace dhcp {
-
-/// @name Constants for unparsing styles
-///
-//@{
-/// Default
-static const uint32_t TO_ELEMENT_DEFAULT        = 0x00000000;
-/// Add default values
-static const uint32_t TO_ELEMENT_ADD_DEFAULTS   = 0x00000001;
-/// Ignore disables
-static const uint32_t TO_ELEMENT_IGNORE_DISABLE = 0x00000002;
-/// Don't copy
-static const uint32_t TO_ELEMENT_DO_NOT_COPY    = 0x00000004;
-
-/// Get option names
-static const uint32_t TO_ELEMENT_OPTION_NAMES   = 0x00000100;
-/// Get ranges as prefixs
-static const uint32_t TO_ELEMENT_TRY_PREFIXES   = 0x00000200;
-
-/// Skip (vs throw) on errors
-static const uint32_t TO_ELEMENT_SKIP_ERRORS    = 0x80000000;
-//@}
-
 
 /// @brief Abstract class for configuration Cfg_* classes
 ///
@@ -62,11 +39,9 @@ struct CfgToElement {
     /// @endcode
     ///
     /// @param extras extra arguments
-    /// @param flags a bitfield which can be used to modify the result
     /// @return a pointer to a configuration which can be parsed into
     /// the initial configuration object
-    virtual isc::data::ElementPtr toElement(Args... extras,
-                                            const uint32_t flags) const = 0;
+    virtual isc::data::ElementPtr toElement(Args... extras) const = 0;
 };
 
 }; // namespace isc::dhcp
