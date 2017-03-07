67f0a792b87b863812206e0a7078d460c23816ed
Tomek Mrugalski <tomasz@isc.org>
2017-03-07 22:29:55 +0100

    [5137] shell tests implemented.

---
+++ src/bin/shell/tests/kea-shell_unittest.py
@@ -0,0 +1,21 @@
+# Copyright (C) 2017 Internet Systems Consortium, Inc. ("ISC")
+#
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+import unittest
+
+class CARequestUnitTest(unittest.TestCase):
+    def setUp(self):
+        print ("Setting up")
+
+    def runTest(self):
+        print ("Run test")
+
+    def tearDown(self):
+        print ("Tearing down")
+
+
+if __name__ == '__main__':
+    unittest.main()
