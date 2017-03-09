f0494b2149c9aba03484c5c03ccd6762fe1ba75b
Francis Dupont <fdupont@isc.org>
2017-03-09 01:17:03 +0100

    [5137] A few fixes and many python style improvements

--- src/bin/shell/kea_connector3.py
+++ src/bin/shell/kea_connector3.py
@@ -1,20 +1,29 @@
-#!/usr/bin/python
+# Copyright (C) 2017 Internet Systems Consortium, Inc. ("ISC")
+#
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
-from kea_conn import CARequest, CAResponse
+"""
+This is PYTHON 3.x version of HTTP connection establishment
+"""
 
 import urllib.request
 
-class KeaConnector:
-    def sendCA(self, params):
-        # Estalbish HTTP connection first.
-        url = "http://" + params.http_host + ":" + str(params.http_port) + str(params.path)
-        
-        req = urllib.request.Request(url = url, data = str.encode(params.content),
-                                     headers = params.headers)
-        resp = urllib.request.urlopen(req)
+from kea_conn import CAResponse # CARequest
 
-        # Now get the response details, put it in CAResponse and
-        # return it
-        x = CAResponse(resp.getcode(), resp.reason, resp.read().decode("utf-8"))
+def send_to_control_agent(params):
+    """Establish HTTP connection first."""
+    url = "http://" + params.http_host + ":"
+    url += str(params.http_port) + str(params.path)
 
-        return (x)
+    req = urllib.request.Request(url=url,
+                                 data=str.encode(params.content),
+                                 headers=params.headers)
+    resp = urllib.request.urlopen(req)
+
+    # Now get the response details, put it in CAResponse and return it
+    result = CAResponse(resp.getcode(), resp.reason,
+                        resp.read().decode("utf-8"))
+
+    return result
