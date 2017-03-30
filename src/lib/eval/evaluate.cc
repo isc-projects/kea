7ed2e66084767c4922dd94854d6ecf2df47d4280
Francis Dupont <fdupont@isc.org>
2017-03-30 21:37:12 +0200

    [5132] spelling

--- src/lib/eval/evaluate.cc
+++ src/lib/eval/evaluate.cc
@@ -17,7 +17,7 @@ bool evaluateBool(const Expression& expr, Pkt& pkt) {
     }
     if (values.size() != 1) {
         isc_throw(EvalBadStack, "Incorrect stack order. Expected exactly "
-                  "1 value at the end of evaluatuion, got " << values.size());
+                  "1 value at the end of evaluation, got " << values.size());
     }
     return (Token::toBool(values.top()));
 }
