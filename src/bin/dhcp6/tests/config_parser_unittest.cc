f14460ea8231bd9803099c94cf10e09ca13460de
Francis Dupont <fdupont@isc.org>
2017-04-18 07:21:47 +0200

    [4501] Fixed outsideSubnetPool tests where error location was moved

--- src/bin/dhcp6/tests/config_parser_unittest.cc
+++ src/bin/dhcp6/tests/config_parser_unittest.cc
@@ -5219,10 +5219,10 @@ TEST_F(Dhcp6ParserTest, outsideSubnetPool) {
     ASSERT_NO_THROW(text = comment->stringValue());
 
     EXPECT_EQ(1, rcode);
-    string expected = "subnet configuration failed: "
+    string expected = "subnet configuration failed (<string>:6:14): "
         "a pool of type IA_NA, with the following address range: "
         "2001:db8::-2001:db8::ffff does not match the prefix of a subnet: "
-        "2001:dc8::/32 to which it is being added (<string>:6:14)";
+        "2001:dc8::/32 to which it is being added";
     EXPECT_EQ(expected, text);
 }
 
