3f740f4ab5cfab8afd065293e5b0972c77bde1d8
Tomek Mrugalski <tomasz@isc.org>
2017-04-24 14:19:19 +0200

    [5208a] Doc written, exception messages are now better formed.

--- src/lib/dhcpsrv/host_mgr.cc
+++ src/lib/dhcpsrv/host_mgr.cc
@@ -216,8 +216,8 @@ HostMgr::get6(const SubnetID& subnet_id,
 void
 HostMgr::add(const HostPtr& host) {
     if (!alternate_source_) {
-        isc_throw(NoHostDataSourceManager, "unable to add new host because there is "
-                  "no alternate host data source present");
+        isc_throw(NoHostDataSourceManager, "Unable to add new host because there is "
+                  "no hosts-database configured.");
     }
     alternate_source_->add(host);
 }
@@ -225,8 +225,8 @@ HostMgr::add(const HostPtr& host) {
 bool
 HostMgr::del(const SubnetID& subnet_id, const asiolink::IOAddress& addr) {
     if (!alternate_source_) {
-        isc_throw(NoHostDataSourceManager, "unable to delete a host because there is "
-                  "no alternate host data source present");
+        isc_throw(NoHostDataSourceManager, "Unable to delete a host because there is "
+                  "no hosts-database configured.");
     }
 
     return (alternate_source_->del(subnet_id, addr));
@@ -236,8 +236,8 @@ bool
 HostMgr::del4(const SubnetID& subnet_id, const Host::IdentifierType& identifier_type,
               const uint8_t* identifier_begin, const size_t identifier_len) {
     if (!alternate_source_) {
-        isc_throw(NoHostDataSourceManager, "unable to delete a host because there is "
-                  "no alternate host data source present");
+        isc_throw(NoHostDataSourceManager, "Unable to delete a host because there is "
+                  "no hosts-database configured.");
     }
 
     return (alternate_source_->del4(subnet_id, identifier_type,
