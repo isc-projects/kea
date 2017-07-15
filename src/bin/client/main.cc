383d0c2c4b17aaae623f9144ffe1efcb5572ee54
Tomek <tomasz.mrugalski@gmail.com>
2017-07-15 13:22:43 +0200

    [client] Initial client files

---
+++ src/bin/client/main.cc
@@ -0,0 +1,39 @@
+// Copyright (C) 2017 Internet Systems Consortium, Inc. ("ISC")
+//
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+#include <config.h>
+#include <client/clnt_controller.h>
+#include <exceptions/exceptions.h>
+
+#include <iostream>
+
+using namespace isc::client;
+using namespace isc::process;
+using namespace std;
+
+/// This file contains entry point (main() function) for standard DHCP-DDNS
+/// process, kea-dhcp-ddns, component of Kea software suite.  It fetches
+/// the D2Controller singleton instance and invokes its launch method.
+/// The exit value of the program will be EXIT_SUCCESS if there were no
+/// errors, EXIT_FAILURE otherwise.
+int main(int argc, char* argv[]) {
+    int ret = EXIT_SUCCESS;
+
+    // Launch the controller passing in command line arguments.
+    // Exit program with the controller's return code.
+    try  {
+        // Instantiate/fetch the client application controller singleton.
+        DControllerBasePtr& controller = ClntController::instance();
+
+        // 'false' value disables test mode.
+        controller->launch(argc, argv, false);
+    } catch (const isc::Exception& ex) {
+        std::cerr << "Service failed: " << ex.what() << std::endl;
+        ret = EXIT_FAILURE;
+    }
+
+    return (ret);
+}
