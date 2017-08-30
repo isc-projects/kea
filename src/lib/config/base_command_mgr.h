349989ba16cb2abc1b6e59a284661aa0726135f0
Thomas Markwalder <tmark@isc.org>
2017-08-30 10:27:55 -0400

    [5111] Added "command-processed" hook point to BaseCommandMgr
    src/lib/config/base_command_mgr.cc
     - BaseCommandMgr::BaseCommandMgr() - registers command-processed hookpoint
     - BaseCommandMgr::processCommand() - invokes command-processed callouts
    
    src/lib/config/tests/command_mgr_unittests.cc
      - Cleanup and renaming to avoid confusion
      - Added new tests to verify hookpoint callout
          TEST_F(CommandMgrTest, commandProcessedHook)
          TEST_F(CommandMgrTest, commandProcessedHookReplaceResponse)

--- src/lib/config/base_command_mgr.h
+++ src/lib/config/base_command_mgr.h
@@ -97,6 +97,7 @@ public:
 
     /// @brief Constructor.
     ///
+    /// Registers hookpoint "command-processed"
     /// Registers "list-commands" command.
     BaseCommandMgr();
 
@@ -107,6 +108,8 @@ public:
     ///
     /// This method processes specified command. The command is specified using
     /// a single Element. See @ref BaseCommandMgr for description of its syntax.
+    /// After the command has been handled, callouts for the hook point,
+    /// "command-processed" will be invoked.
     ///
     /// @param cmd Pointer to the data element representing command in JSON
     /// format.
@@ -191,6 +194,9 @@ private:
     isc::data::ConstElementPtr
     listCommandsHandler(const std::string& name,
                         const isc::data::ConstElementPtr& params);
+
+    /// @brief Index of command-processed hook point
+    int hook_index_command_processed_;
 };
 
 } // end of namespace isc::config
