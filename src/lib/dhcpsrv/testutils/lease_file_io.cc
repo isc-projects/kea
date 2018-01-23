d3092a3d1790d263f2ec8abdde5afa9d41715e3e
Tomek Mrugalski <tomasz@isc.org>
2018-01-24 00:14:03 +0100

    [github36] lease_file_io.cc|h moved.

--- src/lib/dhcpsrv/testutils/lease_file_io.cc
+++ src/lib/dhcpsrv/testutils/lease_file_io.cc
@@ -4,7 +4,7 @@
 // License, v. 2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
-#include <dhcpsrv/tests/lease_file_io.h>
+#include <dhcpsrv/testutils/lease_file_io.h>
 #include <fstream>
 #include <sstream>
 
