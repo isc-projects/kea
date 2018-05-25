c76214b93ce35fbb02d4163173ab663d191b5825
Thomas Markwalder <tmark@isc.org>
2018-05-25 11:12:30 -0400

    [5596] renew-timer and rebind-timer now optional for DHCPv4
    v4 Parsing now allows renew/rebind timers to be unspecified,
    and kea-dhcp4 logic now matchs ISC DHCP:
    
    1. Send T2 only if it is specified and is less than lease lifetime
    2. Send T1 only if it is specified and is less than either T2
    if specified, or lease lifetime in the absence of T2
    
    doc/guide/dhcp4-srv.xml
        Updated discussion of rebind/renew-timers
    
    src/bin/dhcp4/dhcp4_srv.cc
        Dhcpv4Srv::assignLease() - modified to sanity check
        T1 and T2
    
    src/bin/dhcp4/tests/config_parser_unittest.cc
    src/bin/dhcp4/tests/get_config_unittest.cc
    src/bin/dhcp4/tests/simple_parser4_unittest.cc
    src/bin/dhcp6/tests/get_config_unittest.cc
    src/lib/dhcpsrv/tests/cfg_shared_networks4_unittest.cc
    src/lib/dhcpsrv/tests/cfg_shared_networks6_unittest.cc
        updated numerous tests
    
    src/lib/dhcpsrv/network.cc
        Network::toElement() - only outputs T1, T2, and
        valid lifetime if they are specified
    
    src/lib/dhcpsrv/network.h
        Network() - inits t1_, t2_, and valid_ to unspecified
        Triplet value
    
    src/lib/dhcpsrv/parsers/dhcp_parsers.cc
        Subnet4ConfigParser::initSubnet() - allow renew-timer and
        rebind-timer to be optional
    
    src/lib/dhcpsrv/parsers/simple_parser4.cc
        SimpleDefaults SimpleParser4::GLOBAL4_DEFAULTS - removed
        entries for renew-timer and rebind-timer

--- src/lib/dhcpsrv/network.cc
+++ src/lib/dhcpsrv/network.cc
@@ -136,18 +136,26 @@ Network::toElement() const {
         map->set("require-client-classes", class_list);
     }
 
-    // Set renew-timer
-    map->set("renew-timer",
-             Element::create(static_cast<long long>
-                                 (getT1().get())));
+    // T1, T2, and Valid are optional for SharedNetworks, and
+    // T1 and T2 are optional for Subnet4 thus we will only
+    // output them if they are marked as specified.
+    if (!getT1().unspecified()) {
+        map->set("renew-timer",
+                 Element::create(static_cast<long long>(getT1().get())));
+    }
+
     // Set rebind-timer
-    map->set("rebind-timer",
-             Element::create(static_cast<long long>
-                                 (getT2().get())));
+    if (!getT2().unspecified()) {
+        map->set("rebind-timer",
+                 Element::create(static_cast<long long>(getT2().get())));
+    }
+
     // Set valid-lifetime
-    map->set("valid-lifetime",
-             Element::create(static_cast<long long>
+    if (!getValid().unspecified()) {
+        map->set("valid-lifetime",
+                 Element::create(static_cast<long long>
                                  (getValid().get())));
+    }
 
     // Set reservation mode
     Network::HRMode hrmode = getHostReservationMode();
