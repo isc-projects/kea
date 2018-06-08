997e43ad434b5dadd1b7d5d486d5e3efed4071ef
Marcin Siodelski <marcin@isc.org>
2018-06-08 12:37:16 +0200

    [5645] Moved HA hook library from premium repo to main repo.

---
+++ src/hooks/dhcp/high_availability/ha_log.cc
@@ -0,0 +1,17 @@
+// Copyright (C) 2017-2018 Internet Systems Consortium, Inc. ("ISC")
+//
+// This Source Code Form is subject to the terms of the End User License
+// Agreement. See COPYING file in the premium/ directory.
+
+#include <config.h>
+
+#include <ha_log.h>
+
+namespace isc {
+namespace ha {
+
+isc::log::Logger ha_logger("ha-hooks");
+
+} // end of namespace isc::ha
+} // end of namespace isc
+
---
+++ src/hooks/dhcp/high_availability/ha_log.h
@@ -0,0 +1,22 @@
+// Copyright (C) 2017 Internet Systems Consortium, Inc. ("ISC")
+//
+// This Source Code Form is subject to the terms of the End User License
+// Agreement. See COPYING file in the premium/ directory.
+
+#ifndef HA_LOG_H
+#define HA_LOG_H
+
+#include <log/logger_support.h>
+#include <log/macros.h>
+#include <ha_messages.h>
+
+namespace isc {
+namespace ha {
+
+extern isc::log::Logger ha_logger;
+
+} // end of isc::ha
+} // end of isc namespace
+
+
+#endif
