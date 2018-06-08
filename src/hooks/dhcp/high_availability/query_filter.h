d1ed415c9559972ed86b67ceed1821f4301abf3f
Marcin Siodelski <marcin@isc.org>
2018-06-08 13:01:06 +0200

    [5645] Changed copyright headers to MPL.

--- src/hooks/dhcp/high_availability/query_filter.h
+++ src/hooks/dhcp/high_availability/query_filter.h
@@ -1,7 +1,8 @@
 // Copyright (C) 2018 Internet Systems Consortium, Inc. ("ISC")
 //
-// This Source Code Form is subject to the terms of the End User License
-// Agreement. See COPYING file in the premium/ directory.
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
 #ifndef QUERY_FILTER_H
 #define QUERY_FILTER_H
