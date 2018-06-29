79d30f3bf2f0e43c40f215bd82b4603aa8e15cb5
Francis Dupont <fdupont@isc.org>
2018-06-29 14:05:45 +0200

    [master] spelling

--- src/lib/dhcpsrv/cfg_subnets6.h
+++ src/lib/dhcpsrv/cfg_subnets6.h
@@ -81,7 +81,7 @@ public:
     ///
     /// The const pointer is returned by this method to prevent a caller from
     /// modifying the subnet configuration. Modifications to subnet configuration
-    /// is dangerous and must be done carefully. The subnets' configruation is
+    /// is dangerous and must be done carefully. The subnets' configuration is
     /// held in the multi index container and any modifications to the subnet
     /// id or subnet prefix must trigger re-indexing of multi index container.
     /// There is no possibility to enforce this when the non-const pointer is
