afdba503ba931f62e050aa668c4f6aa87d8fa500
Thomas Markwalder <tmark@isc.org>
2018-08-02 15:43:56 -0400

    [5704] Added subnet-id = 0 unit tests to host data sources
        Added tests to verify backends support storing and fetching
        reservations with either v4 or v6 subnet-id values of 0.

--- src/lib/dhcpsrv/tests/mysql_host_data_source_unittest.cc
+++ src/lib/dhcpsrv/tests/mysql_host_data_source_unittest.cc
@@ -285,6 +285,18 @@ TEST_F(MySqlHostDataSourceTest, basic4HWAddr) {
     testBasic4(Host::IDENT_HWADDR);
 }
 
+// Verifies that IPv4 host reservation with options can have a value of zero
+// dhcp4_subnet id
+TEST_F(MySqlHostDataSourceTest, zeroSubnetId4) {
+    testMaxSubnetId4();
+}
+
+// Verifies that IPv6 host reservation with options can have a value of zero
+// dhcp6_subnet id
+TEST_F(MySqlHostDataSourceTest, zeroSubnetId6) {
+    testMaxSubnetId6();
+}
+
 // Verifies that IPv4 host reservation with options can have a max value
 // for  dhcp4_subnet id
 TEST_F(MySqlHostDataSourceTest, maxSubnetId4) {
