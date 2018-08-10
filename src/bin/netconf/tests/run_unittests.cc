43a491c55d92c01653d36ce2f5948f9fdeb8c239
Tomek Mrugalski <tomasz@isc.org>
2018-08-10 19:16:12 +0200

    [gitlab9] Skeleton unit-tests for kea-netconf implemented.

--- src/bin/netconf/tests/run_unittests.cc
+++ src/bin/netconf/tests/run_unittests.cc
@@ -6,21 +6,15 @@
 
 #include <config.h>
 
-#include <log/logger_support.h>
-#include <dhcp4/dhcp4_log.h>
 #include <gtest/gtest.h>
+#include <util/unittests/run_all.h>
+#include <log/logger_support.h>
 
 int
 main(int argc, char* argv[]) {
-
     ::testing::InitGoogleTest(&argc, argv);
 
-    // See the documentation of the KEA_* environment variables in
-    // src/lib/log/README for info on how to tweak logging
     isc::log::initLogger();
 
-    setenv("KEA_PIDFILE_DIR", TEST_DATA_BUILDDIR, 1);
-    int result = RUN_ALL_TESTS();
-
-    return (result);
+    return (isc::util::unittests::run_all());
 }
