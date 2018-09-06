3bc730d19a3eba1467766e4cfb209da4b2530784
Marcin Siodelski <marcin@isc.org>
2018-09-06 17:34:51 +0200

    [#28,!20] Moved backend and server selector classes to database library.

--- src/lib/config_backend/base_config_backend_pool.h
+++ src/lib/config_backend/base_config_backend_pool.h
@@ -9,8 +9,9 @@
 
 #include <cc/data.h>
 #include <config_backend/base_config_backend.h>
+#include <database/backend_selector.h>
 #include <database/db_exceptions.h>
-#include <cstdint>
+#include <database/server_selector.h>
 #include <functional>
 #include <list>
 #include <string>
@@ -18,192 +19,6 @@
 namespace isc {
 namespace cb {
 
-/// @brief Config Backend selector.
-///
-/// Each Kea server using database as a configuration respository
-/// may use multiple configuration backends simultaneously. The most
-/// typical case is to use a single configuration backend per server,
-/// but there are use cases when configuration information is distributed
-/// accross multiple database instances. In the future, there may be
-/// also caching mechanisms implemented, which will allow for storing
-/// results of certain database queries in memory.
-///
-/// From the server perspective, the most common use of the configuration
-/// backend is to fetch entire configuration information from the
-/// databases (upon startup) or fetch the latest updates to the
-/// configuration, e.g. new subnet added, DHCP option modified etc.
-/// In those cases, it is not so important from the server which backend
-/// this data come from. Therefore, the server would fetch this information
-/// from all available backends.
-///
-/// When the server administrator wants to insert some new data into
-/// the database, modify existing data or simply wants to check the
-/// contents of one of the database instance, he would specify which
-/// database backend he wants to direct queries to.
-///
-/// The @c BackendSelector class provides means to specify whether
-/// the queries should be directed to any backend (see server case
-/// above) or to a specific backend (data insertion case above).
-/// In addition, the @c BackendSelector allows for using various
-/// criteria for selecting a backend to use. Currently those criteria
-/// are: database type (e.g. mysql), database host and database port.
-/// In order to use a specific port, the database host must also be
-/// specified. Note that in a general case multiple backends of the
-/// same type can be used simultaneously, e.g. multiple MySQL backends.
-/// In that case, it may be necessary to specify host (and port) to
-/// issue a query to the right one.
-///
-/// The @c BackendSelector class may be extended in the future to provide
-/// additional backend selection criteria.
-class BackendSelector {
-public:
-
-    /// @brief Supported database types.
-    ///
-    /// The @c UNSPEC indicates that the database type is not specified
-    /// as selection criteria.
-    enum class Type {
-        MYSQL,
-        PGSQL,
-        CQL,
-        UNSPEC
-    };
-
-    /// @brief Default constructor.
-    ///
-    /// It sets the selector to "unspecified". When this selector is used
-    /// the backend pool will use "any" backend. This has a different meaning
-    /// for each type of query. See the @c BaseConfigBackendPool for details.
-    explicit BackendSelector();
-
-    /// @brief Constructor specifying backend type as a selection criteria.
-    ///
-    /// @param backend_type Type of the backend to be selected.
-    explicit BackendSelector(const Type& backend_type);
-
-    /// @brief Constructor for specifying host and optionally port as a
-    /// selection criteria.
-    ///
-    /// @param host Hostname to be used for selecting a backend.
-    /// @param port Port number to be used for selecting a backend. This value
-    /// is optional and is ignored when set to 0. It must be used on conjuction
-    /// with hostname.
-    explicit BackendSelector(const std::string& host, const uint16_t port = 0);
-
-    /// @brief Constructor for selecting a backend using JSON access map.
-    ///
-    /// The provided access map must have the same structure as an element
-    /// of the "config-databases" configuration parameter. However, it merely
-    /// takes into account: "type", "host" and "port" parameters. In addition,
-    /// these parameters are optional. The following are valid combinations:
-    ///
-    /// @code
-    /// {
-    ///     "type": "mysql"
-    /// }
-    /// @endcode
-    ///
-    /// @code
-    /// {
-    ///     "host": "somehost.example.org"
-    /// }
-    /// @endcode
-    ///
-    /// @code
-    /// {
-    ///     "host": "somehost.example.org",
-    ///     "port": 1234
-    /// }
-    /// @endcode
-    ///
-    /// @code
-    /// {
-    ///     "type": "mysql"
-    ///     "host": "somehost.example.org",
-    /// }
-    /// @endcode
-    ///
-    /// @code
-    /// {
-    ///     "type": "mysql"
-    ///     "host": "somehost.example.org",
-    ///     "port": 1234
-    /// }
-    /// @endcode
-    ///
-    /// where "type" can be any of the supported backend types.
-    ///
-    /// This constructor is useful for creating backend selectors from the
-    /// received control commands.
-    ///
-    /// @param access_map Access map as provided above.
-    explicit BackendSelector(const data::ConstElementPtr& access_map);
-
-    /// @brief Returns instance of the "unspecified" backend selector.
-    static const BackendSelector& UNSPEC();
-
-    /// @brief Checks if selector is "unspecified".
-    ///
-    /// @return true if backend type is @c UNSPEC, hostname is empty and
-    /// port number 0, false otherwise.
-    bool amUnspecified() const;
-
-    /// @brief Returns backend type selected.
-    Type getBackendType() const {
-        return (backend_type_);
-    }
-
-    /// @brief Returns host selected.
-    ///
-    /// @return host if specified or empty string if host is not
-    /// specified.
-    std::string getBackendHost() const {
-        return (host_);
-    }
-
-    /// @brief Returns port selected.
-    ///
-    /// @return port number of the selected backend or 0 if port number
-    /// is not specified.
-    uint16_t getBackendPort() const {
-        return (port_);
-    }
-
-    /// @brief Returns selections as text.
-    ///
-    /// @return Collection of comma separated selections, e.g.
-    /// "type=mysql,host=somehost.example.org,port=1234".
-    std::string toText() const;
-
-    /// @brief Converts string to backend type.
-    ///
-    /// @param type Backend type as string.
-    static Type stringToBackendType(const std::string& type);
-
-    /// @brief Converts backend type to string.
-    ///
-    /// @param type Backend type to be converted.
-    static std::string backendTypeToString(const Type& type);
-
-
-private:
-
-    /// @brief Checks if the specified selector is valid.
-    ///
-    /// It checks if the port number is specified in conjuction with
-    /// host.
-    /// @throw BadValue if selector validation fails.
-    void validate() const;
-
-    /// @brief Backend type selected.
-    Type backend_type_;
-
-    /// @brief Host selected.
-    std::string host_;
-
-    /// @brief Port number selected.
-    uint16_t port_;
-};
 
 /// @brief Base class for configuration backend pools.
 ///
@@ -293,9 +108,9 @@ protected:
     /// was found.
     template<typename PropertyType, typename... InputType>
     void getPropertyPtrConst(PropertyType (ConfigBackendType::*MethodPointer)
-                             (const ServerSelector&, InputType...) const,
-                             const BackendSelector& selector,
-                             const ServerSelector& server_selector,
+                             (const db::ServerSelector&, InputType...) const,
+                             const db::BackendSelector& selector,
+                             const db::ServerSelector& server_selector,
                              PropertyType& property,
                              InputType... input) const {
 
@@ -372,9 +187,9 @@ protected:
     /// was found.
     template<typename PropertyCollectionType, typename... InputType>
     void getMultiplePropertiesConst(PropertyCollectionType (ConfigBackendType::*MethodPointer)
-                                    (const ServerSelector&, InputType...) const,
-                                    const BackendSelector& selector,
-                                    const ServerSelector& server_selector,
+                                    (const db::ServerSelector&, InputType...) const,
+                                    const db::BackendSelector& selector,
+                                    const db::ServerSelector& server_selector,
                                     PropertyCollectionType& properties,
                                     InputType... input) const {
         if (selector.amUnspecified()) {
@@ -443,9 +258,9 @@ protected:
     /// was found.
     template<typename PropertyCollectionType>
     void getAllPropertiesConst(PropertyCollectionType (ConfigBackendType::*MethodPointer)
-                               (const ServerSelector&) const,
-                               const BackendSelector& selector,
-                               const ServerSelector& server_selector,
+                               (const db::ServerSelector&) const,
+                               const db::BackendSelector& selector,
+                               const db::ServerSelector& server_selector,
                                PropertyCollectionType& properties) const {
         if (selector.amUnspecified()) {
             for (auto backend : backends_) {
@@ -517,9 +332,9 @@ protected:
     /// were found.
     template<typename... InputType>
     void createUpdateDeleteProperty(void (ConfigBackendType::*MethodPointer)
-                                    (const ServerSelector&, InputType...),
-                                    const BackendSelector& selector,
-                                    const ServerSelector& server_selector,
+                                    (const db::ServerSelector&, InputType...),
+                                    const db::BackendSelector& selector,
+                                    const db::ServerSelector& server_selector,
                                     InputType... input) {
         auto backends = selectBackends(selector);
         if (backends.empty()) {
@@ -540,7 +355,8 @@ protected:
     /// "unspecified" or there is no backend in the pool, an empty list is returned.
     ///
     /// @param selector Selector for which matching backends should be selected.
-    std::list<ConfigBackendTypePtr> selectBackends(const BackendSelector& selector) const {
+    std::list<ConfigBackendTypePtr>
+    selectBackends(const db::BackendSelector& selector) const {
 
         std::list<ConfigBackendTypePtr> selected;
 
@@ -560,9 +376,9 @@ protected:
         for (auto backend : backends_) {
             // If backend type is specified and it is not matching,
             // do not select this backend.
-            if ((selector.getBackendType() != BackendSelector::Type::UNSPEC) &&
+            if ((selector.getBackendType() != db::BackendSelector::Type::UNSPEC) &&
                 (selector.getBackendType() !=
-                 BackendSelector::stringToBackendType(backend->getType()))) {
+                 db::BackendSelector::stringToBackendType(backend->getType()))) {
                 continue;
             }
 
--- src/lib/database/tests/backend_selector_unittest.cc
+++ src/lib/database/tests/backend_selector_unittest.cc
@@ -6,16 +6,17 @@
 
 
 #include <config.h>
-#include <config_backend/base_config_backend_pool.h>
+#include <database/backend_selector.h>
 #include <boost/scoped_ptr.hpp>
 #include <gtest/gtest.h>
 
 using namespace isc;
-using namespace isc::cb;
+using namespace isc::db;
 using namespace isc::data;
 
 namespace {
 
+// Verifies defaults of the backend selector.
 TEST(BackendSelectorTest, defaults) {
     BackendSelector sel;
     EXPECT_EQ(BackendSelector::Type::UNSPEC, sel.getBackendType());
@@ -25,6 +26,7 @@ TEST(BackendSelectorTest, defaults) {
     EXPECT_EQ("unspecified", sel.toText());
 }
 
+// Verifies that the backend selector can be set to "unspecified".
 TEST(BackendSelectorTest, unspec) {
     BackendSelector sel = BackendSelector::UNSPEC();
     EXPECT_EQ(BackendSelector::Type::UNSPEC, sel.getBackendType());
@@ -34,6 +36,7 @@ TEST(BackendSelectorTest, unspec) {
     EXPECT_EQ("unspecified", sel.toText());
 }
 
+// Verifies that it is possible to select backend by type.
 TEST(BackendSelectorTest, backendTypeSpec) {
     boost::scoped_ptr<BackendSelector> sel;
     ASSERT_NO_THROW(
@@ -46,6 +49,7 @@ TEST(BackendSelectorTest, backendTypeSpec) {
     EXPECT_EQ("type=mysql", sel->toText());
 }
 
+// Verifies that backend can be selected by host and port.
 TEST(BackendSelectorTest, backendHostPortSpec) {
     boost::scoped_ptr<BackendSelector> sel;
     ASSERT_NO_THROW(
@@ -58,6 +62,7 @@ TEST(BackendSelectorTest, backendHostPortSpec) {
     EXPECT_EQ("host=myhost,port=1234", sel->toText());
 }
 
+// Verifies that backend can be selected by host.
 TEST(BackendSelectorTest, backendHostSpec) {
     boost::scoped_ptr<BackendSelector> sel;
     ASSERT_NO_THROW(
@@ -70,6 +75,8 @@ TEST(BackendSelectorTest, backendHostSpec) {
     EXPECT_EQ("host=otherhost", sel->toText());
 }
 
+// Verifies that backend becomes unspecified if the access
+// map is empty.
 TEST(BackendSelectorTest, accessMapTypeUnSpec) {
     ElementPtr access_map = Element::createMap();
     boost::scoped_ptr<BackendSelector> sel;
@@ -83,6 +90,7 @@ TEST(BackendSelectorTest, accessMapTypeUnSpec) {
     EXPECT_EQ("unspecified", sel->toText());
 }
 
+// Verifies that backend can be selected by type using access map.
 TEST(BackendSelectorTest, accessMapTypeSpec) {
     ElementPtr access_map = Element::createMap();
     access_map->set("type", Element::create("mysql"));
@@ -97,6 +105,8 @@ TEST(BackendSelectorTest, accessMapTypeSpec) {
     EXPECT_EQ("type=mysql", sel->toText());
 }
 
+// Verifies that backend can be selected by host and port using
+// access map.
 TEST(BackendSelectorTest, accessMapHostPortSpec) {
     ElementPtr access_map = Element::createMap();
     access_map->set("host", Element::create("myhost"));
@@ -112,6 +122,8 @@ TEST(BackendSelectorTest, accessMapHostPortSpec) {
     EXPECT_EQ("host=myhost,port=1234", sel->toText());
 }
 
+// Verifies that the backend can be selected by host using access
+// map.
 TEST(BackendSelectorTest, accessMapHostSpec) {
     ElementPtr access_map = Element::createMap();
     access_map->set("host", Element::create("myhost"));
@@ -126,6 +138,7 @@ TEST(BackendSelectorTest, accessMapHostSpec) {
     EXPECT_EQ("host=myhost", sel->toText());
 }
 
+// Verifies that selecting backend by port only is not possible.
 TEST(BackendSelectorTest, accessMapPortSpec) {
     ElementPtr access_map = Element::createMap();
     access_map->set("port", Element::create(int64_t(1234)));
@@ -134,6 +147,7 @@ TEST(BackendSelectorTest, accessMapPortSpec) {
                  BadValue);
 }
 
+// Tests conversions of strings to backend types.
 TEST(BackendSelectorTest, stringToBackendType) {
     EXPECT_EQ(BackendSelector::Type::MYSQL,
               BackendSelector::stringToBackendType("mysql"));
@@ -145,6 +159,7 @@ TEST(BackendSelectorTest, stringToBackendType) {
                  BadValue);
 }
 
+// Tests conversions of backend types to strings.
 TEST(BackendSelectorTest, backendTypeToString) {
     EXPECT_EQ("mysql",
               BackendSelector::backendTypeToString(BackendSelector::Type::MYSQL));
