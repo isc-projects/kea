687140501a80a26bb1347257e7a8ecabb38b9f6d
Francis Dupont <fdupont@isc.org>
2018-09-22 01:29:06 +0200

    [65-libyang-database] Moved syntax comments - capitalized YANG

--- src/lib/yang/translator_database.h
+++ src/lib/yang/translator_database.h
@@ -13,7 +13,60 @@
 namespace isc {
 namespace yang {
 
-// @brief Between Yang and JSON translator class for a database access.
+// @brief A translator class for converting a database access between
+// YANG and JSON.
+//
+// Supports kea-dhcp[46]-server, not yet ietf-dhcpv6-server.
+//
+// JSON syntax for all Kea servers with database access is:
+// @code
+// {
+//     "type": <type>, // required
+//     "user": <user>,
+//     "password": <password>,
+//     "host": <host>,
+//     "name": <name>,
+//     "persist": <persist flag>,
+//     "port": <port>,
+//     "lfc-interval": <lfc interval>,
+//     "readonly": <readonly flag>,
+//     "connect-timeout": <connect timeout>,
+//     "contact-points": <contact points>,
+//     "keyspace": <keyspace>,
+//     "max-reconnect-tries": <maximum reconnect tries>,
+//     "reconnect-wait-time": <reconnect wait time>,
+//     "request-timeout": <request timeout>,
+//     "tcp-keepalive": <TCP keepalive>,
+//     "tcp-nodelay": <TCP nodelay flag>,
+//     "user-context": { <json map> },
+//     "comment": <comment>
+// }
+// @endcode
+//
+// YANG syntax for kea-dhcp[46] is:
+// @code
+//  +--rw database                container
+//    |
+//    +--rw datanase=type?        string
+//    +--rw user?                 string
+//    +--rw password?             string
+//    +--rw host?                 string
+//    +--rw name?                 string
+//    +--rw persist?              boolean
+//    +--rw port?                 uint16
+//    +--rw lfc-interval?         uint32
+//    +--rw readonly?             boolean
+//    +--rw connect-timeout?      uint32
+//    +--rw contact-points?       string
+//    +--rw keyspace?             string
+//    +--rw max-reconnect-tries?  uint32
+//    +--rw reconnect-wait-time?  uint32
+//    +--rw request-timeout?      uint32
+//    +--rw tcp-keepalive?        uint32
+//    +--rw tcp-nodelay?          boolean
+//    +--rw user-context?         string
+// @endcode
+//
 class TranslatorDatabase : virtual public TranslatorBasic {
 public:
 
@@ -26,39 +79,14 @@ public:
     /// @brief Destructor.
     virtual ~TranslatorDatabase();
 
-    /// @brief Get and translate a database access from Yang to JSON.
-    ///
-    /// JSON syntax for all Kea servers with database access is:
-    /// @code
-    /// {
-    ///     "type": <type>, // required
-    ///     "user": <user>,
-    ///     "password": <password>,
-    ///     "host": <host>,
-    ///     "name": <name>,
-    ///     "persist": <persist flag>,
-    ///     "port": <port>,
-    ///     "lfc-interval": <lfc interval>,
-    ///     "readonly": <readonly flag>,
-    ///     "connect-timeout": <connect timeout>,
-    ///     "contact-points": <contact points>,
-    ///     "keyspace": <keyspace>,
-    ///     "max-reconnect-tries": <maximum reconnect tries>,
-    ///     "reconnect-wait-time": <reconnect wait time>,
-    ///     "request-timeout": <request timeout>,
-    ///     "tcp-keepalive": <TCP keepalive>,
-    ///     "tcp-nodelay": <TCP nodelay flag>,
-    ///     "user-context": { <json map> },
-    ///     "comment": <comment>
-    /// }
-    /// @endcode
+    /// @brief Get and translate a database access from YANG to JSON.
     ///
     /// @param xpath The xpath of the database.
     /// @return JSON representation of the database.
     /// @throw SysrepoError when sysrepo raises an error.
     isc::data::ElementPtr getDatabase(const std::string& xpath);
 
-    /// @brief Translate and set database access from JSON to Yang.
+    /// @brief Translate and set database access from JSON to YANG.
     ///
     /// Null elem argument removes the database entry.
     ///
@@ -79,30 +107,6 @@ protected:
 
     /// @brief setDatabase for kea-dhcp[46].
     ///
-    /// Yang syntax for kea-dhcp[46] is:
-    /// @code
-    ///  +--rw database                container
-    ///    |
-    ///    +--rw datanase=type?        string
-    ///    +--rw user?                 string
-    ///    +--rw password?             string
-    ///    +--rw host?                 string
-    ///    +--rw name?                 string
-    ///    +--rw persist?              boolean
-    ///    +--rw port?                 uint16
-    ///    +--rw lfc-interval?         uint32
-    ///    +--rw readonly?             boolean
-    ///    +--rw connect-timeout?      uint32
-    ///    +--rw contact-points?       string
-    ///    +--rw keyspace?             string
-    ///    +--rw max-reconnect-tries?  uint32
-    ///    +--rw reconnect-wait-time?  uint32
-    ///    +--rw request-timeout?      uint32
-    ///    +--rw tcp-keepalive?        uint32
-    ///    +--rw tcp-nodelay?          boolean
-    ///    +--rw user-context?         string
-    /// @endcode
-    ///
     /// @param xpath The xpath of the database access.
     /// @param elem The JSON element.
     /// @param skip The skip type field flag.
@@ -114,7 +118,13 @@ protected:
     std::string model_;
 };
 
-// @brief Between Yang and JSON translator class for database accesses.
+// @brief A translator class for converting a database access list between
+// YANG and JSON.
+//
+// Supports kea-dhcp[46]-server, does not exist in ietf-dhcpv6-server.
+//
+// YANG list key is the database-type.
+//
 class TranslatorDatabases : virtual public TranslatorDatabase {
 public:
 
@@ -127,14 +137,14 @@ public:
     /// @brief Destructor.
     virtual ~TranslatorDatabases();
 
-    /// @brief Get and translate database accesses from Yang to JSON.
+    /// @brief Get and translate database accesses from YANG to JSON.
     ///
     /// @param xpath The xpath of databases.
     /// @return JSON representation of databases.
     /// @throw SysrepoError when sysrepo raises an error.
     isc::data::ConstElementPtr getDatabases(const std::string& xpath);
 
-    /// @brief Translate and set database accesses from JSON to Yang.
+    /// @brief Translate and set database accesses from JSON to YANG.
     ///
     /// Null elem argument removes the database list.
     ///
@@ -152,8 +162,6 @@ protected:
     isc::data::ElementPtr getDatabasesKea(const std::string& xpath);
 
     /// @brief setDatabases for kea-dhcp[46].
-    ///
-    /// Key is the database-type.
     ///
     /// @param xpath The xpath of databases.
     /// @param elem The JSON element.
