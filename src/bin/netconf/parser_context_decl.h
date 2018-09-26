c8fecb881c6a0eec64b44eb36952a61a9366476f
Francis Dupont <fdupont@isc.org>
2018-09-26 23:09:16 +0200

    [5-netconf-config] Added netconf syntax

---
+++ src/bin/netconf/parser_context_decl.h
@@ -0,0 +1,20 @@
+// Copyright (C) 2018 Internet Systems Consortium, Inc. ("ISC")
+//
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+#ifndef NETCONF_CONTEXT_DECL_H
+#define NETCONF_CONTEXT_DECL_H
+
+/// @file netconf/parser_context_decl.h Forward declaration of the ParserContext class
+
+namespace isc {
+namespace netconf {
+
+class ParserContext;
+
+}; // end of isc::dhcp namespace
+}; // end of isc namespace
+
+#endif
