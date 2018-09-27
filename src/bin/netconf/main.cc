7399e186250565c4377d5dcb717c1152627bff62
Francis Dupont <fdupont@isc.org>
2018-09-27 12:42:10 +0200

    [128-netconf-use-libprocess] Added use of libprocess in netconf

--- src/bin/netconf/main.cc
+++ src/bin/netconf/main.cc
@@ -5,135 +5,38 @@
 // file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
 #include <config.h>
-#include <kea_version.h>
-
-#include <netconf/netconf_log.h>
+#include <netconf/netconf_controller.h>
 #include <exceptions/exceptions.h>
-#include <process/daemon.h>
+#include <cstdlib>
 #include <iostream>
-#include <fstream>
-#include <unistd.h>
-#include <cstdio>
-#include <signal.h>
 
-#include <sysrepo-cpp/Session.h>
-
-using namespace std;
-using namespace isc;
 using namespace isc::netconf;
+using namespace isc::process;
 
-/// @brief Prints Kea Usage and exits
-///
-/// Note: This function never returns. It terminates the process.
-void
-usage() {
-    cerr << "Kea netconf daemon, version " << VERSION << endl
-         << endl
-         << "Usage: " << endl
-         << "  -c: config-file" << endl
-         << "  -d: debug mode (maximum verbosity)" << endl
-         << "  -v: print version number and exit" << endl
-         << "  -V: print extended version and exit" << endl;
-    exit(EXIT_FAILURE);
-}
-
-/// @name Temporary code until isc::process::Daemon is used.
-///
-/// @{
-const char* PID_FILENAME = "kea-netconf.test_config.pid";
-
-volatile bool SHUTDOWN_FLAG = false;
-
-void
-createPIDFile(int pid) {
-    // This is not a real implemented. We will soon use the one coming
-    // from isc::process::Daemon AFTER it's moved to libprocess.
-
-    ofstream file(PID_FILENAME, ios::trunc);
-    file << pid;
-}
-
-void
-deletePIDFile() {
-    remove(PID_FILENAME);
-}
-
-static void signal_handler(int ) {
-    SHUTDOWN_FLAG = true;
-}
-/// @}
-
-int
-main(int argc, char* argv[]) {
-    // The standard config file
-    std::string config_file("");
-    int ch;
-
-    while ((ch = getopt(argc, argv, "vVc:")) != -1) {
-        switch (ch) {
-        case 'v':
-            cout << string(PACKAGE_VERSION) << endl;
-            return (EXIT_SUCCESS);
-
-        case 'V':
-            cout << string(PACKAGE_VERSION) << endl;
-            cout << "git " << EXTENDED_VERSION << endl;
-            return (EXIT_SUCCESS);
-
-        case 'c': // config file
-            config_file = optarg;
-            break;
-        default:
-            usage();
-        }
-    }
-
-    // Check for extraneous parameters.
-    if (argc > optind) {
-        usage();
-    }
-
-    // Configuration file is required.
-    if (config_file.empty()) {
-        cerr << "Configuration file not specified." << endl;
-        usage();
-    }
-
+int main(int argc, char* argv[]) {
     int ret = EXIT_SUCCESS;
-    try {
 
-        // Temporary code. This will be replaced with isc::process::Daemon
-        // once it is migrated to libprocess. We DO NOT want to bring
-        // the whole libdhcpsrv into netconf.
-        createPIDFile(getpid());
-        signal(SIGHUP, signal_handler);
-        signal(SIGINT, signal_handler);
-        signal(SIGTERM, signal_handler);
+    // Launch the controller passing in command line arguments.
+    // Exit program with the controller's return code.
+    try  {
+        // Instantiate/fetch the application controller singleton.
+        DControllerBasePtr& controller = NetconfController::instance();
 
-        // Initialize logging.  If verbose, we'll use maximum verbosity.
-        bool verbose_mode = true;
-        isc::process::Daemon::loggerInit(NETCONF_LOGGER_NAME, verbose_mode);
-        LOG_INFO(netconf_logger, NETCONF_STARTING).arg(VERSION).arg(getpid());
-
-        Connection conn("kea-netconf");
-        
-        // Tell the admin we are ready to process packets
-        LOG_INFO(netconf_logger, NETCONF_STATED).arg(VERSION);
-
-        // And run the main loop of the server.
-        while (!SHUTDOWN_FLAG) {
-            cout << "Dummy kea-netconf running. Press ctrl-c to terminate."
-                 << endl;
-            sleep(1);
+        // 'false' value disables test mode.
+        controller->launch(argc, argv, false);
+    } catch (const VersionMessage& ex) {
+        std::string msg(ex.what());
+        if (!msg.empty()) {
+            std::cout << msg << std::endl;
         }
-
-        LOG_INFO(netconf_logger, NETCONF_SHUTDOWN);
-
-        deletePIDFile();
-
+    } catch (const InvalidUsage& ex) {
+        std::string msg(ex.what());
+        if (!msg.empty()) {
+            std::cerr << msg << std::endl;
+        }
+        ret = EXIT_FAILURE;
     } catch (const isc::Exception& ex) {
-        // First, we parint the error on stderr (that should always work)
-        cerr << "ERROR:" << ex.what() << endl;
+        std::cerr << "Service failed: " << ex.what() << std::endl;
         ret = EXIT_FAILURE;
     }
 
---
+++ src/bin/netconf/simple_parser.cc
@@ -0,0 +1,86 @@
+// Copyright (C) 2018 Internet Systems Consortium, Inc. ("ISC")
+//
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+#include <config.h>
+
+#include <netconf/simple_parser.h>
+#include <cc/data.h>
+#include <cc/dhcp_config_error.h>
+#include <hooks/hooks_parser.h>
+#include <boost/foreach.hpp>
+
+using namespace isc::data;
+
+namespace isc {
+namespace netconf {
+/// @brief This sets of arrays define the default values in various scopes
+///        of the Netconf Configuration.
+///
+/// Each of those is documented in @file netconf/simple_parser.cc. This
+/// is different than most other comments in Kea code. The reason
+/// for placing those in .cc rather than .h file is that it
+/// is expected to be one centralized place to look at for
+/// the default values. This is expected to be looked at also by
+/// people who are not skilled in C or C++, so they may be
+/// confused with the differences between declaration and definition.
+/// As such, there's one file to look at that hopefully is readable
+/// without any C or C++ skills.
+///
+/// @{
+
+/// @brief This table defines default values for global options.
+///
+/// These are global Netconf parameters.
+const SimpleDefaults NetconfSimpleParser::NETCONF_DEFAULTS = {
+};
+
+/// @}
+
+/// ---------------------------------------------------------------------------
+/// --- end of default values -------------------------------------------------
+/// ---------------------------------------------------------------------------
+
+size_t NetconfSimpleParser::setAllDefaults(const isc::data::ElementPtr& global) {
+    size_t cnt = 0;
+
+    // Set global defaults first.
+    cnt = setDefaults(global, NETCONF_DEFAULTS);
+
+    return (cnt);
+}
+
+void
+NetconfSimpleParser::parse(const NetconfCfgContextPtr& ctx,
+                           const isc::data::ConstElementPtr& config,
+                           bool check_only) {
+
+    // User context can be done at anytime.
+    ConstElementPtr user_context = config->get("user-context");
+    if (user_context) {
+        ctx->setContext(user_context);
+    }
+
+    // Finally, let's get the hook libs!
+    
+    using namespace isc::hooks;
+    HooksConfig& libraries = ctx->getHooksConfig();
+    ConstElementPtr hooks = config->get("hooks-libraries");
+    if (hooks) {
+        HooksLibrariesParser hooks_parser;
+        hooks_parser.parse(libraries, hooks);
+        libraries.verifyLibraries(hooks->getPosition());
+    }
+
+    if (!check_only) {
+        // This occurs last as if it succeeds, there is no easy way
+        // revert it.  As a result, the failure to commit a subsequent
+        // change causes problems when trying to roll back.
+        libraries.loadLibraries();
+    }
+}
+
+};
+};
