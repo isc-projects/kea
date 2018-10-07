8366e9916789955a612f148fb27a6ad35426753d
Francis Dupont <fdupont@isc.org>
2018-10-07 23:10:09 +0200

    [153-netconf-control-socket] Added control socket code from kea-yang

---
+++ src/bin/netconf/http_control_socket.cc
@@ -0,0 +1,130 @@
+// Copyright (C) 2018 Internet Systems Consortium, Inc. ("ISC")
+//
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+/// @file http_control_socket.cc
+/// Contains the HTTP socket derived class for control socket communication.
+
+#include <config.h>
+
+#include <netconf/http_control_socket.h>
+#include <cc/command_interpreter.h>
+#include <http/client.h>
+#include <http/post_request_json.h>
+#include <http/response_json.h>
+#include <config/timeouts.h>
+
+using namespace std;
+using namespace isc::asiolink;
+using namespace isc::config;
+using namespace isc::data;
+using namespace isc::http;
+
+namespace isc {
+namespace netconf {
+
+template <> ControlSocketBasePtr
+createControlSocket<CfgControlSocket::Type::HTTP>(CfgControlSocketPtr ctrl_sock) {
+    return (HttpControlSocketPtr(new HttpControlSocket(ctrl_sock)));
+}
+
+HttpControlSocket::HttpControlSocket(CfgControlSocketPtr ctrl_sock)
+    : ControlSocketBase(ctrl_sock) {
+}
+
+HttpControlSocket::~HttpControlSocket() {
+}
+
+ConstElementPtr
+HttpControlSocket::configGet(const string& service) {
+    if (service == "ca") {
+        return (sendCommand(createCommand("config-get")));
+    } else {
+        return (sendCommand(createCommand("config-get", service)));
+    }
+}
+
+ConstElementPtr
+HttpControlSocket::configTest(ConstElementPtr config, const string& service) {
+    if (service == "ca") {
+        return (sendCommand(createCommand("config-test", config)));
+    } else {
+        return (sendCommand(createCommand("config-test", config, service)));
+    }
+}
+
+ConstElementPtr
+HttpControlSocket::configSet(ConstElementPtr config, const string& service) {
+    if (service == "ca") {
+        return (sendCommand(createCommand("config-set", config)));
+    } else {
+        return (sendCommand(createCommand("config-set", config, service)));
+    }
+}
+
+ConstElementPtr
+HttpControlSocket::sendCommand(ConstElementPtr command) {
+    PostHttpRequestJsonPtr request;
+    request.reset(new PostHttpRequestJson(HttpRequest::Method::HTTP_POST,
+                                          "/",
+                                          HttpVersion(1, 1)));
+    request->setBodyAsJson(command);
+    try {
+        request->finalize();
+    } catch (const std::exception& ex) {
+        isc_throw(ControlSocketError, "failed to create request: "
+                  << ex.what());
+    }
+
+    IOServicePtr io_service(new IOService());
+    HttpClient client(*io_service);
+    boost::system::error_code received_ec;
+    string receive_errmsg;
+    HttpResponseJsonPtr response(new HttpResponseJson());
+
+    client.asyncSendRequest(getUrl(), request, response,
+                [&io_service, &received_ec, &receive_errmsg]
+                (const boost::system::error_code& ec,
+                const HttpResponsePtr&, const string& errmsg) {
+                        // Capture error code and message.
+                        received_ec = ec;
+                        receive_errmsg = errmsg;
+                        // Got the IO service so stop IO service.
+                        // This causes to stop IO service when
+                        // all handlers have been invoked.
+                        io_service->stopWork();
+                },
+                HttpClient::RequestTimeout(TIMEOUT_AGENT_FORWARD_COMMAND));
+
+    // Perform this synchronously.
+    io_service->run();
+
+    if (received_ec) {
+        // Got an error code.
+        isc_throw(ControlSocketError, "communication error (code): "
+                  << received_ec.message());
+    }
+
+    if (!receive_errmsg.empty()) {
+        // Got an error message.
+        isc_throw(ControlSocketError, "communication error (message): "
+                  << receive_errmsg);
+    }
+
+    if (!response) {
+        // Failed to get the answer.
+        isc_throw(ControlSocketError, "empty response");
+    }
+
+    try {
+        return (response->getBodyAsJson());
+    } catch (const std::exception& ex) {
+        isc_throw(ControlSocketError, "unparsable response: " << ex.what());
+    }
+}
+
+} // namespace netconf
+} // namespace isc
+
---
+++ src/bin/netconf/http_control_socket.h
@@ -0,0 +1,86 @@
+// Copyright (C) 2018 Internet Systems Consortium, Inc. ("ISC")
+//
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+/// @file http_control_socket.h
+/// Contains declarations for HTTP control socket communication.
+
+#ifndef HTTP_CONTROL_SOCKET_H
+#define HTTP_CONTROL_SOCKET_H
+
+#include <netconf/control_socket.h>
+
+namespace isc {
+namespace netconf {
+
+/// @brief Class for control socket communication over HTTP socket.
+///
+/// This class is the derived class for control socket communication
+/// over HTTP sockets.
+/// This class implements config-get, config-test and config-set.
+class HttpControlSocket : public ControlSocketBase {
+public:
+
+    /// @brief Constructor.
+    ///
+    /// @param ctrl_sock The control socket configuration.
+    HttpControlSocket(CfgControlSocketPtr ctrl_sock);
+
+    /// @brief Destructor (does nothing).
+    virtual ~HttpControlSocket();
+
+    /// @brief Get configuration.
+    ///
+    /// Call config-get over the control socket.
+    ///
+    /// @param service The target service.
+    /// @return The JSON element answer of config-get.
+    /// @throw ControlSocketError when a communication error occurs.
+    virtual data::ConstElementPtr configGet(const std::string& service);
+
+    /// @brief Test configuration.
+    ///
+    /// Call config-test over the control socket.
+    ///
+    /// @param config The configuration to test.
+    /// @param service The target service.
+    /// @return The JSON element answer of config-test.
+    /// @throw ControlSocketError when a communication error occurs.
+    virtual data::ConstElementPtr configTest(data::ConstElementPtr config,
+                                             const std::string& service);
+
+    /// @brief Set configuration.
+    ///
+    /// Call config-set over the control socket.
+    ///
+    /// @param config The configuration to set.
+    /// @param service The target service.
+    /// @return The JSON element answer of config-set.
+    /// @throw ControlSocketError when a communication error occurs.
+    virtual data::ConstElementPtr configSet(data::ConstElementPtr config,
+                                            const std::string& service);
+
+private:
+    /// @brief Preform the actual communication.
+    ///
+    /// @param command The command to send.
+    /// @return The answer.
+    data::ConstElementPtr sendCommand(data::ConstElementPtr command);
+};
+
+/// @brief Type definition for the pointer to the @c HttpControlSocket.
+typedef boost::shared_ptr<HttpControlSocket> HttpControlSocketPtr;
+
+/// @brief Factory template specialization for http control sockets.
+///
+/// @param ctrl_sock The control socket configuration.
+/// @return A pointer to a http control socket communication object.
+template <> ControlSocketBasePtr
+createControlSocket<CfgControlSocket::Type::HTTP>(CfgControlSocketPtr ctrl_sock);
+
+} // namespace netconf
+} // namespace isc
+
+#endif // HTTP_CONTROL_SOCKET_H
