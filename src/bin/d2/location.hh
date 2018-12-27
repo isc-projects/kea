55cf0972d35fe136edc26c3d3d5625a22bc87a32
Francis Dupont <fdupont@isc.org>
2018-12-27 23:25:14 +0000

    [#30,!183] Regen flex/bison

--- src/bin/d2/location.hh
+++ src/bin/d2/location.hh
@@ -1,5 +1,5 @@
-// Generated 201811271344
-// A Bison parser, made by GNU Bison 3.2.1.
+// Generated 201812272324
+// A Bison parser, made by GNU Bison 3.2.4.
 
 // Locations for Bison parsers in C++
 
