20155aeb9f7da925eeaf42931bbebc1a6209456f
Francis Dupont <fdupont@isc.org>
2018-12-27 20:10:20 +0000

    [#30,!37] Regen bison

--- src/bin/d2/stack.hh
+++ src/bin/d2/stack.hh
@@ -1,5 +1,5 @@
-// Generated 201811271344
-// A Bison parser, made by GNU Bison 3.2.1.
+// Generated 201812272009
+// A Bison parser, made by GNU Bison 3.2.4.
 
 // Starting with Bison 3.2, this file is useless: the structure it
 // used to define is now defined with the parser itself.
