97c4da6501e193b53be313298e857f541d0dd26a
Francis Dupont <fdupont@isc.org>
2019-01-29 23:32:39 +0100

    [406-update-cb_cmds-with-get-commands] Fixed a trivial bug in an old header

--- src/lib/dhcp/option_space_container.h
+++ src/lib/dhcp/option_space_container.h
@@ -1,4 +1,4 @@
-// Copyright (C) 2013-2017 Internet Systems Consortium, Inc. ("ISC")
+// Copyright (C) 2013-2019 Internet Systems Consortium, Inc. ("ISC")
 //
 // This Source Code Form is subject to the terms of the Mozilla Public
 // License, v. 2.0. If a copy of the MPL was not distributed with this
@@ -124,7 +124,7 @@ public:
                          other_it->second->begin();
                      other_items_it != other_it->second->end();
                      ++other_items_it) {
-                    if (items_it->equals(*other_items_it)) {
+                    if ((*items_it)->equals(*other_items_it)) {
                         match_found = true;
                         break;
                     }
