4c849c500235a854c7a87562b3a300cf9f83f71a
Michal Nowikowski <godfryd@isc.org>
2019-03-24 14:25:05 +0100

    added scapy-based tool for sending RS

---
+++ tools/rs-tool.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+from scapy.layers.inet6 import IPv6, ICMPv6ND_RS, ICMPv6NDOptSrcLLAddr
+from scapy.sendrecv import sr
+
+m1 = IPv6(dst='ff02::2', hlim=255)
+m2 = ICMPv6ND_RS()
+m3 = ICMPv6NDOptSrcLLAddr(lladdr='48:a4:72:cd:ed:da')
+m = m1 / m2 / m3
+print("SENDING:")
+m.show()
+
+ans, unans = sr(m)
+
+print("RECEIVED:")
+for s, r in ans:
+    s.show()
