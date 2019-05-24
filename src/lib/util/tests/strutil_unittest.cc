b126558e9e39e9bff517dceac25a00e96d150085
Marcin Siodelski <marcin@isc.org>
2019-05-24 15:34:44 +0200

    [#539,!330] Updated copyright and the comment in unit test.

--- src/lib/util/tests/strutil_unittest.cc
+++ src/lib/util/tests/strutil_unittest.cc
@@ -1,4 +1,4 @@
-// Copyright (C) 2011-2018 Internet Systems Consortium, Inc. ("ISC")
+// Copyright (C) 2011-2019 Internet Systems Consortium, Inc. ("ISC")
 //
 // This Source Code Form is subject to the terms of the Mozilla Public
 // License, v. 2.0. If a copy of the MPL was not distributed with this
@@ -551,7 +551,7 @@ TEST(StringUtilTest, stringSanitizer) {
                        "abc.123");
 }
 
-// Verifies templated buffer iterator trim() function
+// Verifies templated buffer iterator seekTrimmed() function
 TEST(StringUtilTest, seekTrimmed) {
 
     // Empty buffer should be fine.
