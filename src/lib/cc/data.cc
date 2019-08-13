2ff8d3db8bb4635a03156f4895e957a458cb38bd
Francis Dupont <fdupont@isc.org>
2019-08-13 14:35:45 +0200

    [433-update-yang-models] Moved to fabs to near double equal

--- src/lib/cc/data.cc
+++ src/lib/cc/data.cc
@@ -978,7 +978,7 @@ IntElement::equals(const Element& other) const {
 bool
 DoubleElement::equals(const Element& other) const {
     return (other.getType() == Element::real) &&
-           (str() == other.str());
+           (fabs(d - other.doubleValue()) < 1e-14);
 }
 
 bool
