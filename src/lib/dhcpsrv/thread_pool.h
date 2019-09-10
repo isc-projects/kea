f3f216d4e0e003d45d331d59b31d3e65d6d057bd
Razvan Becheriu <razvan@isc.org>
2019-09-10 18:14:54 +0300

    [#883, !506] added thread pool unit tests

--- src/lib/dhcpsrv/thread_pool.h
+++ src/lib/dhcpsrv/thread_pool.h
@@ -32,141 +32,8 @@
 namespace isc {
 namespace dhcp {
 
-/// @brief Defines a generic thread pool queue.
-///
-/// The main purpose is to safely manage thread pool tasks.
-/// The thread pool queue can be 'disabled', which means that no items can be
-/// added or removed from the queue, or 'enabled', which guarantees that
-/// inserting or removing items are thread safe.
-/// In 'disabled' state, all threads waiting on the queue are unlocked and all
-/// operations are non blocking.
 template <typename WorkItem>
-struct ThreadPoolQueue {
-    /// @brief Constructor
-    ///
-    /// Creates the thread pool queue in 'disabled' state
-    ThreadPoolQueue() : exit_(true) {
-    }
-
-    /// @brief Destructor
-    ///
-    /// Destroys the thread pool queue
-    ~ThreadPoolQueue() {
-        stop(true);
-    }
-
-    /// @brief push work item to the queue
-    ///
-    /// Used to add work items in the queue.
-    /// If the queue is 'disabled', this function returns immediately.
-    /// If the queue is 'enabled', this function adds an item to the queue and
-    /// wakes up at least one thread waiting on the queue.
-    ///
-    /// @param item the new iten to be added to the queue
-    void push(WorkItem item) {
-        std::lock_guard<std::mutex> lock(mutex_);
-        if (exit_) {
-            return;
-        }
-        queue_.push(item);
-        // Notify pop function so that it can effectively remove a work item.
-        cv_.notify_all();
-    }
-
-    /// @brief pop work item from the queue or block waiting
-    ///
-    /// Used to retrieve and remove a work item from the queue
-    /// If the queue is 'disabled', this function returns immediately
-    /// (no element).
-    /// If the queue is 'enabled', this function returns the first element in
-    /// the queue or blocks the calling thread if there are no work items
-    /// available.
-    ///
-    /// @param item the reference of the item removed from the queue, if any
-    ///
-    /// @return true if there was a work item removed from the queue, false
-    /// otherwise
-    bool pop(WorkItem& item) {
-        std::unique_lock<std::mutex> lock(mutex_);
-        while (!exit_) {
-            if (queue_.empty()) {
-                // Wait for push or stop functions.
-                cv_.wait(lock);
-                continue;
-            }
-
-            item = queue_.front();
-            queue_.pop();
-            return true;
-        }
-
-        return false;
-    }
-
-    /// @brief count number of work items in the queue
-    ///
-    /// Returns the number of work items in the queue
-    ///
-    /// @return the number of work items
-    size_t count() {
-        std::lock_guard<std::mutex> lock(mutex_);
-        return queue_.size();
-    }
-
-    /// @brief clear remove all work items
-    ///
-    /// Removes all queued work items
-    void clear() {
-        std::lock_guard<std::mutex> lock(mutex_);
-        reset();
-    }
-
-    /// @brief start and enable the queue
-    ///
-    /// Sets the queue state to 'enabled'
-    void start() {
-        std::lock_guard<std::mutex> lock(mutex_);
-        exit_ = false;
-    }
-
-    /// brief stop and disable the queue
-    ///
-    /// Sets the queue state to 'disabled' and optionally removes all work items
-    ///
-    /// @param clear used to specify if the function should also clear the queue
-    void stop(bool clear = false) {
-        std::lock_guard<std::mutex> lock(mutex_);
-        exit_ = true;
-        // Notify get() so that it can exit.
-        cv_.notify_all();
-        if (clear) {
-            reset();
-        }
-    }
-
-private:
-    /// @brief reset clears the queue removing all work items
-    ///
-    /// Must be called in a critical section (mutex locked scope).
-    void reset() {
-        queue_ = std::queue<WorkItem>();
-    }
-
-    /// @brief underlying queue container
-    std::queue<WorkItem> queue_;
-
-    /// @brief mutex used for critical sections
-    std::mutex mutex_;
-
-    /// @brief condition variable used to signal waiting threads
-    std::condition_variable cv_;
-
-    /// @brief the sate of the queue
-    ///
-    /// The 'enabled' state corresponds to false value
-    /// The 'disabled' state corresponds to true value
-    std::atomic_bool exit_;
-};
+struct ThreadPoolQueue;
 
 /// @brief Defines a thread pool which uses a thread pool queue for managing
 /// work items. Each work item is a 'function' object.
@@ -215,10 +82,10 @@ private:
     void run();
 
     /// @brief list of worker threads
-    std::list<std::shared_ptr<std::thread>> worker_threads_;
+    std::list<std::shared_ptr<std::thread>> threads_;
 
     /// @brief underlying work items queue
-    ThreadPoolQueue<WorkItemCallBack> queue_;
+    std::shared_ptr<ThreadPoolQueue<WorkItemCallBack>> queue_;
 
     /// @brief state of the thread pool
     /// The 'run' state corresponds to false value
