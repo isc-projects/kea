b184c280474b1019fac6f59fedf285ab5f3cbcdc
Razvan Becheriu <razvan@isc.org>
2019-09-11 18:15:47 +0300

    [#886, !508] added thread resource mgr

--- src/lib/dhcpsrv/thread_resource_mgr.h
+++ src/lib/dhcpsrv/thread_resource_mgr.h
@@ -17,20 +17,29 @@ namespace dhcp {
 
 template <typename Resource>
 class ThreadResourceMgr {
-    typedef boost::shared_ptr<Resource> ResourcePtr;
+    typedef std::shared_ptr<Resource> ResourcePtr;
 public:
+    /// @brief function to retrieve the specific resource of calling thread
+    /// This function returns the resource of the calling thread from the map
+    /// container or, in case it is not found, it creates a resource and adds it
+    /// to the map container
+    ///
+    /// @return the specific resource of the calling thread
     ResourcePtr resource() {
         std::lock_guard<std::mutex> lock(mutex_);
         auto id = std::this_thread::get_id();
         if (map_.find(id) != map_.end()) {
             return map_[id];
         }
-        ResourcePtr result(new Resource());
+        ResourcePtr result(std::make_shared<Resource>());
         map_[id] = result;
         return result;
     }
 private:
+    /// @brief mutex used to keep the internal state consistent
     std::mutex mutex_;
+
+    /// @brief map container which holds the resources for each thread
     std::unordered_map<std::thread::id, ResourcePtr> map_;
 };
 
