aab25f42410221178960d223da5848bed0f4f550
Francis Dupont <fdupont@isc.org>
2019-10-12 20:28:52 +0200

    [950-dhcp-option-43-pad-end] Fixed the option 43 END bug

--- src/lib/dhcp/libdhcp++.cc
+++ src/lib/dhcp/libdhcp++.cc
@@ -1,4 +1,4 @@
-// Copyright (C) 2011-2019 Internet Systems Consortium, Inc. ("ISC")
+// Copyright (C) 2011-2020 Internet Systems Consortium, Inc. ("ISC")
 //
 // This Source Code Form is subject to the terms of the Mozilla Public
 // License, v. 2.0. If a copy of the MPL was not distributed with this
@@ -458,7 +458,8 @@ size_t
 LibDHCP::unpackOptions4(const OptionBuffer& buf,
                         const std::string& option_space,
                         isc::dhcp::OptionCollection& options,
-                        std::list<uint16_t>& deferred) {
+			std::list<uint16_t>& deferred,
+			bool flexible_pad_end) {
     size_t offset = 0;
     size_t last_offset = 0;
 
@@ -486,7 +487,11 @@ LibDHCP::unpackOptions4(const OptionBuffer& buf,
         uint8_t opt_type = buf[offset++];
 
         // DHO_END is a special, one octet long option
-        if (space_is_dhcp4 && (opt_type == DHO_END)) {
+        // Valid in dhcp4 space or when flexible_pad_end is true and
+        // there is a sub-option configured for this code.
+        if ((opt_type == DHO_END) &&
+            (space_is_dhcp4 ||
+             (flexible_pad_end && (runtime_idx.count(DHO_END) == 0)))) {
             // just return. Don't need to add DHO_END option
             // Don't return offset because it makes this condition
             // and partial parsing impossible to recognize.
@@ -495,7 +500,11 @@ LibDHCP::unpackOptions4(const OptionBuffer& buf,
 
         // DHO_PAD is just a padding after DHO_END. Let's continue parsing
         // in case we receive a message without DHO_END.
-        if (space_is_dhcp4 && (opt_type == DHO_PAD)) {
+        // Valid in dhcp4 space or when flexible_pad_end is true and
+        // there is a sub-option configured for this code.
+        if ((opt_type == DHO_PAD) &&
+            (space_is_dhcp4 ||
+             (flexible_pad_end && (runtime_idx.count(DHO_PAD) == 0)))) {
             continue;
         }
 
