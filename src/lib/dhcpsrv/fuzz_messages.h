08381e6773c2207dca99b280de006d829ff972b8
Stephen Morris <stephen@isc.org>
2019-12-19 10:24:17 +0000

    [#640] Simplification of fuzzing structure
    Instead of using a separate thread to read input from the fuzzer,
    the input is now read in the main thread and transferred to the
    interface on which Kea is expecting it to appear.

--- src/lib/dhcpsrv/fuzz_messages.h
+++ src/lib/dhcpsrv/fuzz_messages.h
@@ -1,4 +1,4 @@
-// File created from ../../../src/lib/dhcpsrv/fuzz_messages.mes on Sun Jun 16 2019 18:13
+// File created from ../../../src/lib/dhcpsrv/fuzz_messages.mes on Mon Jul 01 2019 12:28
 
 #ifndef FUZZ_MESSAGES_H
 #define FUZZ_MESSAGES_H
@@ -11,23 +11,11 @@ namespace dhcp {
 extern const isc::log::MessageID FUZZ_DATA_READ;
 extern const isc::log::MessageID FUZZ_INIT_COMPLETE;
 extern const isc::log::MessageID FUZZ_INIT_FAIL;
-extern const isc::log::MessageID FUZZ_INTERFACE;
-extern const isc::log::MessageID FUZZ_LOOP_EXIT;
-extern const isc::log::MessageID FUZZ_LOOP_MAX;
-extern const isc::log::MessageID FUZZ_PACKET_PROCESSED_CALLED;
 extern const isc::log::MessageID FUZZ_READ_FAIL;
 extern const isc::log::MessageID FUZZ_SEND;
 extern const isc::log::MessageID FUZZ_SEND_ERROR;
-extern const isc::log::MessageID FUZZ_SET;
-extern const isc::log::MessageID FUZZ_SETTING;
 extern const isc::log::MessageID FUZZ_SHORT_SEND;
-extern const isc::log::MessageID FUZZ_SHUTDOWN_INITIATED;
 extern const isc::log::MessageID FUZZ_SOCKET_CREATE_FAIL;
-extern const isc::log::MessageID FUZZ_THREAD_NOT_TERMINATED;
-extern const isc::log::MessageID FUZZ_THREAD_TERMINATED;
-extern const isc::log::MessageID FUZZ_THREAD_WAIT;
-extern const isc::log::MessageID FUZZ_WAITED;
-extern const isc::log::MessageID FUZZ_WAITING;
 
 } // namespace dhcp
 } // namespace isc
