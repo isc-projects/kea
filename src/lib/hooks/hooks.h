35fb4195e7120c3594de6f30d9759a71549751dd
Razvan Becheriu <razvan@isc.org>
2020-06-22 15:56:28 +0300

    [#1287] bump up libs version and hooks version

--- src/lib/hooks/hooks.h
+++ src/lib/hooks/hooks.h
@@ -12,8 +12,8 @@
 
 namespace {
 
-// Version 18 of the hooks framework, set for Kea 1.7.8
-const int KEA_HOOKS_VERSION = 18;
+// Version 19 of the hooks framework, set for Kea 1.7.9
+const int KEA_HOOKS_VERSION = 19;
 
 // Names of the framework functions.
 const char* const LOAD_FUNCTION_NAME = "load";
