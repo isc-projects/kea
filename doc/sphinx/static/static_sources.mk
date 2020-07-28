08d4128f98c42caa49567b570131c522091eddf8
Michal Nowikowski <godfryd@isc.org>
2020-07-28 10:07:33 +0200

    [#1248] fixed Makefiles for sphinx on Centos 7

--- doc/sphinx/static/static_sources.mk
+++ doc/sphinx/static/static_sources.mk
@@ -1,3 +1,3 @@
-static_sources += %reldir%/kea-imageonly-100bw.png
-static_sources += %reldir%/kea-logo-200.png
-static_sources += %reldir%/kea.css
+static_sources += static/kea-imageonly-100bw.png
+static_sources += static/kea-logo-200.png
+static_sources += static/kea.css
