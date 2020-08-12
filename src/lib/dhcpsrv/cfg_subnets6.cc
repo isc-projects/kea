87803b4a676378da59dce06c67f4297d12426099
Razvan Becheriu <razvan@isc.org>
2020-08-12 17:03:12 +0300

    [#1336] added ChangeLog

--- src/lib/dhcpsrv/cfg_subnets6.cc
+++ src/lib/dhcpsrv/cfg_subnets6.cc
@@ -452,7 +452,7 @@ CfgSubnets6::updateStatistics() {
 
     // Only recount the stats if we have subnets.
     if (subnets_.begin() != subnets_.end()) {
-            LeaseMgrFactory::instance().recountLeaseStats6();
+        LeaseMgrFactory::instance().recountLeaseStats6();
     }
 }
 
