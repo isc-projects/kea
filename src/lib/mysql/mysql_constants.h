c8f9a95bc34fcb0b07c38088ae3ee5ae8459b9fe
Razvan Becheriu <razvan@isc.org>
2020-10-13 17:40:53 +0300

    [#1405] fixed unittests

--- src/lib/mysql/mysql_constants.h
+++ src/lib/mysql/mysql_constants.h
@@ -53,7 +53,7 @@ const int MLM_MYSQL_FETCH_FAILURE = 0;
 /// @name Current database schema version values.
 //@{
 const uint32_t MYSQL_SCHEMA_VERSION_MAJOR = 9;
-const uint32_t MYSQL_SCHEMA_VERSION_MINOR = 4;
+const uint32_t MYSQL_SCHEMA_VERSION_MINOR = 5;
 
 //@}
 
