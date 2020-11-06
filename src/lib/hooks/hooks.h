049355337a98bb441de2a0ea55638bdba6ec4b72
Razvan Becheriu <razvan@isc.org>
2020-11-06 10:34:55 +0200

    [#1530] bump lib versions for 1.8.1

--- src/lib/hooks/hooks.h
+++ src/lib/hooks/hooks.h
@@ -12,8 +12,8 @@
 
 namespace {
 
-// Version 21 of the hooks framework, set for Kea 1.8.0
-const int KEA_HOOKS_VERSION = 21;
+// Version 22 of the hooks framework, set for Kea 1.8.1
+const int KEA_HOOKS_VERSION = 22;
 
 // Names of the framework functions.
 const char* const LOAD_FUNCTION_NAME = "load";
