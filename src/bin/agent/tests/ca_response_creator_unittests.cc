ef340bff3b7cbeb1a9e4c950c561d792596874ff
Francis Dupont <fdupont@isc.org>
2020-11-20 09:58:12 +0100

    [#1421] Addressed comments

--- src/bin/agent/tests/ca_response_creator_unittests.cc
+++ src/bin/agent/tests/ca_response_creator_unittests.cc
@@ -327,6 +327,8 @@ TEST_F(CtrlAgentResponseCreatorTest, hookNoAuth) {
     setBasicContext(request_);
 
     // Body: "list-commands" is natively supported by the command manager.
+    // We add a random value in the extra entry: see next unit test
+    // for an explanation about how it is used.
     auto r32 = isc::cryptolink::random(4);
     ASSERT_EQ(4, r32.size());
     int extra = r32[0];
@@ -381,6 +383,16 @@ TEST_F(CtrlAgentResponseCreatorTest, hookBasicAuth) {
     setBasicContext(request_);
 
     // Body: "list-commands" is natively supported by the command manager.
+    // We add a random value in the extra entry:
+    //  - this proves that the auth callout can get the request argument
+    //  - this proves that the auth callout can modify the request argument
+    //    before the request is executed (the extra entry if still present
+    //    would make the command to be rejected as malformed)
+    //  - this proves that a value can be communicate between the auth
+    //    and response callout points
+    //  - this proves that the response callout can get the response argument
+    //  - this proves that the response callout can modify the response
+    //    argument
     auto r32 = isc::cryptolink::random(4);
     ASSERT_EQ(4, r32.size());
     int extra = r32[0];
