d57741fed674367fe228d332a1cfb6d5a18b88a6
Andrei Pavel <andrei@isc.org>
2020-11-20 16:53:03 +0200

    [#1554] regenerate parsers

--- src/bin/d2/stack.hh
+++ src/bin/d2/stack.hh
@@ -1,4 +1,3 @@
-// Generated 202010231220
 // A Bison parser, made by GNU Bison 3.7.2.
 
 // Starting with Bison 3.2, this file is useless: the structure it
--- src/bin/netconf/location.hh
+++ src/bin/netconf/location.hh
@@ -1,4 +1,3 @@
-// Generated 202010231220
 // A Bison parser, made by GNU Bison 3.7.2.
 
 // Locations for Bison parsers in C++
