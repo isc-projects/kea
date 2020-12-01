6c7463acb6777d3d80661383421a806dad1738ab
Francis Dupont <fdupont@isc.org>
2020-12-01 23:09:57 +0100

    [#1522] Updated reference

--- src/lib/dns/tsig.h
+++ src/lib/dns/tsig.h
@@ -1,12 +1,11 @@
-// Copyright (C) 2011-2015,2017 Internet Systems Consortium, Inc. ("ISC")
+// Copyright (C) 2011-2020 Internet Systems Consortium, Inc. ("ISC")
 //
 // This Source Code Form is subject to the terms of the Mozilla Public
 // License, v. 2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
 // IMPORTANT: the server side of this code MUST NOT be used until
-// it was fixed, cf draft-dupont-dnsop-rfc2845bis-00.txt
-// Note that Kea uses only the client side.
+// it was fixed, cf RFC 8945. Note that Kea uses only the client side.
 
 #ifndef TSIG_H
 #define TSIG_H 1
