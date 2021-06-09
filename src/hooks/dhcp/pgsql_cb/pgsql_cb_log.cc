f4b7750a374b5a5d40730f077093f72a24253e5b
Tomek Mrugalski <tomek@isc.org>
2021-06-09 14:19:09 +0200

    [#1848] pgsql_cb logger added

---
+++ src/hooks/dhcp/pgsql_cb/pgsql_cb_log.cc
@@ -0,0 +1,17 @@
+// Copyright (C) 2021 Internet Systems Consortium, Inc. ("ISC")
+//
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+#include <config.h>
+
+#include <pgsql_cb_log.h>
+
+namespace isc {
+namespace dhcp {
+
+isc::log::Logger pgsql_cb_logger("pgsql-cb-hooks");
+
+}  // namespace dhcp
+}  // namespace isc
---
+++ src/hooks/dhcp/pgsql_cb/pgsql_cb_log.h
@@ -0,0 +1,23 @@
+// Copyright (C) 2021 Internet Systems Consortium, Inc. ("ISC")
+//
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+#ifndef LEASE_CMD_LOG_H
+#define LEASE_CMD_LOG_H
+
+#include <log/logger_support.h>
+#include <log/macros.h>
+#include <pgsql_cb_messages.h>
+
+namespace isc {
+namespace dhcp {
+
+extern isc::log::Logger pgsql_cb_logger;
+
+}  // namespace dhcp
+}  // namespace isc
+
+
+#endif
