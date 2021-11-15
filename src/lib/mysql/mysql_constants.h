3dc6b3e43f18118427cab66accc7179bd6892924
Andrei Pavel <andrei@isc.org>
2021-11-15 17:18:33 +0200

    [#2038] kea-admin lease-export mysql

--- src/lib/mysql/mysql_constants.h
+++ src/lib/mysql/mysql_constants.h
@@ -52,7 +52,7 @@ const int MLM_MYSQL_FETCH_FAILURE = 0;
 
 /// @name Current database schema version values.
 //@{
-const uint32_t MYSQL_SCHEMA_VERSION_MAJOR = 12;
+const uint32_t MYSQL_SCHEMA_VERSION_MAJOR = 13;
 const uint32_t MYSQL_SCHEMA_VERSION_MINOR = 0;
 
 //@}
