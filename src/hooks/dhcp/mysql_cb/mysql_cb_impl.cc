9832c56ad669a0b93842fee434c39f8bb0de8d7e
Andrei Pavel <andrei@isc.org>
2021-12-16 16:09:34 +0200

    [#2038] kea-admin lease-dump mysql outputs a memfile-ready CSV

--- src/hooks/dhcp/mysql_cb/mysql_cb_impl.cc
+++ src/hooks/dhcp/mysql_cb/mysql_cb_impl.cc
@@ -61,7 +61,7 @@ MySqlConfigBackendImpl(const DatabaseConnection::ParameterMap& parameters,
     if (code_version != db_version) {
         isc_throw(DbOpenError, "MySQL schema version mismatch: need version: "
                   << code_version.first << "." << code_version.second
-                  << " found version:  " << db_version.first << "."
+                  << " found version: " << db_version.first << "."
                   << db_version.second);
     }
 
