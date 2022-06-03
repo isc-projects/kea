3e4ab418de3b890e0d4434b73f1ef17302c06091
Dan Theisen <djt@isc.org>
2022-06-03 15:37:24 -0700

    [#2395] Clarify ALLOC_ENGINE_V4_ALLOC_FAIL_SUBNET log message

--- src/lib/dhcpsrv/alloc_engine.cc
+++ src/lib/dhcpsrv/alloc_engine.cc
@@ -4508,11 +4508,15 @@ AllocEngine::allocateUnreservedLease4(ClientContext4& ctx) {
     } else {
         // The client is not connected to a shared network. It is connected
         // to a subnet. Let's log some details about the subnet.
+        std::string shared_network = ctx.subnet_->getSharedNetworkName();
+        if (shared_network.empty()) {
+            shared_network = "(none)";
+        }
         LOG_WARN(alloc_engine_logger, ALLOC_ENGINE_V4_ALLOC_FAIL_SUBNET)
             .arg(ctx.query_->getLabel())
             .arg(ctx.subnet_->toText())
             .arg(ctx.subnet_->getID())
-            .arg(ctx.subnet_->getSharedNetworkName());
+            .arg(shared_network);
         StatsMgr::instance().addValue("v4-allocation-fail-subnet",
                                       static_cast<int64_t>(1));
         StatsMgr::instance().addValue(
