96bbbc6f91c7b1e03ce1f9e9560088854c1663c4
Suzanne Goldlust <sgoldlust@isc.org>
2023-06-21 18:02:03 +0000

    [#2833] Remove old broken link in comments

--- src/lib/dhcpsrv/csv_lease_file6.h
+++ src/lib/dhcpsrv/csv_lease_file6.h
@@ -30,8 +30,7 @@ namespace dhcp {
 ///
 /// @todo This class doesn't validate the lease values read from the file.
 /// The @c Lease6 is a structure that should be itself responsible for this
-/// validation (see http://oldkea.isc.org/ticket/2405). However, when #2405
-/// is implemented, the @c next function may need to be updated to use the
+/// validation. However, the @c next function may need to be updated to use the
 /// validation capability of @c Lease6.
 class CSVLeaseFile6 : public isc::util::VersionedCSVFile, public LeaseFileStats {
 public:
@@ -80,8 +79,7 @@ public:
     /// (false).
     ///
     /// @todo Make sure that the values read from the file are correct.
-    /// The appropriate @c Lease6 validation mechanism should be used once
-    /// ticket http://oldkea.isc.org/ticket/2405 is implemented.
+    /// The appropriate @c Lease6 validation mechanism should be used.
     bool next(Lease6Ptr& lease);
 
 private:
