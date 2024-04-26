4aed88418e1c1c30873a8bd61563841391aa7f48
Piotrek Zadroga <piotrek@isc.org>
2024-04-26 16:59:15 +0200

    [#3355] bump up library versions for 2.5.8 release

--- src/lib/hooks/hooks.h
+++ src/lib/hooks/hooks.h
@@ -12,8 +12,8 @@
 
 namespace {
 
-// Version 20507 of the hooks framework, set for Kea 2.5.7
-const int KEA_HOOKS_VERSION = 20507;
+// Version 20508 of the hooks framework, set for Kea 2.5.8
+const int KEA_HOOKS_VERSION = 20508;
 
 // Names of the framework functions.
 const char* const LOAD_FUNCTION_NAME = "load";
