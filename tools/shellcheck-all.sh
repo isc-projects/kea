bb2ce014c8323d676fc9f676ca2676f2861c8be3
Andrei Pavel <andrei@isc.org>
2024-06-05 18:51:51 +0300

    [#3287] automatically determine list of files to shellcheck
    To not allow developers to get away with not shellchecking a new script
    by omitting to add it to the manual list that existed before.

--- tools/shellcheck-all.sh
+++ tools/shellcheck-all.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-# Copyright (C) 2020-2021 Internet Systems Consortium, Inc. ("ISC")
+# Copyright (C) 2020-2024 Internet Systems Consortium, Inc. ("ISC")
 #
 # This Source Code Form is subject to the terms of the Mozilla Public
 # License, v. 2.0. If a copy of the MPL was not distributed with this
@@ -55,7 +55,27 @@ done
 root_path=$(cd "$(dirname "${0}")/.." && pwd)
 cd "${root_path}"
 
+# Disable shellcheck warnings:
+# SC1117: Backslash is literal in "\/". Prefer explicit escaping: "\\/".
+# SC2119: Use "$@" if function's $1 should mean script's $1.
+# SC2039: In POSIX sh, 'local' is undefined.
+# SC3043: In POSIX sh, 'local' is undefined.
+shellcheck_opts="--exclude=SC1117 --exclude=SC2119 --exclude=SC2039 --exclude=SC3043"
+
+files="$(find . -type f -name '*.sh' -or -name '*.sh.in' | sort)"
+
+# Add shell script files that don't end in .sh or .sh.in.
+for i in \
+  src/bin/admin/kea-admin.in \
+  src/bin/keactrl/keactrl.conf.in \
+  src/bin/keactrl/keactrl.in \
+; do
+  if test -f "${i}"; then
+    files="${files} ${i}"
+  fi
+done
+
 # shellcheck disable=SC2046
 # SC2046: Quote this to prevent word splitting.
 # Reason: We explicitly want the parameters split.
-shellcheck $(grep -F 'SHELLCHECK_OPTS:' ./.gitlab-ci.yml | cut -d '"' -f 2 | xargs | sort -uV) $(grep -F 'SCRIPTS+=' ./.gitlab-ci.yml | cut -d '"' -f 2 | xargs | sort -uV)
+shellcheck ${shellcheck_opts} ${files}
