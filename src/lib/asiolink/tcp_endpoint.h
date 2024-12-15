81edc181f85395c39964104ef049a195bafb9737
q66 <q66@chimera-linux.org>
2024-12-15 03:04:53 +0100

    [#3696] Update asiolink for boost 1.87

--- src/lib/asiolink/tcp_endpoint.h
+++ src/lib/asiolink/tcp_endpoint.h
@@ -42,7 +42,7 @@ public:
     /// \param port The TCP port number of the endpoint.
     TCPEndpoint(const IOAddress& address, const unsigned short port) :
         asio_endpoint_placeholder_(
-            new boost::asio::ip::tcp::endpoint(boost::asio::ip::address::from_string(address.toText()),
+            new boost::asio::ip::tcp::endpoint(boost::asio::ip::make_address(address.toText()),
                               port)),
         asio_endpoint_(*asio_endpoint_placeholder_)
     {}
