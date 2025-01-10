079e0352ae0cde1321ec83f2de4c0e30ea29ddfa
Andrei Pavel <andrei@isc.org>
2025-01-10 17:47:16 +0200

    [#3530] Address review
    - Add @ to suppress Makefile output.
    - Warn about file already existing and exit.
    - Use template file as base.

--- changelog_unreleased/.add-entry.sh
+++ changelog_unreleased/.add-entry.sh
@@ -4,12 +4,17 @@ set -eu
 
 parent_dir=$(cd "$(dirname "${0}")" && pwd)
 cd "${parent_dir}" || exit 1
+basedir=$(basename "${parent_dir}")
 
 author=$(git show -s --format='%ae' | cut -d '@' -f 1)
 branch=$(git branch --show-current)
-file="${branch}"
 gitlab_id=$(printf '%s' "${branch}" | cut -d '-' -f 1)
-printf '[category]\t\t%s\n' "${author}" > "${file}"
-printf '\tdescription\n' >> "${file}"
-printf '\t(Gitlab #%s)\n' "${gitlab_id}" >> "${file}"
-printf 'Created changelog_unreleased/%s.\n' "${file}"
+file="${branch}"
+if test -e "${file}"; then
+  printf 'Nothing done. File already exists: %s\n' "${basedir}/${file}"
+  exit 1
+fi
+content=$(cat .template)
+content="${content//author/${author}}"
+content="${content//#0000/#${gitlab_id}}"
+printf '%s\n' "${content}" > "${file}"
