a28eb1f27fb15d7dc51c06f9ab02304f1dfcc0cf
Andrei Pavel <andrei@isc.org>
2025-02-20 15:33:23 +0200

    [#3730] Update meson.sh to pull latest

--- meson.sh
+++ meson.sh
@@ -1,9 +1,15 @@
 #!/bin/sh
 
+# Pulls the latest meson that has fix https://github.com/mesonbuild/meson/pull/13532
+# for issue https://github.com/mesonbuild/meson/issues/11322.
+#
+# Usage: just like meson
+# Example: ./meson.sh setup build
+
 if test ! -d venv; then
   python3 -m venv ./venv
   ./venv/bin/pip install --upgrade pip
-  ./venv/bin/pip install git+https://github.com/serebit/meson.git@iterative-transitive-link-dep-resolve
+  ./venv/bin/pip install git+https://github.com/mesonbuild/meson.git
 fi
 # shellcheck disable=SC1091
 # SC1091: Not following: ... was not specified as input (see shellcheck -x).
