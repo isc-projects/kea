abedf4ca9685d219c590a90211e03e3fab5efc48
Andrei Pavel <andrei@isc.org>
2025-02-24 14:52:14 +0200

    [#3731] Install ninja too in meson.sh

--- meson.sh
+++ meson.sh
@@ -9,7 +9,9 @@
 if test ! -d venv; then
   python3 -m venv ./venv
   ./venv/bin/pip install --upgrade pip
+  # When meson 1.8 will be released, we can pin the versions.
   ./venv/bin/pip install git+https://github.com/mesonbuild/meson.git
+  ./venv/bin/pip install ninja
 fi
 # shellcheck disable=SC1091
 # SC1091: Not following: ... was not specified as input (see shellcheck -x).
