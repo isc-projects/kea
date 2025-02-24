7226bdcaa9c47da3703af0e8063e025ce03e5c95
Razvan Becheriu <razvan@isc.org>
2025-02-24 10:05:56 +0200

    [#3333] updated meson files

--- src/hooks/dhcp/lease_cmds/binding_variables.cc
+++ src/hooks/dhcp/lease_cmds/binding_variables.cc
@@ -1,8 +1,9 @@
 // Copyright (C) 2025 Internet Systems Consortium, Inc. ("ISC")
 //
-// This Source Code Form is subject to the terms of the Kea Hooks Basic
-// Commercial End User License Agreement v2.0. See COPYING file in the premium/
-// directory.
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
 
 #include <config.h>
 
