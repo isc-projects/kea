a5710470ed655e13bdc794bc205fa964bfd6e4b0
Andrei Pavel <andrei@isc.org>
2025-03-20 08:33:47 +0200

    [#3731] Fix KEA_LFC_BUILD_DIR

--- src/lib/dhcpsrv/tests/memfile_lease_mgr_unittest.cc
+++ src/lib/dhcpsrv/tests/memfile_lease_mgr_unittest.cc
@@ -1,4 +1,4 @@
-// Copyright (C) 2012-2024 Internet Systems Consortium, Inc. ("ISC")
+// Copyright (C) 2012-2025 Internet Systems Consortium, Inc. ("ISC")
 //
 // This Source Code Form is subject to the terms of the Mozilla Public
 // License, v. 2.0. If a copy of the MPL was not distributed with this
@@ -120,9 +120,7 @@ public:
         DatabaseConnection::setIOService(io_service_);
         ProcessSpawn::setIOService(io_service_);
 
-        std::ostringstream s;
-        s << KEA_LFC_BUILD_DIR << "/kea-lfc";
-        setenv("KEA_LFC_EXECUTABLE", s.str().c_str(), 1);
+        setenv("KEA_LFC_EXECUTABLE", KEA_LFC_EXECUTABLE, 1);
 
         // Remove lease files and products of Lease File Cleanup.
         removeFiles(getLeaseFilePath("leasefile4_0.csv"));
