4edef571dd73936dfdbc8680dadfe4fb975a6280
Andrei Pavel <andrei@isc.org>
2025-07-10 17:25:18 +0300

    [#4009] Bump up library versions for 2.6.4

--- src/lib/hooks/hooks.h
+++ src/lib/hooks/hooks.h
@@ -12,8 +12,8 @@
 
 namespace {
 
-// Version 20603 of the hooks framework, set for Kea 2.6.3
-const int KEA_HOOKS_VERSION = 20603;
+// Version 20604 of the hooks framework, set for Kea 2.6.4
+const int KEA_HOOKS_VERSION = 20604;
 
 // Names of the framework functions.
 const char* const LOAD_FUNCTION_NAME = "load";
