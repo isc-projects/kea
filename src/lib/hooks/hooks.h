fbe1095a205887753c378f2f921ae6f6f06436aa
Thomas Markwalder <tmark@isc.org>
2025-08-14 11:22:24 -0400

    [#4060] Bumped library version numbers
    Changes to be committed:
    modified:   src/lib/asiolink/meson.build
    modified:   src/lib/cryptolink/meson.build
    modified:   src/lib/dhcpsrv/meson.build
    modified:   src/lib/hooks/hooks.h
    modified:   src/lib/hooks/meson.build

--- src/lib/hooks/hooks.h
+++ src/lib/hooks/hooks.h
@@ -12,8 +12,8 @@
 
 namespace {
 
-// Version 30000 of the hooks framework, set for Kea 3.0.0
-const int KEA_HOOKS_VERSION = 30000;
+// Version 30001 of the hooks framework, set for Kea 3.0.1
+const int KEA_HOOKS_VERSION = 30001;
 
 // Names of the framework functions.
 const char* const LOAD_FUNCTION_NAME = "load";
