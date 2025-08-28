b03071ba6912a08e4cc4cb4db50d9624ce86f41e
Francis Dupont <fdupont@isc.org>
2025-08-29 00:40:03 +0200

    [#4085] Proposed update

--- src/lib/asiodns/io_fetch.h
+++ src/lib/asiodns/io_fetch.h
@@ -16,6 +16,7 @@
 #include <util/buffer.h>
 
 #include <boost/asio/coroutine.hpp>
+#include <boost/asio/deadline_timer.hpp>
 #include <boost/shared_array.hpp>
 #include <boost/shared_ptr.hpp>
 #include <boost/date_time/posix_time/posix_time_types.hpp>
--- src/lib/asiolink/interval_timer.h
+++ src/lib/asiolink/interval_timer.h
@@ -7,6 +7,7 @@
 #ifndef ASIOLINK_INTERVAL_TIMER_H
 #define ASIOLINK_INTERVAL_TIMER_H 1
 
+#include <boost/asio/deadline_timer.hpp>
 #include <boost/shared_ptr.hpp>
 #include <functional>
 
