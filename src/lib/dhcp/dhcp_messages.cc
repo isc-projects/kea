dd95d1d8751c221364a4e0971b8e58e4bf32b82f
Francis Dupont <fdupont@isc.org>
2025-12-05 15:16:30 +0100

    [#4248] Checkpoint added logger

---
+++ src/lib/dhcp/dhcp_messages.cc
@@ -0,0 +1,23 @@
+// File created from src/lib/dhcp/dhcp_messages.mes
+
+#include <cstddef>
+#include <log/message_types.h>
+#include <log/message_initializer.h>
+
+namespace isc {
+namespace dhcp {
+
+
+} // namespace dhcp
+} // namespace isc
+
+namespace {
+
+const char* values[] = {
+    NULL
+};
+
+const isc::log::MessageInitializer initializer(values);
+
+} // Anonymous namespace
+
---
+++ src/lib/dhcp/dhcp_messages.h
@@ -0,0 +1,15 @@
+// File created from src/lib/dhcp/dhcp_messages.mes
+
+#ifndef DHCP_MESSAGES_H
+#define DHCP_MESSAGES_H
+
+#include <log/message_types.h>
+
+namespace isc {
+namespace dhcp {
+
+
+} // namespace dhcp
+} // namespace isc
+
+#endif // DHCP_MESSAGES_H
