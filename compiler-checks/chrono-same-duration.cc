812a34d836cda7a75b4f81b31195c342ccba6cdf
Francis Dupont <fdupont@isc.org>
2026-03-08 23:26:33 +0100

    [#3982] Last improvements

--- compiler-checks/chrono-same-duration.cc
+++ compiler-checks/chrono-same-duration.cc
@@ -2,6 +2,8 @@
 #include <type_traits>
 
 int main() {
-    return(std::is_same<std::chrono::system_clock::duration,
-                        std::chrono::steady_clock::duration>::value ? 0 : 1);
+    static_assert(std::is_same<std::chrono::system_clock::duration,
+                               std::chrono::steady_clock::duration>::value,
+                  "CHRONO_SAME_DURATION");
+    return(0);
 }
