7eac4063b21f51a115647c43502c6d601412905e
Razvan Becheriu <razvan@isc.org>
2026-03-13 11:36:46 +0200

    [#4401] bump up library versions for 2.6.5 release

--- src/lib/hooks/hooks.h
+++ src/lib/hooks/hooks.h
@@ -12,8 +12,8 @@
 
 namespace {
 
-// Version 20604 of the hooks framework, set for Kea 2.6.4
-const int KEA_HOOKS_VERSION = 20604;
+// Version 20605 of the hooks framework, set for Kea 2.6.5
+const int KEA_HOOKS_VERSION = 20605;
 
 // Names of the framework functions.
 const char* const LOAD_FUNCTION_NAME = "load";
