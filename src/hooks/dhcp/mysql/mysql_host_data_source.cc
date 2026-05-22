de732a53ff258c0e936d736a435c3a2cecb15a51
Razvan Becheriu <razvan@isc.org>
2026-05-22 10:00:50 +0000

    [#4483] addressed review comments

--- src/hooks/dhcp/mysql/mysql_host_data_source.cc
+++ src/hooks/dhcp/mysql/mysql_host_data_source.cc
@@ -1494,7 +1494,7 @@ public:
         columns_[excluded_prefix_index_] = "excluded_prefix";
         columns_[excluded_prefix_len_index_] = "excluded_prefix_len";
 
-        static_assert(6 < RESERVATION_COLUMNS, "6 < RESERVATION_COLUMNS");.
+        static_assert(6 < RESERVATION_COLUMNS, "6 < RESERVATION_COLUMNS");
     }
 
     /// @brief Returns last fetched reservation id.
